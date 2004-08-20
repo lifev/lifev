b8a5fa51e113d4ea2a5abbbc37c887a82da7edc3
prudhomm <prudhomm>
2004-08-20 10:45:15 +0000

    instead of an assert that will be disabled with -DNDEBUG or configure --enable-opt. throw an exception std::invalid_argument which will be then caught as some point of the execution flow.

--- life/lifefem/bdf.cpp
+++ life/lifefem/bdf.cpp
@@ -1,53 +1,62 @@
 /*
   This file is part of the LifeV library
   Copyright (C) 2001,2002,2003,2004 EPFL, INRIA and Politechnico di Milano
-  
+
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
   version 2.1 of the License, or (at your option) any later version.
-  
+
   This library is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
   Lesser General Public License for more details.
-  
+
   You should have received a copy of the GNU Lesser General Public
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
+#include <stdexcept>
+#include <sstream>
+
 #include "bdf.hpp"
 
 
 Bdf::Bdf(const UInt n):
   _n(n),_s(0),_alpha(n+1),_beta(n)
 {
-  ASSERT(n >0 && n <= MAX_ORDER, "Error in bdf order specification");
-
-  switch (n){ 
-    case 1:
-      _alpha[0] = 1.; // Backward Euler
-      _alpha[1] = 1.;
-      _beta[0] = 1.; // u^{n+1} \approx u^n
-      break;
-    case 2:
-      _alpha[0] = 3./2.;
-      _alpha[1] = 2.;
-      _alpha[2] = -1./2.;
-      _beta[0] = 2.;
-      _beta[1] = -1.;
-      break;
-    case 3:
-      _alpha[0] = 11./6.;
-      _alpha[1] = 3.;
-      _alpha[2] = -3./2.;
-      _alpha[3] = 1./3.;
-      _beta[0] = 3.;
-      _beta[1] =-3.;
-      _beta[2] = 1.;
-      break;
- }   
-  _unk.resize(n);
+    if ( n <=0 || n > MAX_ORDER )
+    {
+        std::ostringstream __ex;
+        __ex << "Error: wrong BDF order\n"
+             << " you want to use BDF order " << n << "\n"
+             << " we support BDF order from 1 to " << MAX_ORDER << "\n";
+        throw std::invalid_argument( __ex.str() );
+    }
+    switch (n){
+        case 1:
+            _alpha[0] = 1.; // Backward Euler
+            _alpha[1] = 1.;
+            _beta[0] = 1.; // u^{n+1} \approx u^n
+            break;
+        case 2:
+            _alpha[0] = 3./2.;
+            _alpha[1] = 2.;
+            _alpha[2] = -1./2.;
+            _beta[0] = 2.;
+            _beta[1] = -1.;
+            break;
+        case 3:
+            _alpha[0] = 11./6.;
+            _alpha[1] = 3.;
+            _alpha[2] = -3./2.;
+            _alpha[3] = 1./3.;
+            _beta[0] = 3.;
+            _beta[1] =-3.;
+            _beta[2] = 1.;
+            break;
+    }
+    _unk.resize(n);
 }
 
 
@@ -61,7 +70,7 @@ void Bdf::initialize_unk(Vector u0)
   vector< Vector >::iterator iter_end=_unk.end();
 
   _s = u0.size();
-  
+
   for (  iter=_unk.begin() ; iter != iter_end; iter++){
     *iter=u0;
   }
@@ -74,7 +83,7 @@ void Bdf::initialize_unk(vector<Vector> uv0)
 {
   vector< Vector >::iterator iter=_unk.begin();
   vector< Vector >::iterator iter_end=_unk.end();
-  
+
   vector< Vector >::iterator iter0 = uv0.begin();
 
   _s = iter0->size();
@@ -85,8 +94,8 @@ void Bdf::initialize_unk(vector<Vector> uv0)
   ASSERT(n0<_n,"Initial data are not enough for the selected BDF")
 
   // if n0>n, only the first n unital data will be considered
-  if (_n>n0) cout << "The initial data set is larger than needed by the BDF. Only the first n data will be considered. " << endl; 
-  
+  if (_n>n0) cout << "The initial data set is larger than needed by the BDF. Only the first n data will be considered. " << endl;
+
   for (iter=_unk.begin() ; iter != iter_end; iter++){
     *iter=*iter0;
     iter0++;
@@ -111,14 +120,14 @@ vector<Vector> Bdf::unk()
 
 double Bdf::coeff_der(UInt i)
 {
-  // Pay attention: i is c-based indexed 
+  // Pay attention: i is c-based indexed
   ASSERT(i>=0&i<_n+1,"Error in specification of the time derivative coefficient for the BDF formula (out of range error)");
   return _alpha[i];
 }
 
 double Bdf::coeff_ext(UInt i)
 {
-  // Pay attention: i is c-based indexed 
+  // Pay attention: i is c-based indexed
   ASSERT(i>=0&i<_n,"Error in specification of the time derivative coefficient for the BDF formula (out of range error)");
   return _beta[i];
 }
@@ -129,9 +138,9 @@ void Bdf::showMe()
   cout << endl;
   cout << "*** Coefficients: " << endl;
   cout << endl;
-  for (UInt i=0;i<_n+1;++i) 
+  for (UInt i=0;i<_n+1;++i)
     cout << "alpha("<<i<<") = "<<_alpha[i]<<endl;
-  for (UInt i=0;i<_n;++i) 
+  for (UInt i=0;i<_n;++i)
     cout << "beta("<<i<<") = "<<_beta[i]<<endl;
 
   cout << "Length unknown vectors:" << _unk.size() << "," << _s << endl;
@@ -155,16 +164,16 @@ void Bdf::showMe()
 
 void Bdf::shift_right(Vector unk_curr)
 {
-  vector< Vector >::iterator it=_unk.end()-1;  
-  vector< Vector >::iterator itm1=_unk.end()-1;  
-  vector< Vector >::iterator itb=_unk.begin();  
+  vector< Vector >::iterator it=_unk.end()-1;
+  vector< Vector >::iterator itm1=_unk.end()-1;
+  vector< Vector >::iterator itb=_unk.begin();
 
   for (; it!=itb; --it){
     itm1--;
-    *it=*itm1; 
+    *it=*itm1;
   }
-  *itb=unk_curr;  
- 
+  *itb=unk_curr;
+
   return;
 }
 
@@ -173,14 +182,14 @@ Vector Bdf::time_der(Real dt)
 {
   // ! Compute the right hand side of the time derivative formula, i.e.
   // ! \alpha[0] u^{n+1} = right hand side
-  Vector ut(_s);  
+  Vector ut(_s);
 
   for (UInt j=0;j<_s;++j) ut[j]=0.;
 
   for (UInt j=0;j<_s;++j){
-   for (UInt i=0;i<_n;++i) 
+   for (UInt i=0;i<_n;++i)
      ut[j]=ut[j]+_alpha[i+1]/dt*_unk[i][j];
-     }  
+     }
 
   return ut;
 }
@@ -190,14 +199,14 @@ Vector Bdf::time_der()
   // ! Compute the right hand side of the time derivative formula, i.e.
   // ! \alpha[0] u^{n+1} = right hand side
   // ! In this case, the time step is considered elsewhere (e.g. included in the mass matrix)
-  Vector ut(_s);  
+  Vector ut(_s);
 
   for (UInt j=0;j<_s;++j) ut[j]=0.;
 
   for (UInt j=0;j<_s;++j){
-   for (UInt i=0;i<_n;++i) 
+   for (UInt i=0;i<_n;++i)
      ut[j]=ut[j]+_alpha[i+1]*_unk[i][j];
-     }  
+     }
 
   return ut;
 }
@@ -206,13 +215,13 @@ Vector Bdf::extrap()
 {
   // ! Compute the approximation of u^{n+1}
   // ! extrapolated
-  Vector ue(_s);  
+  Vector ue(_s);
   for (UInt j=0;j<_s;++j) ue[j]=0.;
 
   for (UInt j=0;j<_s;++j){
-   for (UInt i=0;i<_n;++i) 
+   for (UInt i=0;i<_n;++i)
      ue[j]=ue[j]+_beta[i]*_unk[i][j];
-     }  
+     }
 
   return ue;
 }
