18715f98aeb5b74cecb8b1a636c99afbdb48f00e
winkelma <winkelma>
2004-09-03 09:50:32 +0000

    * moved wr_opendx_vector(...) without template to new file openDX_wrtrs.cpp   non-template function bodies should NOT be in header files! * missing std:: added where required * translated itailan comments and variables to english * realignment

--- life/lifefilters/openDX_wrtrs.hpp
+++ life/lifefilters/openDX_wrtrs.hpp
@@ -21,20 +21,20 @@
 /      ...                                                                   /
 /                                                                            /
 /                                                                            /
-/ VTK WRITERS UTILITIES                                                      /
+/ OPENDX WRITERS UTILITIES                                                   /
 /                                                                            /
 / #Version 0.1 Experimental:   30/08/2001 Alessandro Veneziani               /
 /                                                                            /
 /  I am sorry for my C++ at a very beginner level !!!!                       /
 /                                                                            /
-/ #Purpose: Container for subroutines for OPENDX                                /
+/ #Purpose: Container for subroutines for OPENDX                             /
 /                                                                            /
 /                                                                            /
 /---------------------------------------------------------------------------*/
 #ifndef _OPENDX_WRTRS_
 #define _OPENDX_WRTRS_
 
-#define OPENDX_VERSION 4.0 //DA VERIFICARE
+#define OPENDX_VERSION 4.0 // to be verified
 #define VRANK 1 //Vector Rank
 #define SRANK 0 //Scalar Rank
 #define EPS_DX 1.e-30 // a "sort" of machine epsilon due to a DX bug.
@@ -52,20 +52,22 @@
 
 namespace LifeV
 {
-/*! This subroutines writes the header part of a OPENDX file (which is constant in a time-dependent but fixed-mesh problem)
-    for a general problem, in particular for instance for a P2 on LinearTetra Mesh: in such a case
-    we need to compute the coordinates of all the supplementary nodes
-    For "iso" cases (whenever we have ALL the coordinates of the nodes, it is recommended using the other subroutine
-    with the same name but with a different signature.
+/*! This subroutine writes the header part of a OPENDX file (which is constant
+  in a time-dependent but fixed-mesh problem). For a general problem, in
+  particular for instance for a P2 on LinearTetra Mesh: in such a case
+  we need to compute the coordinates of all the supplementary nodes.
+  For "iso" cases (whenever we have ALL the coordinates of the nodes, it is
+  recommended using the other subroutine with the same name but with a
+  different signature.
 */
 template<typename TheMesh, typename TheDof, typename TheFem>
-void wr_opendx_header(string fname, const TheMesh& mesh, const TheDof& dof, TheFem& fem, string nam_fe)
+void wr_opendx_header(std::string fname, const TheMesh& mesh, const TheDof& dof,
+                      TheFem& fem, std::string nam_fe)
 {
 
- ofstream ofile(fname.c_str());
+ std::ofstream ofile(fname.c_str());
 
  ASSERT(ofile,"Error: Output file cannot be open"); //
-
   UInt nldpe=fem.refFE.nbDofPerEdge;
   UInt nldpv=fem.refFE.nbDofPerVertex;
   UInt nldpf=fem.refFE.nbDofPerFace;
@@ -79,18 +81,18 @@ void wr_opendx_header(string fname, const TheMesh& mesh, const TheDof& dof, TheF
   UInt nf=mesh.numFaces();
   UInt nldof = nle*nldpe+nlv*nldpv+nlf*nldpf+nldpV;
  //  _totalDof=nV*nldpV+ne*nldpe+nv*nldpv+nf*nldpf;
- UInt num_points =  dof.numTotalDof(); // discuterne con Luca
- UInt num_points_supp =  dof.numTotalDof() - nv; // discuterne con Luca
- vector<Real> supp_x(num_points_supp,0.0);
- vector<Real> supp_y(num_points_supp,0.0);
- vector<Real> supp_z(num_points_supp,0.0);
+ UInt num_points =  dof.numTotalDof(); // discuss this with Luca
+ UInt num_points_supp =  dof.numTotalDof() - nv; // discuss this with Luca
+ std::vector<Real> supp_x(num_points_supp,0.0);
+ std::vector<Real> supp_y(num_points_supp,0.0);
+ std::vector<Real> supp_z(num_points_supp,0.0);
  Real x,y,z;
- char virgole='"';
+ char quotes='"';
 
  // Coordinates writing
 
-  ofile << endl; // Aggiunta Diego
-  ofile << "object " << virgole << "pos" << virgole << endl;
+  ofile << endl; // added by Diego
+  ofile << "object " << quotes << "pos" << quotes << endl;
   ofile << "class array ";
   ofile << "type float ";
   ofile << "rank " << VRANK << " shape " << NDIM;
@@ -103,9 +105,10 @@ void wr_opendx_header(string fname, const TheMesh& mesh, const TheDof& dof, TheF
 
   // Vertex based Dof: the coordinates are available from the Pont List
  for(i=0;i<nv;++i)
-  ofile << mesh.pointList[i].x() << " " <<  mesh.pointList[i].y() << " " <<  mesh.pointList[i].z() << endl;
+  ofile << mesh.pointList[i].x() << " " <<  mesh.pointList[i].y() << " "
+        <<  mesh.pointList[i].z() << endl;
 
- // Now I store the coordinates of the supplementary nodes in a temporary vector
+// Now I store the coordinates of the supplementary nodes in a temporary vector
   // Edge Based Dof
   gcount = 0;
   lcount = nlv-1;
@@ -113,7 +116,8 @@ void wr_opendx_header(string fname, const TheMesh& mesh, const TheDof& dof, TheF
     for (ie=1; ie<= nV; ++ie){
       fem.updateJac(mesh.volumeList(ie));
       for (i=1; i<=nle; ++i){
-	fem.coorMap(x,y,z,fem.refFE.xi(i+lcount),fem.refFE.eta(i+lcount),fem.refFE.zeta(i+lcount));
+	fem.coorMap(x,y,z,fem.refFE.xi(i+lcount),fem.refFE.eta(i+lcount),
+                    fem.refFE.zeta(i+lcount));
 	index = mesh.localEdgeId(ie,i);
 	supp_x[index-1]=x;supp_y[index-1]=y;supp_z[index-1]=z;
       }
@@ -126,7 +130,8 @@ void wr_opendx_header(string fname, const TheMesh& mesh, const TheDof& dof, TheF
     for (ie=1; ie<= nV; ++ie){
       fem.updateJac(mesh.volumeList(ie));
       for (i=1; i<=nlf; ++i){
-	fem.coorMap(x,y,z,fem.refFE.xi(i+lcount),fem.refFE.eta(i+lcount),fem.refFE.zeta(i+lcount));
+	fem.coorMap(x,y,z,fem.refFE.xi(i+lcount),fem.refFE.eta(i+lcount),
+                    fem.refFE.zeta(i+lcount));
 	index = mesh.localFaceId(ie,i)+gcount;
 	supp_x[index-1]=x;supp_y[index-1]=y;supp_z[index-1]=z;
 
@@ -142,7 +147,8 @@ void wr_opendx_header(string fname, const TheMesh& mesh, const TheDof& dof, TheF
       fem.updateJac(mesh.volumeList(ie));
       //Alain (11/07/02) just one dof on volume ! change if there is more...
       for (i=1; i<=1; ++i){
-	fem.coorMap(x,y,z,fem.refFE.xi(i+lcount),fem.refFE.eta(i+lcount),fem.refFE.zeta(i+lcount));
+	fem.coorMap(x,y,z,fem.refFE.xi(i+lcount),fem.refFE.eta(i+lcount),
+                    fem.refFE.zeta(i+lcount));
 	index = ie+gcount;
 	supp_x[index-1]=x;supp_y[index-1]=y;supp_z[index-1]=z;
       }
@@ -159,7 +165,7 @@ void wr_opendx_header(string fname, const TheMesh& mesh, const TheDof& dof, TheF
   ofile << endl;
 
  // connectivity
-  ofile << "object " << virgole << "con" << virgole << endl;
+  ofile << "object " << quotes << "con" << quotes << endl;
   ofile << "class array ";
   ofile << "type int ";
   // Alain: this test was not working in case of using mixed fe
@@ -175,7 +181,8 @@ void wr_opendx_header(string fname, const TheMesh& mesh, const TheDof& dof, TheF
   //#endif
   else if (nam_fe == "P2tilde")
     ofile << "rank " << VRANK << " shape " << nldof-7;
-  ASSERT((nam_fe == "P1" || nam_fe == "P2" || nam_fe == "P2tilde" || nam_fe == "P1bubble"),
+  ASSERT((nam_fe == "P1" || nam_fe == "P2" || nam_fe == "P2tilde" ||
+          nam_fe == "P1bubble"),
 	 "output header defined only for finite element P1, P2 or P2tilde");
 
   //#if defined(LINEAR_P1)
@@ -278,23 +285,26 @@ for (i=0;i<nV;++i){
   }
   //#endif
 
-  ofile << "attribute " << virgole << "element type" << virgole << " string " << virgole << CELL << virgole << endl;
-  ofile << "attribute " << virgole << "ref" << virgole << " string " << virgole << "positions" << virgole << endl;
+  ofile << "attribute " << quotes << "element type" << quotes
+        << " string " << quotes << CELL << quotes << endl;
+  ofile << "attribute " << quotes << "ref" << quotes
+        << " string " << quotes << "positions" << quotes << endl;
 
   ofile << endl;
 
 }
 
 
-/* ! This subroutine considers the "iso" cases, whenever the points of the mesh actually coincide with the ones
- adopted for the computation. In this case, the coordinates of the nodes are ALL immediately available
- and there is no need of recomputing or rereading the other coordinates
- (which on the base of the McKoy algorithm are not explicitly computed so far (see fields...update))
+/*! This subroutine considers the "iso" cases, whenever the points of the mesh
+  actually coincide with the ones adopted for the computation. In this case,
+  the coordinates of the nodes are ALL immediately available and there is no
+  need of recomputing or rereading the other coordinates (which on the base of
+  the McKoy algorithm are not explicitly computed so far (see fields...update))
 */
 template<typename RegionMesh, typename Dof>
-void wr_opendx_header(string fname, const RegionMesh& mesh, const Dof& dof)
+void wr_opendx_header(std::string fname, const RegionMesh& mesh, const Dof& dof)
 {
- ofstream ofile(fname.c_str());
+ std::ofstream ofile(fname.c_str());
 
  ASSERT(ofile,"Error: Output file cannot be open"); //
 
@@ -306,11 +316,11 @@ void wr_opendx_header(string fname, const RegionMesh& mesh, const Dof& dof)
   UInt nf=mesh.numFaces();
   UInt nldof = mesh.numLocalVertices();;
  //  _totalDof=nV*nldpV+ne*nldpe+nv*nldpv+nf*nldpf;
- char virgole='"';
+ char quotes='"';
 
  // Coordinates writing
 
-  ofile << "object " << virgole << "pos" << virgole << endl;
+  ofile << "object " << quotes << "pos" << quotes << endl;
   ofile << "class array ";
   ofile << "type float ";
   ofile << "rank " << VRANK << " shape " << NDIM;
@@ -321,15 +331,17 @@ void wr_opendx_header(string fname, const RegionMesh& mesh, const Dof& dof)
 
   // Dof: the coordinates are available from the Pont List
   for(i=0;i<nv;++i){
-   if (abs(mesh.pointList[i].x())<EPS_DX)  mesh.pointList[i].x()=0. ; // needed due to dx bugs
-   if (abs(mesh.pointList[i].y())<EPS_DX)  mesh.pointList[i].y()=0. ; // needed due to dx bugs
-   if (abs(mesh.pointList[i].z())<EPS_DX)  mesh.pointList[i].z()=0. ; // needed due to dx bugs
+   // needed due to dx bugs
+   if (abs(mesh.pointList[i].x())<EPS_DX)  mesh.pointList[i].x()=0. ;
+   if (abs(mesh.pointList[i].y())<EPS_DX)  mesh.pointList[i].y()=0. ;
+   if (abs(mesh.pointList[i].z())<EPS_DX)  mesh.pointList[i].z()=0. ;
 
-  ofile << mesh.pointList[i].x() << " " <<  mesh.pointList[i].y() << " " <<  mesh.pointList[i].z() << endl;
+  ofile << mesh.pointList[i].x() << " " <<  mesh.pointList[i].y() << " "
+        <<  mesh.pointList[i].z() << endl;
   }
 
  // connectivity
-  ofile << "object " << virgole << "con" << virgole << endl;
+  ofile << "object " << quotes << "con" << quotes << endl;
   ofile << "class array ";
   ofile << "type int ";
   ofile << "rank " << VRANK << " shape " << nldof;
@@ -344,31 +356,33 @@ void wr_opendx_header(string fname, const RegionMesh& mesh, const Dof& dof)
   ofile << endl;
  }
 
-  ofile << "attribute " << virgole << "element type" << virgole << " string " << virgole << CELL << virgole << endl;
-  ofile << "attribute " << virgole << "ref" << virgole << " string " << virgole << "positions" << virgole << endl;
+  ofile << "attribute " << quotes << "element type" << quotes << " string "
+        << quotes << CELL << quotes << endl;
+  ofile << "attribute " << quotes << "ref" << quotes << " string " << quotes
+        << "positions" << quotes << endl;
   ofile << endl;
 
 
 }
 
-/* ! {\tt  wr_opendx_scalar} is a subrotuine for writing SCALARS unknown for OPENDX
-   the file is appended to an existing one (at least you should call before {\tt wr_vtk_ascii_header})
-
-modification for accepting various type of vector. Alain 21/01/02.
+/*! {\tt  wr_opendx_scalar} is a subrotuine for writing SCALARS unknown for
+  OPENDX. The file is appended to an existing one (at least you should call
+  before {\tt wr_vtk_ascii_header}). Modification for accepting various type of
+  vector. Alain 21/01/02.
 */
 template<typename VectorType>
-void wr_opendx_scalar(string fname, string name, VectorType U)
+void wr_opendx_scalar(std::string fname, std::string name, VectorType U)
 {
  unsigned int i;
- char virgole='"';
- ofstream ofile(fname.c_str(),ios::app);
- string Ext = ".data"; // Aggiunta Diego
- string nameExt = name + Ext; // Aggiunta Diego
+ char quotes='"';
+ std::ofstream ofile(fname.c_str(),std::ios::app);
+ std::string Ext = ".data"; // added by Diego
+ std::string nameExt = name + Ext; // added by Diego
 
  ASSERT(ofile,"Error: Output file cannot be open");
 
 
-  ofile << "object " << virgole << nameExt << virgole << endl; // Modificata Diego
+  ofile << "object " << quotes << nameExt << quotes << endl; // modif. by Diego
   ofile << "class array ";
   ofile << "type double ";
   ofile << "rank " << SRANK << " ";
@@ -380,68 +394,41 @@ void wr_opendx_scalar(string fname, string name, VectorType U)
    ofile << U[i] << endl;
   }
 
-  ofile << "attribute " << virgole << "dep" << virgole << " string " << virgole << "positions" << virgole << endl;
+  ofile << "attribute " << quotes << "dep" << quotes << " string " << quotes
+        << "positions" << quotes << endl;
   ofile << endl;
-  ofile << "object " << virgole << name << virgole << " class field" << endl;
-  ofile << "component " << virgole << "positions" << virgole << " value " << virgole << "pos" << virgole << endl;
-  ofile << "component " << virgole << "connections" << virgole << " value " << virgole << "con" << virgole << endl;
-  ofile << "component " << virgole << "data" << virgole << " value " << virgole << nameExt << virgole << endl; // Modificata Diego
-  ofile << endl; // Aggiunta Diego
-  ofile << "end";// Aggiunta Diego: Attenzione deve essere posizionato una sola volta alla fine del file !
-  ofile << endl;
-}
-
-void wr_opendx_vector(string fname, string name, vector<Real> U, vector<Real> V, vector<Real> W)
-{
- unsigned int i;
- char virgole='"';
- ofstream ofile(fname.c_str(),ios::app);
- string Ext = ".data";
- string nameExt = name + Ext;
-
- ASSERT(ofile,"Error: Output file cannot be open");
-
-
-  ofile << "object " << virgole << nameExt << virgole << endl;
-  ofile << "class array ";
-  ofile << "type double ";
-  ofile << "rank " << VRANK << " shape " << NDIM;
-  ofile << " items " <<  U.size() << " ";
-  ofile << "data follows" << endl;
-
-  for (i=0;i<U.size();++i){
-   if (abs(U[i])<EPS_DX) U[i]=0. ; // needed due to dx bugs
-   if (abs(V[i])<EPS_DX) V[i]=0. ; // needed due to dx bugs
-   if (abs(W[i])<EPS_DX) W[i]=0. ; // needed due to dx bugs
-   ofile << U[i] << " " << V[i] << " " << W[i] << endl;
-  }
-
-  ofile << "attribute " << virgole << "dep" << virgole << " string " << virgole << "positions" << virgole << endl;
-  ofile << endl;
-  ofile << "object " << virgole << name << virgole << " class field" << endl;
-  ofile << "component " << virgole << "positions" << virgole << " value " << virgole << "pos" << virgole << endl;
-  ofile << "component " << virgole << "connections" << virgole << " value " << virgole << "con" << virgole << endl;
-  ofile << "component " << virgole << "data" << virgole << " value " << virgole << nameExt << virgole << endl;
-  ofile << endl;
-  ofile << "end";
+  ofile << "object " << quotes << name << quotes << " class field" << endl;
+  ofile << "component " << quotes << "positions" << quotes << " value "
+        << quotes << "pos" << quotes << endl;
+  ofile << "component " << quotes << "connections" << quotes << " value "
+        << quotes << "con" << quotes << endl;
+  ofile << "component " << quotes << "data" << quotes << " value " << quotes
+        << nameExt << quotes << endl; // modified by Diego
+  ofile << endl; // added by Diego
+  ofile << "end";// added by Diego
+  // caution: "end" must be put only once at the end of the file
   ofile << endl;
 }
+
+void wr_opendx_vector(std::string fname, std::string name, std::vector<Real> U,
+                      std::vector<Real> V, std::vector<Real> W);
 
 // Version accepting various vector types, Alain 21/01/02
 template<typename VectorType>
-void wr_opendx_vector(string fname, string name, VectorType U, UInt nbcomp)
+void wr_opendx_vector(std::string fname, std::string name, VectorType U,
+                      UInt nbcomp)
 {
  unsigned int i,j,udim;
- char virgole='"';
- ofstream ofile(fname.c_str(),ios::app);
- string Ext = ".data";
- string nameExt = name + Ext;
+ char quotes='"';
+ std::ofstream ofile(fname.c_str(),std::ios::app);
+ std::string Ext = ".data";
+ std::string nameExt = name + Ext;
 
  ASSERT(ofile,"Error: Output file cannot be open");
 
  udim=U.size();
 
-  ofile << "object " << virgole << nameExt << virgole << endl;
+  ofile << "object " << quotes << nameExt << quotes << endl;
   ofile << "class array ";
   ofile << "type float ";
   ofile << "rank " << VRANK << " shape " << nbcomp;
@@ -451,18 +438,23 @@ void wr_opendx_vector(string fname, string name, VectorType U, UInt nbcomp)
  for (i=0;i< udim/nbcomp; ++i)
    {
      for (j=0; j<nbcomp; j++){
-       if (abs(U[i+j*udim/nbcomp])<EPS_DX) U[i+j*udim/nbcomp]=0. ; // needed due to dx bugs
-       ofile << U[i+j*udim/nbcomp] << " ";
+       unsigned int pos=i+j*udim/nbcomp;
+       if (abs(U[pos])<EPS_DX) U[pos]=0. ; // needed due to dx bugs
+       ofile << U[pos] << " ";
      }
      ofile << endl;
    }
 
-  ofile << "attribute " << virgole << "dep" << virgole << " string " << virgole << "positions" << virgole << endl;
+  ofile << "attribute " << quotes << "dep" << quotes << " string " << quotes
+        << "positions" << quotes << endl;
   ofile << endl;
-  ofile << "object " << virgole << name << virgole << " class field" << endl;
-  ofile << "component " << virgole << "positions" << virgole << " value " << virgole << "pos" << virgole << endl;
-  ofile << "component " << virgole << "connections" << virgole << " value " << virgole << "con" << virgole << endl;
-  ofile << "component " << virgole << "data" << virgole << " value " << virgole << nameExt << virgole << endl;
+  ofile << "object " << quotes << name << quotes << " class field" << endl;
+  ofile << "component " << quotes << "positions" << quotes << " value "
+        << quotes << "pos" << quotes << endl;
+  ofile << "component " << quotes << "connections" << quotes << " value "
+        << quotes << "con" << quotes << endl;
+  ofile << "component " << quotes << "data" << quotes << " value " << quotes
+        << nameExt << quotes << endl;
   ofile << endl;
   ofile << "end";
   ofile << endl;
