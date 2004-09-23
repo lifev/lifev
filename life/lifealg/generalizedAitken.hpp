e73c5bb03cbe63218634172abc68069d8789eede
simone <simone>
2004-09-23 18:56:32 +0000

    nonLinRichardson.hpp is a slight modification of newton.hpp for the resolution of a nonlinear system of equation. The aim is to use methods deriving from domain decomposition and apply them to a soupled non-linear system.
    the class generalizedAitken is used by noLinRichardson to determine the
    relaxation parameter. It is wrote in general form, even if it still lack of
    some codelines for the real general case. Should work for nonLinRichardson.

---
+++ life/lifealg/generalizedAitken.hpp
@@ -0,0 +1,166 @@
+
+/* -*- mode: c++ -*-
+
+  This file is part of the LifeV library
+
+  Author(s): Simone Deparis <simone.deparis@epfl.ch>
+       Date: 2004-09-23
+
+  Copyright (C) 2004 EPFL
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+#ifndef _GENERALIZEDAITKEN_H
+#define _GENERALIZEDAITKEN_H
+
+namespace LifeV
+{
+
+template<class Vector, class Real>
+class generalizedAitken
+{
+  /*
+  compute the acceleration with the vector variant of Aitken.
+  lk,lk1.  are column vectors (lambda_k lambda_{k-1})
+  Rk, Rk-1  are  column vectors of residuals(possibly more than one) .
+  lk: lambda at the iterate k
+  lk1: lambda at the iterate k-1
+  Rk: residual(s) associated to lk. in some cases = lk-f(lk)
+  Rk1: residual(s) associated to lk1.
+  defaultOmega: defaultOmega can only be a scalar.
+     If defaultOmega < 0, then do not use Aitken, but fixed relaxation
+      parameter=abs(defaultOmega)
+     If you want to assign a fixed parameter different from 
+     the default one, you can use 
+       Vector deltaLambda(lam, res, omega)
+     be sure that omega has the size sizeOmega!
+  If length(Rk1)==0, then Aitken acceleration is not applicable,
+     hence we need a default value for omega.
+     !: should be of the size 1,size(Rk,2)
+  sizeOmega is the number of relaxation parameters to be computed.
+    usually sizeOmega=1, but for example fo r a N-N preconditioner, =2.
+  */
+private:
+  Vector* lk, lk1;
+  Vector* Rk, Rk1;
+  Real defaultOmega;
+  int sizeOmega;
+public:
+  generalizedAitken(const Real& defOmega=0.1, const int size=1);
+  ~generalizedAitken();
+  void restart();
+  Vector deltaLambda(const Vector& lam, const Vector* res);
+  Vector deltaLambda(const Vector& lam, const Vector* res, Real* omega);
+  // in this case, omega is taken as the default value
+
+};
+
+
+
+
+template<class Vector, class Real>
+generalizedAitken<class Vector, class Real>::generalizedAitken(const Real& defOmega=0.1, const int size=1):
+  lk(nil),lk1(nil),Rk(nil),Rk1(nil), sizeOmega(size),
+  defaultOmega(defOmega)
+{}
+
+template<class Vector, class Real>
+generalizedAitken<class Vector, class Real>::~generalizedAitken()
+{
+  restart();
+}
+
+template<class Vector, class Real>
+generalizedAitken<class Vector, class Real>::~generalizedAitken()
+{
+  delete[] lk;
+  delete[] lk1;
+  delete[] Rk;
+  delete[] Rk1;
+}
+
+
+template<class Vector, class Real>
+generalizedAitken<class Vector, class Real>::Vector deltaLambda(const Vector& lam, const Vector* residue, Real* omega)
+{
+  Vector res(lam.size());
+  res=0;
+  int i;
+
+  if (defaultOmega<=0){
+    for (i=0; i<sizeOmega; i++){
+      res+= omega[i] * residue[i];
+    }
+    return(res);
+  }
+  
+
+  if (lk1 == nil){ // back to default omega
+    lk1 = lk;
+    Rk1 = Rk;
+    lk = new Vector;
+    Rk = new Vector[sizeOmega];
+
+    *lk = lam;
+    for (i=0; i<sizeOmega; i++){
+      *Rk = residue[i];
+      res+= omega[i] * Rk[i];
+    }
+    return(res);
+  }
+
+  Vector* temp;
+  temp = lk1;  lk1 = lk;  lk = temp;
+  temp = Rk1;  Rk1 = lk;  Rk = temp;
+  *lk = lam;
+  for (i=0; i<sizeOmega; i++){
+    *Rk = residue[i];
+  }
+  
+  Vector *resDiff;
+  resDiff = new Vector[sizeOmega];
+  for (i=0; i<sizeOmega; i++){
+    resDiff[i] = Rk[i] - Rk1[i];
+  }
+  
+  if (sizeOmega == 1) {
+    omega[0]= - resDiff[0] * (*lk[0] - lk1[0]) / resDiff[0].norm();
+  } else {
+    cout << "not yet implemented" << endl;
+    //  *omega=-pinv(Rk-Rk1)*(lk-lk1);
+  }
+
+  for (i=0; i<sizeOmega; i++){
+    res+= omega[i] * Rk[i];
+  }
+  return(res);
+}
+
+
+template<class Vector, class Real>
+generalizedAitken<class Vector, class Real>::Vector deltaLambda(const Vector& lam, const Vector* residue)
+{
+  Real* omega;
+  omega = new Real[sizeOmega];
+  int i;
+  for(i=0; i<sizeOmega; i++){
+    omega[i] = abs(defaultOmega);
+  }
+  return(deltaLambda(lam,residue,omega) );
+
+}
+
+}
+#endif
---
+++ life/lifealg/nonLinRichardson.hpp
@@ -0,0 +1,183 @@
+/*
+  This file is part of the LifeV library
+  Copyright (C) 2001,2002,2003,2004 EPFL, INRIA and Politechnico di Milano
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+#ifndef _NONLINRICHARDSON_H
+#define _NONLINRICHARDSON_H
+
+#include <algorithm> // for min and max
+#include "linesearch_parabolic.hpp"
+#include "linesearch_cubic.hpp"
+#include "generalizedAitken.hpp"
+
+namespace LifeV
+{
+template<class Fct,class Vector,class Real, class Norm>
+int nonLinRichardson(Vector& sol,Fct& f,Norm& norm,Real abstol,Real reltol,int& maxit,
+	   Real eta_max,int linesearch=0, ofstream& out_res, const Real& time,
+	   const Real omega)
+{
+  /*
+    sol            :  the solution
+    maxit          :  input: maximum iterations, output: nb of iterations
+    abstol, reltol :  the stoping criteria is abstol+reltol*norm(residual_0),
+    eta_max        :  Maximum error tolerance for residual in linear solver.
+                      The linear solver terminates when the relative
+		      linear residual is smaller than eta*| f(sol) |.
+		      The value linear_rel_tol send for the relative tolerance
+		      to the linear solver is therefore eta. eta is determined
+		      by the modified Eisenstat-Walker formula if etamax > 0.
+		      If eta_max < 0, then eta = |etamax| for the entire
+		      iteration (e.g. etamax = -1e-6 ensures that the linear
+		      tolerance would be always 1e-6). Default value = 0.9
+    linesearch     :  for now consider only the case linesearch=0 
+                      (coded but not theoretically analysed)
+    omega          :  default relaxation parameter to be passed to Aitken.
+                      if omega is negative, then its absolute value is
+		      taken as constant relaxation parameter
+  */
+
+  /*
+    max_increase_res: maximum number of successive increases in residual
+                      before failure is reported
+  */
+  const int max_increase_res=5;
+  /*
+     Parameters for the linear solver, gamma: Default value = 0.9
+  */
+  const Real gamma   = 0.9;
+  //----------------------------------------------------------------------
+  Real linres;
+
+  int iter=0,increase_res=0;
+  Vector residual=sol,step=sol;
+  step=0.;
+  Real  normResOld=1,lambda,slope;
+  f.evalResidual(residual,sol,iter);
+  Real normRes = norm(residual),normStep=0,
+    stop_tol = abstol + reltol*normRes,
+    ratio;
+  Real eta_old,eta_new,linear_rel_tol=fabs(eta_max);
+  generalizedAitken<Vector,Real> aitk(omega);
+  //
+  cout << "------------------------------------------------------------------"
+       << endl;
+  cout << "    NonLinRichardson 0: residual=" << normRes << ", stoping tolerance = "
+       << stop_tol << endl;
+  cout << "------------------------------------------------------------------"
+       << endl;
+
+  out_res << time << "    " << iter << "   " << normRes << endl;
+
+  while( normRes > stop_tol && iter < maxit){
+    iter++;
+    ratio = normRes / normResOld;
+    normResOld = normRes;
+    normRes = norm(residual);
+    f.updatePrec(sol,iter);
+    linres = linear_rel_tol;
+    f.solvePrec(step,-1.*residual,linres); // residual = f(sol)
+    /*
+      linres contains the relative linear tolerance achieved by the
+      linear solver, i.e linear_rel_tol = | -f(sol) - P step | / |-f(sol)|
+    */
+    step = aitk.deltaLambda(sol,&step);
+
+
+    slope = normRes*normRes*(linres*linres - 1);
+    cout << "### slope = " << slope << endl;
+    /*
+      slope denotes the quantity f^T J step, which is generally used by
+      line search algorithms. This formula comes form Brown & Saad (1990),
+      formula (3.4). BE CAREFUL: it assumes that the linear solver is GMRES,
+      with zero as initial guess (in particular it does not work with restart
+      gmres, see formula (3.7) and (3.9) of Brown & Saad (1990))
+    */
+    lambda = 1.;
+    //
+    // -- line search
+    //
+    switch(linesearch){
+    case 0:// no linesearch
+      sol += step;
+      f.evalResidual(residual,sol,iter);
+      normRes = norm(residual);
+      break;
+    case 1:
+      lineSearch_parab(f,norm,residual,sol,step,normRes,lambda,iter);
+      break;
+    case 2: // recommended
+      lineSearch_cubic(f,norm,residual,sol,step,normRes,lambda,slope,iter);
+      break;
+    default:
+      cout << "Unknown linesearch \n";
+      exit(1);
+    }
+    //
+    //-- end of line search
+    //
+    normStep = lambda*norm(step);
+    ratio = normRes/normResOld;
+    if(ratio > 1){
+      increase_res ++;
+      cout << "!!! NonLinRichardson warning: increase in residual \n";
+      if(increase_res == max_increase_res){
+	cout << "!!! NonLinRichardson:" << max_increase_res
+	     << " consecutive increases in residual" << endl;
+	maxit = iter;
+	return 1;
+      }
+    } else {
+      increase_res=0;
+    }
+    cout << "------------------------------------------------------------------"
+	 << endl;
+    cout << "    NonLinRichardson " << iter << ": residual=" << normRes << ",  step="
+	 << normStep << endl;
+    cout << "------------------------------------------------------------------"
+	 << endl;
+
+    out_res << time << "    " << iter << "   " << normRes << endl;
+
+    //
+    //-- forcing term computation (Eisenstat-Walker)
+    //
+    if (eta_max>0){
+      eta_old=linear_rel_tol;
+      eta_new=gamma*ratio*ratio;
+      if(gamma*eta_old*eta_old>.1) eta_new=max(eta_new,gamma*eta_old*eta_old);
+      linear_rel_tol=min(eta_new,eta_max);
+      linear_rel_tol=min(eta_max,max(linear_rel_tol,.5*stop_tol/normRes));
+      cout <<"    NonLinRichardson: forcing term eta = " << linear_rel_tol << endl;
+    }
+    //
+    //-- end of forcing term computation
+    //
+  }
+  if(normRes > stop_tol){
+    cout << "!!! NonLinRichardson: convergence fails" << endl;
+    maxit = iter;
+    return 1;
+  }
+  cout << "--- NonLinRichardson: convergence in " << iter << " iterations\n\n";
+  maxit = iter;
+  return 0;
+}
+}
+#endif
+
+
