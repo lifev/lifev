fb93aeb16c793242d12f83267554ca2d09771e76
winkelma <winkelma>
2004-09-24 09:41:01 +0000

    provide similar interface to Aztec as already available for Petsc

---
+++ life/lifealg/SolverAztec.cpp
@@ -0,0 +1,117 @@
+/* -*- mode: c++ -*-
+
+  This file is part of the LifeV library
+
+  Author(s): Christoph Winkelmann <christoph.winkelmann@epfl.ch>
+       Date: 2004-09-22
+
+  Copyright (C) 2004 EPFL
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+/**
+   \file SolverAztec.hpp
+   \author Christoph Winkelmann <christoph.winkelmann@epfl.ch>
+   \date 2004-09-22
+*/
+
+#include <SolverAztec.hpp>
+
+class GetPot;
+
+namespace LifeV {
+
+UInt SolverAztec::_solverNumber = 1;
+
+SolverAztec::SolverAztec()
+    : _matrix(0), _precond(0), _tempPattern(0), _tempMatrix(0)
+{
+    _data_org[AZ_N_internal] = 0;
+    _data_org[AZ_N_border]= 0;
+    _data_org[AZ_N_external]= 0;
+    _data_org[AZ_N_neigh]= 0;
+    _data_org[AZ_name]= _solverNumber++;
+    AZ_set_proc_config(_proc_config, AZ_NOT_MPI);
+
+    // let dataAztec set the defaults
+    GetPot dataFile;
+    DataAztec dataAztec(dataFile, "aztec");
+    dataAztec.aztecOptionsFromDataFile(_options, _params);
+}
+
+SolverAztec::~SolverAztec() {
+    if (_matrix) {
+        AZ_matrix_destroy(&_matrix);
+        AZ_precond_destroy(&_precond);
+    }
+}
+
+SolverAztec* SolverAztec::New() {
+    return new SolverAztec;
+}
+
+/*!
+  \brief Gets the last residual norm that has been computed.
+
+  \return last residual norm
+*/
+double SolverAztec::residualNorm() const {
+    return _status[AZ_r];
+}
+
+void SolverAztec::setMatrix(MSRMatr<value_type> const& m) {
+    _tempPattern.reset(0);
+    _tempMatrix.reset(0);
+    _setMatrix(m);
+}
+
+void SolverAztec::setMatrix(CSRMatr<CSRPatt, value_type> const& m) {
+    _tempPattern.reset(new MSRPatt(*(m.Patt())));
+    // _tempMatrix.reset(new MSRMatr<value_type>(*_tempPattern, m));
+    _tempMatrix.reset(new MSRMatr<value_type>(*_tempPattern));
+    CSRmat2MSRmat(*_tempMatrix, m);
+    _setMatrix(*_tempMatrix);
+}
+
+void SolverAztec::_setMatrix(MSRMatr<value_type> const& m) {
+    int nEq = m.Patt()->nRows();
+    _data_org[AZ_N_internal] = nEq;
+    if (_matrix) {
+        AZ_matrix_destroy(&_matrix);
+        AZ_precond_destroy(&_precond);
+    }
+    _matrix = AZ_matrix_create(nEq);
+    _precond = AZ_precond_create(_matrix, AZ_precondition, NULL);
+    AZ_set_MSR(_matrix, (int*)(m.Patt()->giveRaw_bindx()),
+               (double*)(m.giveRaw_value()), _data_org, 0, NULL, AZ_LOCAL);
+}
+
+void SolverAztec::solve( array_type& x, array_type const& b) {
+    if (_matrix) {
+        std::cerr << "[SolverAztec::solve]  Solving primal\n";
+        AZ_iterate(&x[0], &b[0], _options, _params, _status, _proc_config,
+                   _matrix, _precond, NULL);
+    } else {
+        std::cerr << "[SolverAztec::solve]  ERROR: Matrix not set\n";
+    }
+}
+
+void SolverAztec::setOptionsFromGetPot(GetPot const& dataFile,
+                                       std::string section) {
+    DataAztec dataAztec(dataFile, section);
+    dataAztec.aztecOptionsFromDataFile(_options, _params);
+}
+
+} // namespace LifeV
---
+++ life/lifealg/SolverAztec.hpp
@@ -0,0 +1,152 @@
+/* -*- mode: c++ -*-
+
+  This file is part of the LifeV library
+
+  Author(s): Christoph Winkelmann <christoph.winkelmann@epfl.ch>
+       Date: 2004-09-22
+
+  Copyright (C) 2004 EPFL
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+/**
+   \file SolverAztec.hpp
+   \author Christoph Winkelmann <christoph.winkelmann@epfl.ch>
+   \date 2004-09-22
+*/
+#ifndef SolverAztec_H
+#define SolverAztec_H 1
+
+#include <iostream>
+#include "dataAztec.hpp"
+#include <vecUnknown.hpp>
+
+class GetPot;
+
+namespace LifeV {
+
+/*!
+  \class SolverAztec
+  \brief wrap aztec linear solvers
+
+  @author Christoph Winkelmann
+*/
+class SolverAztec {
+public:
+
+    /** @name Typedefs
+     */
+    //@{
+
+    typedef double value_type;
+    typedef SolverAztec solver_type;
+    //typedef St::Array::SArray<double,1>::type array_type;
+    typedef Vector array_type;
+
+    //@}
+
+    /** @name Constructors, destructor
+     */
+    //@{
+
+    //! default constructor
+    SolverAztec();
+
+    //! create a new instance
+    static SolverAztec* New();
+
+    //! destructor
+    ~SolverAztec();
+
+    //@}
+
+    /** @name Accessors
+     */
+    //@{
+
+    double residualNorm() const;
+
+    //@}
+
+    /** @name  Mutators
+     */
+    //@{
+
+    void setMatrix(MSRMatr<value_type> const& m);
+    
+    void setMatrix(CSRMatr<CSRPatt, value_type> const& m);
+
+    //@}
+
+    /** @name  Methods
+     */
+    //@{
+
+    /*
+      solve the problem \f$ A x = b \f$
+
+      \c A has been entered via \c setMatrix .
+
+    */
+    void solve( array_type& x, array_type const& b);
+
+    //@}
+
+    /*! Sets options from data file for this solver.
+     *  @param dataFile GetPot object containing the options from the data file
+     *  @param section section in the GetPot object containing the Aztec stuff
+     *
+     *  @author Christoph Winkelmann
+     */
+    void setOptionsFromGetPot(GetPot const& dataFile,
+                              std::string section = "aztec");
+
+private:
+    //! private method containing code shared by public setMatrix methods
+    void _setMatrix(MSRMatr<value_type> const& m);
+        
+    //! data organisation for C
+    int _data_org[AZ_COMM_SIZE];
+
+    //! processor information
+    int _proc_config[AZ_PROC_SIZE];
+
+    //! array used to select solver options
+    int _options[AZ_OPTIONS_SIZE];
+
+    //! user selected solver parameters
+    double _params[AZ_PARAMS_SIZE];
+
+    //! information returned from AZ_solve() indicating success or failure
+    double _status[AZ_STATUS_SIZE];
+
+    //! aztec matrix
+    AZ_MATRIX* _matrix;
+
+    //! aztec preconditioner
+    AZ_PRECOND* _precond;
+
+    //! number of the next solver to be created
+    static UInt _solverNumber;
+    
+    //! MSRPatt converted from CSRPatt if matrix given as CSRMatr
+    std::auto_ptr<MSRPatt> _tempPattern;
+    
+    //! MSRMatr converted from CSRMatr if given as such
+    std::auto_ptr<MSRMatr<value_type> > _tempMatrix;
+};
+
+}
+#endif /* SolverAztec_H */
