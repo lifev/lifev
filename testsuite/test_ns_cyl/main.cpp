568364f4aa8e0e32b7ef6a6118051b93ccd243f1
moura <moura>
2004-10-04 14:23:24 +0000

    New test for NS using as BC an input vector (BC Vector).

--- testsuite/test_ns_cyl/main.cpp
+++ testsuite/test_ns_cyl/main.cpp
@@ -1,17 +1,17 @@
 /* -*- mode: c++ -*-
-   This program is part of the LifeV library
+   This program is part of the LifeV library 
    Copyright (C) 2001,2002,2003,2004 EPFL, INRIA, Politechnico di Milano
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
    as published by the Free Software Foundation; either version 2
    of the License, or (at your option) any later version.
-
+   
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-
+   
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
@@ -26,73 +26,93 @@
 
 int main(int argc, char** argv)
 {
-    using namespace LifeV;
-    using namespace std;
-    // Reading from data file
-    //
-    GetPot command_line(argc,argv);
-    const char* data_file_name = command_line.follow("data", 2, "-f","--file");
-    GetPot data_file(data_file_name);
+  using namespace LifeV;
+  using namespace std;
+  // Reading from data file 
+  //
+  GetPot command_line(argc,argv);
+  const char* data_file_name = command_line.follow("data", 2, "-f","--file");
+  GetPot data_file(data_file_name);
+ 
+  BC_Handler BCh_u(5);
+ 
+  // Navier-Stokes Solver
+  //
+  NavierStokesSolverPC< RegionMesh3D<LinearTetra> > ns(data_file, feTetraP1bubble, feTetraP1,quadRuleTetra64pt, 
+						       quadRuleTria3pt, quadRuleTetra5pt, quadRuleTria3pt, BCh_u);
+  ns.showMe();
 
 
-    // Boundary conditions definition
-    //
-    BCFunction_Base u_wall(u1);
-    BCFunction_Base in_flow(u2);
-    BC_Handler BCh_u(4);
-    BCh_u.addBC("Wall",   2, Essential, Full, u_wall,  3);
-    BCh_u.addBC("Wall-inflow",   4, Essential, Full, u_wall,  3);
-    BCh_u.addBC("Wall-outflow",   5, Essential, Full, u_wall,  3);
-    BCh_u.addBC("InFlow", 1, Essential,   Full, in_flow, 3);
+  // boundary conditions definition
+  //
+  BCFunction_Base u_wall(u1);
+  BCFunction_Base out_flow(u1);
+
+  // Definition of the vector to impose as BC at inflow
+  Real lambda=-2;
+  UInt dim_lambda = ns.uDof().numTotalDof();
+  Vector vec_lambda(dim_lambda); 
+  BCVector bcvec(vec_lambda,dim_lambda,1); 
+
+  for (UInt i=0 ; i < dim_lambda; ++i) {
+    vec_lambda[i]=-lambda;
+  }
+
+  BCh_u.addBC("Wall",   2, Essential, Full, u_wall,  3);
+  BCh_u.addBC("Wall-inflow",   4, Essential, Full, u_wall,  3);
+  BCh_u.addBC("Wall-outflow",   5, Essential, Full, u_wall,  3);
+  BCh_u.addBC("InFlow", 1, Natural,  Full, bcvec , 3);
+  BCh_u.addBC("OutFlow", 3, Natural,  Full, out_flow, 3);
 
 
 
-    // Navier-Stokes Solver
-    //
-    NavierStokesSolverPC< RegionMesh3D<LinearTetra> > ns(data_file, feTetraP1bubble, feTetraP1,quadRuleTetra64pt,
-                                                         quadRuleTria3pt, quadRuleTetra5pt, quadRuleTria3pt, BCh_u);
-    ns.showMe();
 
 
-    // Initialization
-    //
-    Real dt = ns.timestep();
-    Real startT = ns.inittime();
-    Real T  = ns.endtime();
+  // Initialization
+  //
+  Real dt = ns.timestep();  
+  Real startT = ns.inittime();
+  Real T  = ns.endtime();
 
-    if(startT > 0.0){
-        cout << "initialize velocity and pressure with data from file" << std::endl;
-        ostringstream indexin;
-        string vinname, cinname;
-        indexin << (startT*100);
-        vinname = "fluid.res"+indexin.str();
-        ns.initialize(vinname);}
-    else{
-        cout << "initialize velocity and pressure with u0 and p0" << std::endl;
-        ns.initialize(u0,p0,0.0,dt);
-    }
+  if(startT > 0.0){
+     cout << "initialize velocity and pressure with data from file" << std::endl;
+     ostringstream indexin;
+     string vinname, cinname;
+     indexin << (startT*100);
+     vinname = "fluid.res"+indexin.str();
+     ns.initialize(vinname);}
+  else{
+     cout << "initialize velocity and pressure with u0 and p0" << std::endl;	
+     ns.initialize(u0,p0,0.0,dt);
+  }
 
 
-    // Temporal loop
-    //
-    for (Real time=startT+dt ; time <= T; time+=dt) {
+  Real p;
 
-        ns.timeAdvance(f,time);
-        ns.iterate(time);
+  // Temporal loop
+  //
+  for (Real time=startT+dt ; time <= T; time+=dt) {
+
+    ns.timeAdvance(f,time);
+    ns.iterate(time); 
+    
+    // Calculation of the flux at inflow!
+    cout << "flux  " << ns.flux(1);
+
 
 // ************* saving result on file *****************************************
-        ostringstream indexout;
-        indexout << (time*100);
-        string voutname;
-        voutname = "fluid.res"+indexout.str();
-        fstream Resfile(voutname.c_str(),ios::out | ios::binary);
-        Resfile.write((char*)&ns.u()(1),ns.u().size()*sizeof(double));
-        Resfile.write((char*)&ns.p()(1),ns.p().size()*sizeof(double));
-        Resfile.close();
+    ostringstream indexout;
+    indexout << (time*100);
+    string voutname;
+    voutname = "fluid.res"+indexout.str();
+    fstream Resfile(voutname.c_str(),ios::out | ios::binary);
+    Resfile.write((char*)&ns.u()(1),ns.u().size()*sizeof(double));
+    Resfile.write((char*)&ns.p()(1),ns.p().size()*sizeof(double));
+    Resfile.close();
 
-
-        ns.postProcess();
-    }
-
-    return 0;
+ 
+    ns.postProcess();
+  }
+  
+  return 0;
 }
--- testsuite/test_ns_cyl/ud_functions.hpp
+++ testsuite/test_ns_cyl/ud_functions.hpp
@@ -1,17 +1,17 @@
 /* -*- mode: c++ -*-
-   This program is part of the LifeV library
+   This program is part of the LifeV library 
    Copyright (C) 2001,2002,2003,2004 EPFL, INRIA, Politechnico di Milano
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
    as published by the Free Software Foundation; either version 2
    of the License, or (at your option) any later version.
-
+   
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-
+   
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
@@ -33,7 +33,7 @@ Real u3(const Real& t, const Real& x, const Real& y, const Real& z, const ID& i)
   return 0.1;
 }
 
-// Initial velocity
+// Initial velocity 
 Real u0(const Real& t, const Real& x, const Real& y, const Real& z, const ID& i)
 {
   return 0.0;
@@ -47,7 +47,7 @@ Real p0(const Real& t, const Real& x, const Real& y, const Real& z, const ID& i)
 
 Real u2(const Real& t, const Real& x, const Real& y, const Real& z, const ID& i)
 {
-  Real pi = 3.14159265358979;
+  Real pi = 3.14159265358979;  
   switch(i) {
   case 1:
   case 2:
@@ -55,8 +55,8 @@ Real u2(const Real& t, const Real& x, const Real& y, const Real& z, const ID& i)
     break;
   case 3:
      return 2.0*(sin(pi*t/2.0))*(1.0-4.0*(x*x+y*y));
-     break;
-  }
+     break; 
+  }  
   return 0.0;
 }
 }
