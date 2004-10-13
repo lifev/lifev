fb31f3fa4ff058985e3f1d80a69f5d8bd4d3b4fe
ddipietro <ddipietro>
2004-10-13 09:37:14 +0000

    Some minor modifications were necessary for (solved) run-time problems.

--- life/lifefem/currentBFDG.hpp
+++ life/lifefem/currentBFDG.hpp
@@ -1,11 +1,37 @@
+/* -*- mode: c++ -*-
+   This program is part of the LifeV library
+   Copyright (C) 2001,2002,2003,2004 EPFL, INRIA, Politechnico di Milano
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
 #ifndef _CURRENTBFDG_H
 #define _CURRENTBFDG_H
 
 #include "currentBdDG.hpp"
-#include "bcHandler.hpp"
+#include "bcCond.hpp"
 
 namespace LifeV
 {
+
+/*!
+  \class CurrentBFDG
+  \brief The class for a discontinuous finite element 
+  \author D. A. Di Pietro
+  \date 12/2003
+*/
+
 class CurrentBFDG:public CurrentBdDG{
 #ifdef TEST_PRE
  protected:
@@ -29,7 +55,7 @@ class CurrentBFDG:public CurrentBdDG{
 #endif
 
   template<class GEOELE>
-    void updateBCType(const GEOELE& geoele, const BCHandler& BCh){
+    void updateBCType(const GEOELE& geoele, const BC_Handler& BCh){
     marker = EntityFlag(geoele.marker());
     bcType = BCh.boundaryType(marker);
   }
