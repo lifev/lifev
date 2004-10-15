40ab37db86b918a667b8fc63a61e0c0adc6c640a
winkelma <winkelma>
2004-10-15 15:11:28 +0000

    * added preprocessor flags for variants of stabilization choices * added timing output

--- testsuite/test_ns_ipstab/ipStabilization.hpp
+++ testsuite/test_ns_ipstab/ipStabilization.hpp
@@ -30,6 +30,9 @@
 #ifndef _IPSTABILIZATION_HPP_
 #define _IPSTABILIZATION_HPP_
 
+#define USE_OLD_PARAMETERS 0
+#define WITH_DIVERGENCE 0
+
 namespace LifeV
 {
 
@@ -127,6 +130,10 @@ void IPStabilization<MESH, DOF>::apply( MATRIX& matrix, const VECTOR& state )
         return;
     }
 
+    Chrono chronoBeta;
+    Chrono chronoUpdate;
+    Chrono chronoElemComp;
+    Chrono chronoAssembly;
     const UInt nDof = M_dof.numTotalDof();
 
     // local trace of the velocity
@@ -136,15 +143,20 @@ void IPStabilization<MESH, DOF>::apply( MATRIX& matrix, const VECTOR& state )
     for ( UInt iFace = M_mesh.numBFaces()+1; iFace<= M_mesh.numFaces();
           ++iFace )
     {
+        chronoUpdate.start();
         // update current finite elements
-        M_feBd.updateMeas( M_mesh.face( iFace ) );
+        M_feBd.updateMeasNormal( M_mesh.face( iFace ) );
+        //M_feBd.updateMeas( M_mesh.face( iFace ) );
+        KNM<Real>& normal = M_feBd.normal;
         const Real hK2 = M_feBd.measure();
         const Real hK = sqrt( hK2 );
         const UInt iElAd1 = M_mesh.face( iFace ).ad_first();
         const UInt iElAd2 = M_mesh.face( iFace ).ad_second();
         M_fe1.updateFirstDeriv( M_mesh.volumeList( iElAd1 ) );
         M_fe2.updateFirstDeriv( M_mesh.volumeList( iElAd2 ) );
+        chronoUpdate.stop();
 
+        chronoBeta.start();
         // determine bmax = ||\beta||_{0,\infty,K}
         // first, get the local trace of the velocity into beta
 
@@ -167,92 +179,176 @@ void IPStabilization<MESH, DOF>::apply( MATRIX& matrix, const VECTOR& state )
             if ( bmax < fabs( beta.vec()[ l ] ) )
                 bmax = fabs( beta.vec()[ l ] );
         }
+        chronoBeta.stop();
 
         // pressure stabilization
         if ( M_gammaPress != 0.0 )
         {
+#if USE_OLD_PARAMETERS
             Real coeffPress = M_gammaPress * hK2; // P1, P2 (code)
             //Real coeffPress = M_gammaPress * hK; // P1 p nonsmooth (code)
-            //Real coeffPress = M_gammaPress * hK2 / // Pk (paper)
-            //    std::max<Real>( bmax, M_viscosity/hK );
+#else
+            Real coeffPress = M_gammaPress * hK2 / // Pk (paper)
+                std::max<Real>( bmax, M_viscosity/hK );
+#endif
 
+            chronoElemComp.start();
             ipstab_grad(coeffPress, M_elMatP, M_fe1, M_fe1, M_feBd,
                         nDimensions, nDimensions);
+            chronoElemComp.stop();
+            chronoAssembly.start();
             assemb_mat(matrix, M_elMatP, M_fe1, M_dof,
                        nDimensions, nDimensions);
+            chronoAssembly.stop();
 
             M_elMatP.zero();
+            chronoElemComp.start();
             ipstab_grad(coeffPress, M_elMatP, M_fe2, M_fe2, M_feBd,
                         nDimensions, nDimensions);
+            chronoElemComp.stop();
+            chronoAssembly.start();
             assemb_mat(matrix, M_elMatP, M_fe2, M_dof,
                        nDimensions, nDimensions);
+            chronoAssembly.stop();
 
+            chronoElemComp.start();
             ipstab_grad(-coeffPress, M_elMatP, M_fe1, M_fe2, M_feBd,
                         nDimensions, nDimensions);
+            chronoElemComp.stop();
+            chronoAssembly.start();
             assemb_mat(matrix, M_elMatP, M_fe1, M_fe2, M_dof,
                        nDimensions, nDimensions);
+            chronoAssembly.stop();
 
+            chronoElemComp.start();
             ipstab_grad(-coeffPress, M_elMatP, M_fe2, M_fe1, M_feBd,
                         nDimensions, nDimensions);
+            chronoElemComp.stop();
+            chronoAssembly.start();
             assemb_mat(matrix, M_elMatP, M_fe2, M_fe1, M_dof,
                        nDimensions, nDimensions);
+            chronoAssembly.stop();
         }
 
         // velocity stabilization
         if ( ( M_gammaDiv != 0 || M_gammaBeta != 0 ) && bmax > 0 )
         {
+#if WITH_DIVERGENCE
+#if USE_OLD_PARAMETERS
             Real coeffBeta = M_gammaBeta * hK2 / std::max<Real>(bmax, hK2); // code
-            //Real coeffBeta = M_gammaBeta * hK2 / bmax; // paper
+#else
+            Real coeffBeta = M_gammaBeta * hK2 / bmax; // paper
+#endif
 
             Real coeffDiv = M_gammaDiv * hK2 * bmax; // (code and paper)
             //Real coeffDiv = M_gammaDiv * hK * bmax; // ? (code)
+#else
+            // determine bnmax = ||\beta \cdot n||_{0,\infty,K}
+            // and       bcmax = ||\beta \cross n||_{0,\infty,K}
 
+            chronoBeta.start();
+            Real bnmax = 0;
+            Real bcmax = 0;
+            for ( int iNode=0; iNode<M_feBd.nbNode; ++iNode ) {
+                Real bn = 0;
+                for ( int iCoor=0; iCoor<M_fe1.nbCoor; ++iCoor ) {
+                    bn += normal(iNode, iCoor) *
+                        beta.vec()[ iCoor*M_feBd.nbNode + iNode ];
+                    bcmax = std::max<Real>
+                        (bcmax, normal(iNode, (iCoor+1)%3) *
+                         beta.vec()[ iCoor*M_feBd.nbNode + (iNode+2)%3 ] -
+                         normal(iNode, (iCoor+2)%3) *
+                         beta.vec()[ iCoor*M_feBd.nbNode + (iNode+1)%3 ]);
+                }
+                bnmax = std::max<Real> (bnmax, bn);
+            }
+            chronoBeta.stop();
+
+            Real coeffGrad = hK2 * (M_gammaBeta*bnmax + M_gammaDiv*bcmax);
+#endif
             M_elMatU.zero();
-            //ipstab_grad( coeffDiv, M_elMatU, M_fe1, M_fe1, M_feBd, 0, 0,
-            //nDimensions );
+            chronoElemComp.start();
+#if WITH_DIVERGENCE
             ipstab_bgrad( coeffBeta, M_elMatU, M_fe1, M_fe1, beta,
                           M_feBd, 0, 0, nDimensions );
             ipstab_div( coeffDiv, M_elMatU, M_fe1, M_fe1, M_feBd );
+#else
+            ipstab_grad( coeffGrad, M_elMatU, M_fe1, M_fe1, M_feBd, 0, 0,
+            nDimensions );
+#endif
+            chronoElemComp.stop();
+            chronoAssembly.start();
             for ( UInt iComp = 0; iComp<nDimensions; ++iComp )
                 for ( UInt jComp = 0; jComp<nDimensions; ++jComp )
                     assemb_mat( matrix, M_elMatU, M_fe1, M_dof,
                                 iComp, jComp );
+            chronoAssembly.stop();
 
             M_elMatU.zero();
-            //ipstab_grad( coeffDiv, M_elMatU, M_fe2, M_fe2, M_feBd, 0, 0,
-            //nDimensions );
+            chronoElemComp.start();
+#if WITH_DIVERGENCE
             ipstab_bgrad( coeffBeta, M_elMatU, M_fe2, M_fe2, beta,
                           M_feBd, 0, 0, nDimensions );
             ipstab_div( coeffDiv, M_elMatU, M_fe2, M_fe2, M_feBd );
+#else
+            ipstab_grad( coeffGrad, M_elMatU, M_fe2, M_fe2, M_feBd, 0, 0,
+            nDimensions );
+#endif
+            chronoElemComp.stop();
+            chronoAssembly.start();
             for ( UInt iComp = 0; iComp<nDimensions; ++iComp )
                 for ( UInt jComp = 0; jComp<nDimensions; ++jComp )
                     assemb_mat( matrix, M_elMatU, M_fe2, M_dof,
                                 iComp, jComp );
+            chronoAssembly.stop();
 
             M_elMatU.zero();
-            //ipstab_grad( -coeffDiv, M_elMatU, M_fe1, M_fe2, M_feBd, 0, 0,
-            //             nDimensions );
+            chronoElemComp.start();
+#if WITH_DIVERGENCE
             ipstab_bgrad( -coeffBeta, M_elMatU, M_fe1, M_fe2, beta,
                           M_feBd, 0, 0, nDimensions );
             ipstab_div( -coeffDiv, M_elMatU, M_fe1, M_fe2, M_feBd );
+#else
+            ipstab_grad( -coeffGrad, M_elMatU, M_fe1, M_fe2, M_feBd, 0, 0,
+                         nDimensions );
+#endif
+            chronoElemComp.stop();
+            chronoAssembly.start();
             for ( UInt iComp = 0; iComp<nDimensions; ++iComp )
                 for ( UInt jComp = 0; jComp<nDimensions; ++jComp )
                     assemb_mat( matrix, M_elMatU, M_fe1, M_fe2, M_dof,
                                 iComp, jComp );
+            chronoAssembly.stop();
 
             M_elMatU.zero();
-            //ipstab_grad( -coeffDiv, M_elMatU, M_fe2, M_fe1, M_feBd, 0, 0,
-            //             nDimensions );
+            chronoElemComp.start();
+#if WITH_DIVERGENCE
             ipstab_bgrad( -coeffBeta, M_elMatU, M_fe2, M_fe1, beta,
-                          M_feBd, 0, 0, nDimensions );
+                         M_feBd, 0, 0, nDimensions );
             ipstab_div( -coeffDiv, M_elMatU, M_fe2, M_fe1, M_feBd );
+#else
+            ipstab_grad( -coeffGrad, M_elMatU, M_fe2, M_fe1, M_feBd, 0, 0,
+                         nDimensions );
+#endif
+            chronoElemComp.stop();
+            chronoAssembly.start();
             for ( UInt iComp = 0; iComp<nDimensions; ++iComp )
                 for ( UInt jComp = 0; jComp<nDimensions; ++jComp )
                     assemb_mat( matrix, M_elMatU, M_fe2, M_fe1, M_dof,
                                 iComp, jComp );
+            chronoAssembly.stop();
         }
 
     } // loop on interior faces
+    std::cout << std::endl;
+    std::cout << "Updating of element   done in "
+              << chronoUpdate.diff_cumul()   << "s." << std::endl;
+    std::cout << "Determination of beta done in "
+              << chronoBeta.diff_cumul()     << "s." << std::endl;
+    std::cout << "Element computations  done in "
+              << chronoElemComp.diff_cumul() << "s." << std::endl;
+    std::cout << "Assembly              done in "
+              << chronoAssembly.diff_cumul() << "s." << std::endl;
 
 } // apply(...)
 
