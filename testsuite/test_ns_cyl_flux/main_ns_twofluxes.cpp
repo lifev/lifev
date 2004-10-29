5a0965312a189753e509df85cf65b7c2e42d6beb
vergara <vergara>
2004-10-29 09:50:11 +0000

    main to impose two fluxes generating test_ns_twofluxes. The structure of the algorithm is the same of test_ns_flux, i.e. with temporal loop in NSWithTwoFluxes.hpp

---
+++ testsuite/test_ns_cyl_flux/main_ns_twofluxes.cpp
@@ -0,0 +1,103 @@
+/* -*- mode: c++ -*-
+
+  This file is part of the LifeV library
+
+  Author(s): Christophe Prud'homme <christophe.prudhomme@epfl.ch>
+       Date: 2004-10-12
+
+  Copyright (C) 2004 EPFL, INRIA, Politecnico di Milano
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+/**
+   \file main_ns_twoflux.cpp
+   \author Christophe Prud'homme <christophe.prudhomme@epfl.ch>
+   \date 2004-10-12
+ */
+#include <lifeV.hpp>
+#include <NavierStokesSolverPC.hpp>
+#include <NavierStokesWithTwoFluxes.hpp>
+#include <chrono.hpp>
+#include <ud_functions.hpp>
+#include <GetPot.hpp>
+
+namespace LifeV
+{
+struct toEnsight
+{
+    void
+    operator()( uint __it,
+                RegionMesh3D<LinearTetra> const& __mesh,
+                Vector const& __u,
+                Vector const& __p,
+                Real __flux ) const
+        {
+            std::cout << "============================================================\n"
+                      << "Call ensight writer here to save the data \n"
+                      << "============================================================\n";
+
+        }
+};
+}
+
+int
+main(int argc, char** argv)
+{
+    using namespace LifeV;
+    using namespace std;
+
+    // Reading from data file
+    //
+    GetPot command_line(argc,argv);
+    const char* data_file_name = command_line.follow("data", 2, "-f","--file");
+    GetPot data_file(data_file_name);
+
+    // BC definitions
+    //
+    BCFunctionBase u_wall(u1);
+    BCFunctionBase inflow(uo);
+    BCFunctionBase outflow(u1);
+    BCHandler BCh_u(4);
+    BCh_u.addBC("Wall",   4, Essential, Full, u_wall,  3);                          
+    BCh_u.addBC("InFlow", 1, Natural,   Full, inflow, 3);
+    BCh_u.addBC("OutFlow1", 2, Natural,   Full, outflow, 3);   
+    BCh_u.addBC("OutFlow2", 3, Natural,   Full, outflow, 3);
+
+    // Navier-Stokes Solver
+    //
+    typedef NavierStokesSolverPC< RegionMesh3D<LinearTetra> > ns_type;
+    boost::shared_ptr<ns_type> __ns ( new ns_type(data_file,
+                                                  feTetraP1bubble, feTetraP1,
+                                                  quadRuleTetra15pt,quadRuleTria3pt,
+                                                  quadRuleTetra5pt, quadRuleTria3pt,
+                                                  BCh_u) );
+    __ns->showMe();
+    NavierStokesWithTwoFluxes<ns_type> __ns_with_flux( __ns );
+
+    // Impose the fluxes
+    // 
+    __ns_with_flux.setFlux(1, my_flux_cost); //costant
+    __ns_with_flux.setFlux(2, my_flux_cost2); //costant
+    //__ns_with_flux.setFlux("InFlow", my_flux_cos); //cosinusoidal
+    //__ns_with_flux.setFlux("InFlow", my_flux_physio); // physiological
+
+    toEnsight EnsightFilter;
+    __ns_with_flux.doOnIterationFinish( EnsightFilter  );
+    __ns_with_flux.solve(1,2);
+
+    return EXIT_SUCCESS;
+}
+
+
