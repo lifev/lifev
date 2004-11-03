0af460e1ef8c1371b31f77571a3121e178b6d386
prudhomm <prudhomm>
2004-11-03 08:03:37 +0000

    pressure is a ScalUnknown and not a PhysVectUnknown now we can get rid of the loops for pressure copies

--- life/lifesolver/NavierStokesWithTwoFluxes.hpp
+++ life/lifesolver/NavierStokesWithTwoFluxes.hpp
@@ -191,10 +191,10 @@ NavierStokesWithTwoFluxes<NSSolver>::solve(int label0, int label1)
     PhysVectUnknown<Vector> uprec(_M_solver->uDof().numTotalDof());
     PhysVectUnknown<Vector> uns1(_M_solver->uDof().numTotalDof());
     PhysVectUnknown<Vector> unso1(_M_solver->uDof().numTotalDof());
-    PhysVectUnknown<Vector> unso2(_M_solver->uDof().numTotalDof());   
-    PhysVectUnknown<Vector> pns1(_M_solver->pDof().numTotalDof());
-    PhysVectUnknown<Vector> pnso1(_M_solver->pDof().numTotalDof());
-    PhysVectUnknown<Vector> pnso2(_M_solver->pDof().numTotalDof()); 
+    PhysVectUnknown<Vector> unso2(_M_solver->uDof().numTotalDof());
+    ScalUnknown<Vector> pns1(_M_solver->pDof().numTotalDof());
+    ScalUnknown<Vector> pnso1(_M_solver->pDof().numTotalDof());
+    ScalUnknown<Vector> pnso2(_M_solver->pDof().numTotalDof());
 
     // Initialization
     //
@@ -211,7 +211,7 @@ NavierStokesWithTwoFluxes<NSSolver>::solve(int label0, int label1)
     UInt dim_lambda = _M_solver->uDof().numTotalDof();
     Vector vec_lambda0(dim_lambda);
     Vector vec_lambda1(dim_lambda);
-    BCVector bcvec0(vec_lambda0,dim_lambda,1); 
+    BCVector bcvec0(vec_lambda0,dim_lambda,1);
     BCVector bcvec1(vec_lambda1,dim_lambda,1);
     _M_solver->bcHandler().modifyBC(label0,bcvec0);
     _M_solver->bcHandler().modifyBC(label1,bcvec1);
@@ -219,206 +219,197 @@ NavierStokesWithTwoFluxes<NSSolver>::solve(int label0, int label1)
     // Initialize
     //
     _M_solver->initialize(u0,p0,0.0,dt);
-    
+
     // Temporal loop
     //
     for (Real time=startT+dt ; time <= T; time+=dt)
     {
-      Q[0]=(_M_fluxes[label0])(time);
-      Q[1]=(_M_fluxes[label1])(time);
-      lambda[0]=-Q[0];
-      lambda[1]=-Q[1];
-      std::cout << "imposed flux 0" << " " << Q[0] << "\n";
-      std::cout << "imposed flux 1" << " " << Q[1] << "\n";  
+        Q[0]=(_M_fluxes[label0])(time);
+        Q[1]=(_M_fluxes[label1])(time);
+        lambda[0]=-Q[0];
+        lambda[1]=-Q[1];
+        std::cout << "imposed flux 0" << " " << Q[0] << "\n";
+        std::cout << "imposed flux 1" << " " << Q[1] << "\n";
 
-      // NS1
-      // 
-      // Costruction of vectors vec_lambda
-      //
-      for (UInt i=0 ; i < dim_lambda; ++i) {
-        vec_lambda0[i]=-lambda[0];
-        vec_lambda1[i]=lambda[1];
-      }
-      
-      //std::cout << "\n";
-      std::cout << "start NS1 time" << " " << time << "\n";
-      _M_solver->timeAdvance(f,time);
-      _M_solver->iterate(time);
+        // NS1
+        //
+        // Costruction of vectors vec_lambda
+        //
+        for (UInt i=0 ; i < dim_lambda; ++i) {
+            vec_lambda0[i]=-lambda[0];
+            vec_lambda1[i]=lambda[1];
+        }
 
-      //_M_iteration_finish_signal( ( uint )time*100,
-      //                             _M_solver->mesh(),
-      //                             _M_solver->u(),
-      //                             _M_solver->p(),
-      //                        1 );
+        //std::cout << "\n";
+        std::cout << "start NS1 time" << " " << time << "\n";
+        _M_solver->timeAdvance(f,time);
+        _M_solver->iterate(time);
 
-      
-
-      // Store the solution
-      // 
-      uns1=_M_solver->u();  
-      //pns1=_M_solver->p();
-      for (UInt jj=0 ; jj<_M_solver->pDof().numTotalDof(); jj+=1) {
-        pns1[jj]=_M_solver->p()[jj];
-      }
-
-      
-
-      // Compute the fluxes of NS1
-      //
-      Qn[0]=_M_solver->flux(label0);
-      Qn[1]=_M_solver->flux(label1); 
-      std::cout << "flux 0 before the update" << " " << Qn[0] << "\n";
-      std::cout << "flux 1 before the update" << " " << Qn[1] << "\n"; 
-
-      // GMRes algorithm
-      // 
-      r0[0]=Qn[0]-Q[0];
-      r0[1]=Qn[1]-Q[1];
-      absr0=std::sqrt(r0[0]*r0[0]+r0[1]*r0[1]);
-      v1[0]=r0[0]/absr0;
-      v1[1]=r0[1]/absr0; 
-
-      // NSo1
-      //
-      // Changing BC 
-      //
-      for (UInt i=0 ; i < dim_lambda; ++i) {
-        vec_lambda0[i]=v1[0];
-        vec_lambda1[i]=v1[1];
-      }
-
-      std::cout << "start NSo1 time" << " " << time << "\n";
-      
-      _M_solver->u()=uzero;
-      _M_solver->timeAdvance(f,time);
-      //_M_solver->f()=uzero;
-      _M_solver->u()=uprec;
-      _M_solver->iterate(time);
-      
-
-      // _M_iteration_finish_signal( ( uint )time*100,
-      //                             _M_solver->mesh(),
-      //                            _M_solver->u(),
-      //                            _M_solver->p(),
-      //                       1 );
-     
-      // Store the solution
-      // 
-      unso1=_M_solver->u();  
-      //pnso1=_M_solver->p();
-      for (UInt jj=0 ; jj<_M_solver->pDof().numTotalDof(); jj+=1) {
-        pnso1[jj]=_M_solver->p()[jj];
-      }
-    
-      // Compute the fluxes of NSo1
-      //
-      w1[0]=_M_solver->flux(label0);
-      w1[1]=_M_solver->flux(label1); 
-
-      std::cout << "coeff" << " " << w1[0] << "\n";
-      std::cout << "coeff" << " " << w1[1] << "\n";
-      
-      h[0][0]=w1[0]*v1[0]+w1[1]*v1[1];
-      w1[0]=w1[0]-h[0][0]*v1[0];
-      w1[1]=w1[1]-h[0][0]*v1[1];
-      h[1][0]=std::sqrt(w1[0]*w1[0]+w1[1]*w1[1]);
-      //h[1,0]=w1[0]*w1[0]+w1[1]*w1[1];
-      v2[0]=w1[0]/h[1][0];
-      v2[1]=w1[1]/h[1][0];
-
-      std::cout << "coeff" << " " << h[0][0] << "\n";
-      std::cout << "coeff" << " " << h[1][0] << "\n";
-      std::cout << "coeff" << " " << w1[0] << "\n";
-      std::cout << "coeff" << " " << w1[1] << "\n";
-      std::cout << "coeff" << " " << v1[0] << "\n";
-      std::cout << "coeff" << " " << v1[1] << "\n";
-      std::cout << "coeff" << " " << v2[0] << "\n";
-      std::cout << "coeff" << " " << v2[1] << "\n";
+        //_M_iteration_finish_signal( ( uint )time*100,
+        //                             _M_solver->mesh(),
+        //                             _M_solver->u(),
+        //                             _M_solver->p(),
+        //                        1 );
 
 
-      // NSo2
-      //
-      // Changing BC 
-      //
-      for (UInt i=0 ; i < dim_lambda; ++i) {
-        vec_lambda0[i]=v2[0];
-        vec_lambda1[i]=v2[1];
-      }
 
-      std::cout << "start NSo2 time" << " " << time << "\n";
-      
-      _M_solver->u()=uzero;
-      _M_solver->timeAdvance(f,time); 
-      //_M_solver->f()=uzero; 
-      _M_solver->u()=uprec;
-      _M_solver->iterate(time);
+        // Store the solution
+        //
+        uns1=_M_solver->u();
+        pns1=_M_solver->p();
 
-      //      _M_iteration_finish_signal( ( uint )time*100,
-      //                            _M_solver->mesh(),
-      //                            _M_solver->u(),
-      //                            _M_solver->p(),
-      //                       1 );
 
-      // Store the solution
-      // 
-      unso2=_M_solver->u();  
-      //pnso2=_M_solver->p();
-      for (UInt jj=0 ; jj<_M_solver->pDof().numTotalDof(); jj+=1) {
-        pnso2[jj]=_M_solver->p()[jj];
-      }
 
-      // Compute the fluxes of NSo2
-      //
-      w2[0]=_M_solver->flux(label0);
-      w2[1]=_M_solver->flux(label1); 
-      
-      h[0][1]=w2[0]*v1[0]+w2[1]*v1[1];       
-      w2[0]=w2[0]-h[0][1]*v1[0];
-      w2[1]=w2[1]-h[0][1]*v1[1];
-      h[1][1]=w2[0]*v2[0]+w2[1]*v2[1];       
-      w2[0]=w2[0]-h[1][1]*v2[0];
-      w2[1]=w2[1]-h[1][1]*v2[1];
+        // Compute the fluxes of NS1
+        //
+        Qn[0]=_M_solver->flux(label0);
+        Qn[1]=_M_solver->flux(label1);
+        std::cout << "flux 0 before the update" << " " << Qn[0] << "\n";
+        std::cout << "flux 1 before the update" << " " << Qn[1] << "\n";
 
-      // check
-      //      
-      std::cout << "w2" << " " << w2[0] << "\n";
-      std::cout << "w2" << " " << w2[1] << "\n";
-      std::cout << "coeff" << " " << h[0][0] << "\n";
-      std::cout << "coeff" << " " << h[1][0] << "\n";
-      std::cout << "coeff" << " " << h[0][1] << "\n";
-      std::cout << "coeff" << " " << h[1][1] << "\n";
-      
-      // Update lambda
-      //
-      z[0]=-h[1][1]*absr0/(h[1][0]*h[0][1]-h[0][0]*h[1][1]);
-      z[1]=-h[1][0]*absr0/(h[1][0]*h[0][1]-h[0][0]*h[1][1]);  
-      lambda[0]=lambda[0]+v1[0]*z[0]+v2[0]*z[1];
-      lambda[1]=lambda[1]+v1[1]*z[0]+v2[1]*z[1];
+        // GMRes algorithm
+        //
+        r0[0]=Qn[0]-Q[0];
+        r0[1]=Qn[1]-Q[1];
+        absr0=std::sqrt(r0[0]*r0[0]+r0[1]*r0[1]);
+        v1[0]=r0[0]/absr0;
+        v1[1]=r0[1]/absr0;
 
-      std::cout << "coeff" << " " << z[0] << "\n";
-      std::cout << "coeff" << " " << z[1] << "\n";       
+        // NSo1
+        //
+        // Changing BC
+        //
+        for (UInt i=0 ; i < dim_lambda; ++i) {
+            vec_lambda0[i]=v1[0];
+            vec_lambda1[i]=v1[1];
+        }
 
-      // Update the velocity and the pressure
-      // 
-      _M_solver->u()=uns1-z[0]*unso1-z[1]*unso2;
-      _M_solver->p()=pns1-z[0]*pnso1-z[1]*pnso2;
+        std::cout << "start NSo1 time" << " " << time << "\n";
 
-      // Store the velocity
-      //
-      uprec=_M_solver->u();
+        _M_solver->u()=uzero;
+        _M_solver->timeAdvance(f,time);
+        //_M_solver->f()=uzero;
+        _M_solver->u()=uprec;
+        _M_solver->iterate(time);
 
-      // Save the final solutions
-      // 
-      _M_solver->postProcess();   
 
-      // Compute the fluxes of NS: the definitive one
-      //
-      Qn[0]=_M_solver->flux(label0); 
-      Qn[1]=_M_solver->flux(label1);
-      std::cout << "imposed flux 0" << " " << Q[0] << "\n"; 
-      std::cout << "numerical flux 0" << " " << Qn[0] << "\n";
-      std::cout << "imposed flux 1" << " " << Q[1] << "\n"; 
-      std::cout << "numerical flux 1" << " " << Qn[1] << "\n";
+        // _M_iteration_finish_signal( ( uint )time*100,
+        //                             _M_solver->mesh(),
+        //                            _M_solver->u(),
+        //                            _M_solver->p(),
+        //                       1 );
+
+        // Store the solution
+        //
+        unso1=_M_solver->u();
+        pnso1=_M_solver->p();
+
+        // Compute the fluxes of NSo1
+        //
+        w1[0]=_M_solver->flux(label0);
+        w1[1]=_M_solver->flux(label1);
+
+        std::cout << "coeff" << " " << w1[0] << "\n";
+        std::cout << "coeff" << " " << w1[1] << "\n";
+
+        h[0][0]=w1[0]*v1[0]+w1[1]*v1[1];
+        w1[0]=w1[0]-h[0][0]*v1[0];
+        w1[1]=w1[1]-h[0][0]*v1[1];
+        h[1][0]=std::sqrt(w1[0]*w1[0]+w1[1]*w1[1]);
+        //h[1,0]=w1[0]*w1[0]+w1[1]*w1[1];
+        v2[0]=w1[0]/h[1][0];
+        v2[1]=w1[1]/h[1][0];
+
+        std::cout << "coeff" << " " << h[0][0] << "\n";
+        std::cout << "coeff" << " " << h[1][0] << "\n";
+        std::cout << "coeff" << " " << w1[0] << "\n";
+        std::cout << "coeff" << " " << w1[1] << "\n";
+        std::cout << "coeff" << " " << v1[0] << "\n";
+        std::cout << "coeff" << " " << v1[1] << "\n";
+        std::cout << "coeff" << " " << v2[0] << "\n";
+        std::cout << "coeff" << " " << v2[1] << "\n";
+
+
+        // NSo2
+        //
+        // Changing BC
+        //
+        for (UInt i=0 ; i < dim_lambda; ++i) {
+            vec_lambda0[i]=v2[0];
+            vec_lambda1[i]=v2[1];
+        }
+
+        std::cout << "start NSo2 time" << " " << time << "\n";
+
+        _M_solver->u()=uzero;
+        _M_solver->timeAdvance(f,time);
+        //_M_solver->f()=uzero;
+        _M_solver->u()=uprec;
+        _M_solver->iterate(time);
+
+        //      _M_iteration_finish_signal( ( uint )time*100,
+        //                            _M_solver->mesh(),
+        //                            _M_solver->u(),
+        //                            _M_solver->p(),
+        //                       1 );
+
+        // Store the solution
+        //
+        unso2=_M_solver->u();
+        pnso2=_M_solver->p();
+
+        // Compute the fluxes of NSo2
+        //
+        w2[0]=_M_solver->flux(label0);
+        w2[1]=_M_solver->flux(label1);
+
+        h[0][1]=w2[0]*v1[0]+w2[1]*v1[1];
+        w2[0]=w2[0]-h[0][1]*v1[0];
+        w2[1]=w2[1]-h[0][1]*v1[1];
+        h[1][1]=w2[0]*v2[0]+w2[1]*v2[1];
+        w2[0]=w2[0]-h[1][1]*v2[0];
+        w2[1]=w2[1]-h[1][1]*v2[1];
+
+        // check
+        //
+        std::cout << "w2" << " " << w2[0] << "\n";
+        std::cout << "w2" << " " << w2[1] << "\n";
+        std::cout << "coeff" << " " << h[0][0] << "\n";
+        std::cout << "coeff" << " " << h[1][0] << "\n";
+        std::cout << "coeff" << " " << h[0][1] << "\n";
+        std::cout << "coeff" << " " << h[1][1] << "\n";
+
+        // Update lambda
+        //
+        z[0]=-h[1][1]*absr0/(h[1][0]*h[0][1]-h[0][0]*h[1][1]);
+        z[1]=-h[1][0]*absr0/(h[1][0]*h[0][1]-h[0][0]*h[1][1]);
+        lambda[0]=lambda[0]+v1[0]*z[0]+v2[0]*z[1];
+        lambda[1]=lambda[1]+v1[1]*z[0]+v2[1]*z[1];
+
+        std::cout << "coeff" << " " << z[0] << "\n";
+        std::cout << "coeff" << " " << z[1] << "\n";
+
+        // Update the velocity and the pressure
+        //
+        _M_solver->u()=uns1-z[0]*unso1-z[1]*unso2;
+        _M_solver->p()=pns1-z[0]*pnso1-z[1]*pnso2;
+
+        // Store the velocity
+        //
+        uprec=_M_solver->u();
+
+        // Save the final solutions
+        //
+        _M_solver->postProcess();
+
+        // Compute the fluxes of NS: the definitive one
+        //
+        Qn[0]=_M_solver->flux(label0);
+        Qn[1]=_M_solver->flux(label1);
+        std::cout << "imposed flux 0" << " " << Q[0] << "\n";
+        std::cout << "numerical flux 0" << " " << Qn[0] << "\n";
+        std::cout << "imposed flux 1" << " " << Q[1] << "\n";
+        std::cout << "numerical flux 1" << " " << Qn[1] << "\n";
     }
 }
 }
