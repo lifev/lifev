5b50f8993fa9ca15b4d07ded531c742af9220795
prudhomm <prudhomm>
2005-01-27 11:12:42 +0000

    disable test_nsale as it does not compile

--- testsuite/test_nsale/main.cpp
+++ testsuite/test_nsale/main.cpp
@@ -23,33 +23,33 @@ int main(int argc, char** argv)
   using namespace LifeV;
   using namespace std;
 
-  
+
 #ifdef PVM
   cout << "Je suis au debut du fluid" << endl << flush;
 #endif
-  
-  // Reading from data file 
+
+  // Reading from data file
   //
   GetPot command_line(argc,argv);
-  const char* data_file_name = command_line.follow("data", 2, "-f","--file"); 
+  const char* data_file_name = command_line.follow("data", 2, "-f","--file");
   GetPot data_file(data_file_name);
-  
+
   // Number of boundary conditions for the mesh motion and velocity
-  //  
-  
+  //
+
 #ifdef PVM
   BCHandler BCh_u;
 #else
   BCHandler BCh_u;
 #endif
-  BCHandler BCh_mesh;    
+  BCHandler BCh_mesh;
 
 
 
   // NavierStokes ALE solver
-  NavierStokesAleSolverPC< RegionMesh3D_ALE<LinearTetra> > nsAle(data_file, feTetraP1bubble, 
-								 feTetraP1,quadRuleTetra64pt, 
-								 quadRuleTria3pt, quadRuleTetra64pt, 
+  NavierStokesAleSolverPC< RegionMesh3D_ALE<LinearTetra> > nsAle(data_file, feTetraP1bubble,
+								 feTetraP1,quadRuleTetra64pt,
+								 quadRuleTria3pt, quadRuleTetra64pt,
 								 quadRuleTria3pt, BCh_u,BCh_mesh);
 
   nsAle.showMe();
@@ -57,7 +57,7 @@ int main(int argc, char** argv)
 
   // partie initialisation pour pvm
 #ifdef PVM
-  masterId = pvm_parent(); 
+  masterId = pvm_parent();
   if(masterId==PvmNoParent){
     cout << "I am a poor lonesome job\n";
     exit(1);
@@ -72,54 +72,54 @@ int main(int argc, char** argv)
   cout << " Avant rv " << endl << flush;
   nsAle.rvFromMasterFSI(140);
 #endif
-  
+
 
   // Boundary conditions for the fluid velocity u
-  // 
+  //
   cout << "Main - BC" << endl << flush;
-  
+
   BCFunctionBase bcf(fZero); // BCFunction object holding the null function
- 
 
 
-#ifdef PVM 
-  BCFunctionBase in_flow(g); 
-#endif 
 
-  
+#ifdef PVM
+  BCFunctionBase in_flow(g);
+#endif
+
+
   // BCVector object holding the velocity of the fluid domain
   BCVector u_wall(nsAle.wInterpolated(),nsAle.uDof().numTotalDof());
 
-  BCh_u.addBC("Wall",    1,  Essential, Full, u_wall,  3);  
-  BCh_u.addBC("Wall",   20, Essential, Full, u_wall,   3);  
-  BCh_u.addBC("Wall",   10, Essential, Full, u_wall,   3);  
-  BCh_u.addBC("OutFlow", 3,  Natural,   Full, bcf, 3); 
+  BCh_u.addBC("Wall",    1,  Essential, Full, u_wall,  3);
+  BCh_u.addBC("Wall",   20, Essential, Full, u_wall,   3);
+  BCh_u.addBC("Wall",   10, Essential, Full, u_wall,   3);
+  BCh_u.addBC("OutFlow", 3,  Natural,   Full, bcf, 3);
 
 #ifdef PVM
-  BCh_u.addBC("InFlow", 2,  Natural,   Full, in_flow, 3);  
+  BCh_u.addBC("InFlow", 2,  Natural,   Full, in_flow, 3);
 #else
-  BCh_u.addBC("InFlow", 2,  Natural,   Full, bcf, 3);   
+  BCh_u.addBC("InFlow", 2,  Natural,   Full, bcf, 3);
 #endif
-  
-  
+
+
 
 
   // Boundary conditions for the mesh displacement (harmonic extension)
-  
+
   UInt dim_mesh = nsAle.dofMesh().numTotalDof(); // Number of Degree of Freedom DOF for the displacement of the fluid mesh (number of points
 
   Vector analytic_disp(3*dim_mesh); // The vector holding the displacement of the fluid domain
-  analytic_disp = ZeroVector( analytic_disp.size() ); // initalization to zero 
- 
+  analytic_disp = ZeroVector( analytic_disp.size() ); // initalization to zero
+
 
   BCVector dispbd(analytic_disp,dim_mesh); // The BCVector object holding the displacement of the fluid domain d^s
   vector<ID> compDir(1); // array containg the number of the component corresponding to the axial direction
   compDir[0]=3;
-  BCh_mesh.addBC("Wall", 1, Essential, Full, dispbd, 3);  //  displacement given on the lateral points 
+  BCh_mesh.addBC("Wall", 1, Essential, Full, dispbd, 3);  //  displacement given on the lateral points
   BCh_mesh.addBC("Wall", 20, Essential, Full, dispbd, 3);  //   "       "
   BCh_mesh.addBC("Wall", 10, Essential, Full, dispbd, 3);
-  BCh_mesh.addBC("Axis", 2, Essential, Component, bcf, compDir); // Fixing axial ("3th-component) displacement 
-                                                                 // on the upper and bottom faces 
+  BCh_mesh.addBC("Axis", 2, Essential, Component, bcf, compDir); // Fixing axial ("3th-component) displacement
+                                                                 // on the upper and bottom faces
   BCh_mesh.addBC("Axis", 3, Essential, Component, bcf, compDir); //     "                               "
 
 
@@ -138,10 +138,10 @@ int main(int argc, char** argv)
   int iter=0,istep=1;
   Real time=dt; // pas de temps a regler
 
-do{ 
+do{
 
  cout << "Main : boucle -do-" << endl << flush;
-  
+
 #ifdef PVM
      nsAle.rvFromMasterFSI(110);
 #else
@@ -155,7 +155,7 @@ do{
 #endif
       nsAle.timeAdvance(f,time);
       cout << "PVM's timeAdvance ok!" << endl << flush;
-      // Updating the given imposed displacement at this time step 
+      // Updating the given imposed displacement at this time step
 #ifdef PVM
       nsAle.dep_fluid_interf(analytic_disp);
 #else
@@ -163,46 +163,46 @@ do{
 	if ( nsAle.mesh().point(i+1).marker() == 1 ||
 	     nsAle.mesh().point(i+1).marker() == 20 ||
 	     nsAle.mesh().point(i+1).marker() == 10   ) { // only on lateral points
-	  x=nsAle.mesh().point(i+1).x(); 
+	  x=nsAle.mesh().point(i+1).x();
 	  y=nsAle.mesh().point(i+1).y();
 	  z=nsAle.mesh().point(i+1).z();
-	  for (UInt j=0; j<3; ++j) 
+	  for (UInt j=0; j<3; ++j)
 	    analytic_disp(i + j*dim_mesh) = bdDisp(time, x, y, z, j+1);
 	}
       }
 #endif
       cout << "analytic_disp ok!" << endl << flush;
-      nsAle.updateMesh(time); // compute the harmonic extension d^f and updating the mesh points 
-      nsAle.iterate(time);  
-      
+      nsAle.updateMesh(time); // compute the harmonic extension d^f and updating the mesh points
+      nsAle.iterate(time);
+
 #ifdef PVM
       nsAle.sdToMasterFSI();
 #else
       nsAle.postProcess();
 #endif
-      
+
       time+=dt; //pas de temps a regler
       iter++;
     }
 #ifdef PVM
     if(fsiStatus == 0) {  // on reste au meme pas de temps (iterations internes)
       // depl maillage au pas n-1 ??? a-t-on vraiment besoin ??? NON
-      // Updating the given imposed displacement at this time step 
+      // Updating the given imposed displacement at this time step
       nsAle.dep_fluid_interf(analytic_disp);
       cout << "analytic_disp for the sub-iteration ok!" << endl << flush;
-      nsAle.updateMesh(); // compute the harmonic extension d^f and updating the mesh points 
-      nsAle.iterate();  
+      nsAle.updateMesh(); // compute the harmonic extension d^f and updating the mesh points
+      nsAle.iterate();
       nsAle.sdToMasterFSI();
       istep++;
     }
-#endif 
+#endif
 }
 #ifdef PVM
     while ( 1 ); // c'est le maitre qui decide en renvoyant fsiStatus = -1
 #else
-    while( time<= T  && iter <= max_iter ); 
+    while( time<= T  && iter <= max_iter );
 #endif
 
     return 0;
-} 
+}
 
