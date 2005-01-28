a72363dd9320989f041b779610a6a91e237a6849
ddipietro <ddipietro>
2005-01-28 09:03:14 +0000

    Sorry, I forgot the LGPL header.

--- life/lifesolver/LevelSetSolver_utils.hpp
+++ life/lifesolver/LevelSetSolver_utils.hpp
@@ -1,3 +1,31 @@
+/* -*- mode: c++ -*-
+
+This file is part of the LifeV library
+
+Author(s): Daniele Antonio Di Pietro <dipietro@unibg.it>
+Date: 26-1-2005
+
+Copyright (C) 2005 Università degli Studi di Bergamo
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+/**
+   \file LevelSetHandler_utils.hpp
+   \author Daniele Antonio Di Pietro <dipietro@unibg.it>
+   \date 1-26-2005
+*/
 #ifndef _LEVELSETSOLVERUTILS_H_
 #define _LEVELSETSOLVERUTILS_H_
 
