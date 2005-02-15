39761efa5d38ec7919c925654ba132d1a925e4f6
ddipietro <ddipietro>
2005-02-15 11:20:29 +0000

    Added grad operator allowing for a different space for the velocity field.

--- life/lifefem/elemOper2Fluids.hpp
+++ life/lifefem/elemOper2Fluids.hpp
@@ -1,43 +1,50 @@
 /* -*- mode: c++ -*-
 
-  This file is part of the LifeV library
+This file is part of the LifeV library
 
-  Author(s): Daniele Antonio Di Pietro <dipietro@unibg.it>
-       Date: 2-1-2005
+Author(s): Daniele Antonio Di Pietro <dipietro@unibg.it>
+Date: 2-1-2005
 
-  Copyright (C) 2005 Università degli Studi di Bergamo
+Copyright (C) 2005 Università degli Studi di Bergamo
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
 
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
 
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 /**
    \file elemOper2Fluids.hpp
    \author Daniele A. Di Pietro <dipietro@unibg.it>
    \date 2-1-2005
- */
+*/
 
 #ifndef _ELEMOPER2FLUIDS_H_
 #define _ELEMOPER2FLUIDS_H_
 
-#include "lifeV.hpp"
-#include "elemMat.hpp"
-#include "elemVec.hpp"
-#include "currentFE.hpp"
-#include "currentBdFE.hpp"
-#include "dof.hpp"
+#include <lifeV.hpp>
+#include <elemMat.hpp>
+#include <elemVec.hpp>
+#include <currentFE.hpp>
+#include <currentBdFE.hpp>
+#include <dof.hpp>
 
 namespace LifeV {
+
+    /**
+       \This file contains elementary operators for 2 fluid Navier-Stokes 
+       \problems. The user is warned that the following routines assume that
+       \THE SAME QUADRATURE RULE is used for pressure, velocity and level set
+       \function elements.
+    */
 
     /**
        \Two-fluid mass contribution
@@ -65,6 +72,24 @@ namespace LifeV {
                       const ElemVec& ls_fun_loc, const CurrentFE& fe_ls,
                       ElemMat& elmat, const CurrentFE& fe,
                       int iblock, int jblock);
+
+    /**
+       \Two-fluid driving force
+    */
+
+    template<typename UsrFct>
+    void compute_vec_2f(Real coef1, Real coef2,
+                        const ElemVec& ls_fun_loc, const CurrentFE& fe_ls,
+                        const UsrFct& fct, ElemVec& elvec, const CurrentFE& fe, 
+                        const Real& t, int iblock = 0);
+
+    /**
+       \Gradient operator
+    */
+    void grad( const int icoor, const ElemVec& vec_loc, const CurrentFE& fe_u,
+               ElemMat& elmat,
+               const CurrentFE& fe1, const CurrentFE& fe2,
+               int iblock, int jblock );
 
     // Implementations
 
@@ -210,6 +235,61 @@ namespace LifeV {
             }
         }
     }
-}
 
+    template<typename UsrFct>
+    void compute_vec_2f(Real coef1, Real coef2,
+                        const ElemVec& ls_fun_loc, const CurrentFE& fe_ls,
+                        const UsrFct& fct, ElemVec& elvec, const CurrentFE& fe, 
+                        const Real& t, int iblock = 0) {
+        ElemVec::vector_view vec = elvec.block( iblock );
+        Real s, x, y, z;
+        Real coef_s, ls_fun_on_qn;
+        for (int i = 0;i < fe.nbNode;i++ ) {
+            s = 0;
+            for (int iq = 0;iq < fe.nbQuadPt;iq++ ) {
+                // Evaluate level set function on current quadrature node
+
+                ls_fun_on_qn = 0.;
+                for(int k = 0; k < fe_ls.nbNode; k++)
+                    ls_fun_on_qn += ls_fun_loc(k) * fe_ls.phi(k, iq);
+
+                // Evaluate coefficient on current quadrature node
+
+                coef_s = coef1 * (ls_fun_on_qn > 0) + coef2 * (ls_fun_on_qn < 0);
+                fe.coorQuadPt( x, y, z, iq );
+                s += coef_s * fe.phi( i, iq ) * fct(t, x, y, z, iblock + 1 ) * fe.weightDet( iq );
+            }
+            vec( i ) += s;
+        }
+    }
+
+    void grad( const int icoor, const ElemVec& vec_loc, const CurrentFE& fe_u,
+               ElemMat& elmat,
+               const CurrentFE& fe1, const CurrentFE& fe2,
+               int iblock, int jblock ) {
+        ElemMat::matrix_view mat = elmat.block( iblock, jblock );
+
+        if ( iblock == jblock ) {
+            int iq;
+            int i, j;
+            double s, coef;
+            int nbN1 = fe1.nbNode;
+            for ( i = 0;i < nbN1;i++ ) {
+                for ( j = 0;j < fe2.nbNode;j++ ) {
+                    s = 0;
+                    for ( iq = 0;iq < fe1.nbQuadPt;iq++ ) {
+                        coef = 0;
+                        for (int iloc = 0; iloc < fe_u.nbNode; iloc++ )
+                            coef += vec_loc.vec() [iloc + icoor * fe_u.nbNode] * fe_u.phi(iloc, iq);
+
+                        s += coef * fe2.phi( i, iq ) * fe1.phiDer( j, icoor, iq ) * fe1.weightDet( iq );
+                    } // Loop on quadrature nodes
+
+                    mat( i, j ) += s;
+                } //Loop on j
+            } // Loop on i
+        } // if
+
+    }
+}
 #endif
