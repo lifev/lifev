a55a7be524973da428f862dd24b431d1b07c725c
crosetto <crosetto>
2009-01-06 10:44:29 +0000

    adding tool for serial mesh reordering. Usage: ./test_meshReorder -o output.mesh . The input mesh must be specified in the data file.

---
+++ testsuite/test_meshReorder/main.cpp
@@ -0,0 +1,37 @@
+#include "mpi.h"
+#include <life/lifefilters/medit_wrtrs.hpp>
+#include<life/lifemesh/dataMesh.hpp>
+#include<life/lifecore/GetPot.hpp>
+int main(int argc, char** argv)
+{
+    MPI_Init(&argc, &argv);
+    using namespace LifeV;
+    GetPot command_line(argc,argv);
+    const char* data_file_name = command_line.follow("data", 2, "-f","--file");
+    GetPot data_file(data_file_name);
+    DataMesh<RegionMesh3D<LinearTetra> > mesh_data(data_file, "fluid/discretization");
+    //DataMesh<RegionMesh3D<LinearTetra> > solidData(data_file, "solid/discretization");
+    //const char* mesh_input = command_line.follow(data_file("fluid/discretization/mesh_file", "mesh", 0), 2, "-i","--input");
+    const char* mesh_output = command_line.follow(data_file("fluid/discretization/mesh_file", "mesh", 0), 2, "-o","--output");
+    int numtasks;
+    int me;
+    MPI_Comm_rank(MPI_COMM_WORLD, &me);
+    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+    MPI_Comm MPIcomm;
+    MPI_Group  originGroup, newGroup;
+    MPI_Comm_group(MPI_COMM_WORLD, &originGroup);
+    int members[numtasks];
+    for(ID i=0; i<numtasks; ++i)
+        members[i]=0;
+    int ierr = MPI_Group_incl(originGroup, 1, members, &newGroup);
+
+    MPI_Comm_create(MPI_COMM_WORLD, newGroup, &MPIcomm);
+    if(me==0)
+        {
+            mesh_data.mesh()->orderMesh( MPIcomm);
+            //solidData.mesh()->orderMesh( MPIcomm);
+            writeMesh( mesh_output , *mesh_data.mesh());
+            //writeMesh( "solid_ord.mesh", *solidData.mesh());
+        }
+    MPI_Finalize();
+}
