49cde796fff84ba91a6756c6cad89d6a4183f82f
gfourestey <gfourestey>
2009-01-15 13:27:49 +0000

    removing unnecessary preconditioner creation pointer

--- testsuite/test_monolithic/main.cpp
+++ testsuite/test_monolithic/main.cpp
@@ -305,10 +305,7 @@ namespace LifeV
 
 namespace
 {
-EpetraPreconditioner* createIfpack(){ return new IfpackPreconditioner(); }
 static bool regIF = (PRECFactory::instance().registerProduct( "Ifpack", &createIfpack ));
-
-EpetraPreconditioner* createML(){ return new MLPreconditioner(); }
 static bool regML = (PRECFactory::instance().registerProduct( "ML", &createML ));
 }
 }
