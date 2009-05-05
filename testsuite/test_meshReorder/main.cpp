b4a1dc61759a9946cc27c4e99aae8bfe59d35e1c
crosetto <crosetto>
2009-05-05 09:00:46 +0000

    FSI: new tool in test_meshreorder to create a new marker on a rigion of the fluid-structure interface.
    main input:
     * the new marker that we want to create on the interface of mesh1
     * the marker that determines the same region on mesh2
    
    an exemple is written on the data file

--- testsuite/test_meshReorder/main.cpp
+++ testsuite/test_meshReorder/main.cpp
@@ -4,10 +4,16 @@
 
 
 #include "Epetra_config.h"
+
 #include <life/lifefilters/medit_wrtrs.hpp>
+
 #include<life/lifemesh/dataMesh.hpp>
+
 #include<life/lifecore/GetPot.hpp>
 #include <string>
+
+#include <life/lifefem/dofInterface3Dto3D.hpp>
+#include <life/lifefem/FESpace.hpp>
 
 #ifdef HAVE_MPI
 #include "mpi.h"
@@ -24,27 +30,75 @@ int main(int argc, char** argv)
     DataMesh<RegionMesh3D<LinearTetra> > mesh_data(data_file, "discretization");
     //DataMesh<RegionMesh3D<LinearTetra> > solidData(data_file, "solid/discretization");
     //const char* mesh_input = command_line.follow(data_file("fluid/discretization/mesh_file", "mesh", 0), 2, "-i","--input");
-    std::string mesh_output = command_line.follow(data_file("fluid/discretization/output_mesh_file", "mesh", 0).c_str(), 2, "-o","--output");
-    int numtasks;
-    int me;
-    MPI_Comm_rank(MPI_COMM_WORLD, &me);
-    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
-    MPI_Comm MPIcomm;
-    MPI_Group  originGroup, newGroup;
-    MPI_Comm_group(MPI_COMM_WORLD, &originGroup);
-    int members[numtasks];
-    for(ID i=0; i<numtasks; ++i)
-        members[i]=0;
-    int ierr = MPI_Group_incl(originGroup, 1, members, &newGroup);
 
-    MPI_Comm_create(MPI_COMM_WORLD, newGroup, &MPIcomm);
-    if(me==0)
+    const std::string mesh_output = command_line.follow((data_file("discretization/output_mesh_file", "mesh").c_str()), 2, "-o", "--output");
+    bool ordering = data_file("discretization/ordering", false);
+    bool create_edge = data_file("interface/create_edge", true);
+    UInt newMarker = data_file("interface/edgeMarker", 20);
+
+    if(ordering)
         {
-            mesh_data.mesh()->orderMesh( MPIcomm);
-            //solidData.mesh()->orderMesh( MPIcomm);
-            writeMesh( mesh_output , *mesh_data.mesh());
-            //writeMesh( "solid_ord.mesh", *solidData.mesh());
+            int numtasks;
+            int me;
+            MPI_Comm_rank(MPI_COMM_WORLD, &me);
+            MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+            MPI_Comm MPIcomm;
+            MPI_Group  originGroup, newGroup;
+            MPI_Comm_group(MPI_COMM_WORLD, &originGroup);
+            int members[numtasks];
+            for(ID i=0; i<numtasks; ++i)
+                members[i]=0;
+            int ierr = MPI_Group_incl(originGroup, 1, members, &newGroup);
+
+            MPI_Comm_create(MPI_COMM_WORLD, newGroup, &MPIcomm);
+            if(me==0)
+                {
+                    mesh_data.mesh()->orderMesh( MPIcomm);
+                    //solidData.mesh()->orderMesh( MPIcomm);
+                    writeMesh( mesh_output , *mesh_data.mesh());
+                    //writeMesh( "solid_ord.mesh", *solidData.mesh());
+                }
         }
+    else
+        if(create_edge)
+            {
+                UInt FluidInterfaceFlag           = data_file("interface/fluidInterfaceFlag",      2 );
+                UInt SolidInterfaceFlag           = data_file("interface/solidInterfaceFlag",      2 );
+                EntityFlag edgeFlag               = data_file("interface/edgeFlag",      2 );
+
+                boost::shared_ptr<Epetra_Comm> uselessComm(new Epetra_MpiComm(MPI_COMM_WORLD));
+                DataMesh<RegionMesh3D<LinearTetra> > mesh_data2(data_file, "second_mesh/discretization");
+
+                boost::shared_ptr<FESpace<RegionMesh3D<LinearTetra>, EpetraMap> > firstFESpace;
+                firstFESpace.reset(new FESpace<RegionMesh3D<LinearTetra>, EpetraMap>(mesh_data.mesh(),
+                                                                   feTetraP1,
+                                                                   quadRuleTetra4pt,
+                                                                   quadRuleTria3pt,
+                                                                   3,
+                                                                   *uselessComm));
+
+                boost::shared_ptr<FESpace<RegionMesh3D<LinearTetra>, EpetraMap> > secondFESpace;
+                secondFESpace.reset(new FESpace<RegionMesh3D<LinearTetra>, EpetraMap>(mesh_data2.mesh(),
+                                                                   feTetraP1,
+                                                                   quadRuleTetra4pt,
+                                                                   quadRuleTria3pt,
+                                                                   3,
+                                                                   *uselessComm));
+
+
+                boost::shared_ptr<DofInterface3Dto3D>  dofEdgeFluidToEdgeSolid( new DofInterface3Dto3D );
+
+                dofEdgeFluidToEdgeSolid->setup(firstFESpace->refFE(), firstFESpace->dof(),
+                                               secondFESpace->refFE(), secondFESpace->dof()
+                                               );
+
+                dofEdgeFluidToEdgeSolid->update(
+                                                *mesh_data.mesh(), FluidInterfaceFlag,
+                                                *mesh_data2.mesh(), SolidInterfaceFlag,
+                                                0., &edgeFlag);
+                mesh_data2.mesh()->edgeMarkers(dofEdgeFluidToEdgeSolid->locDofMap(), newMarker);
+                writeMesh( mesh_output , *mesh_data2.mesh());
+            }
     MPI_Finalize();
 #endif
 }
