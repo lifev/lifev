5d58a6bc8a007f7f53661034ab33bc6890525ca4
passerini <passerini>
2009-05-15 18:05:28 +0000

    layout correction

--- life/lifefem/dofOneD.hpp
+++ life/lifefem/dofOneD.hpp
@@ -23,20 +23,20 @@
   \version 1.0
 
   \brief This file contains a very basic one dimensional dof class.
-         The principle is to mimic the behaviour of the "real"
-     Dof class of dof.hpp, without creating it.
-     Thus one can use, with this DofOneD class, the assemblage
-     function which already exists in assemb.hpp.
-     To be used for instance with a mesh handler like BasicOneDMesh.
+  The principle is to mimic the behaviour of the "real"
+  Dof class of dof.hpp, without creating it.
+  Thus one can use, with this DofOneD class, the assemblage
+  function which already exists in assemb.hpp.
+  To be used for instance with a mesh handler like BasicOneDMesh.
 
-     In fact I can't use the "dof::update()" that requires
-     a format of mesh that I did not build. So I turn around
-     this difficulty, and create my own simple DofOneD class.
+  In fact I can't use the "dof::update()" that requires
+  a format of mesh that I did not build. So I turn around
+  this difficulty, and create my own simple DofOneD class.
 
-     \TODO
-     The next improvement would be to construct in lifeV a mesh
-     handler that could also take care of 1D meshes,
-     and then to use the "real" Dof.
+  \TODO
+  The next improvement would be to construct in lifeV a mesh
+  handler that could also take care of 1D meshes,
+  and then to use the "real" Dof.
 
 */
 
@@ -57,29 +57,29 @@ namespace LifeV
 class DofOneD
 {
 public:
-  //! Constructor
-  DofOneD( const UInt& totalDof ): _M_totalDof(totalDof) {}
+    //! Constructor
+    DofOneD( const UInt& totalDof ): _M_totalDof(totalDof) {}
 
-  //! The total number of Dof
-  INLINE UInt numTotalDof() const {return _M_totalDof;}
+    //! The total number of Dof
+    INLINE UInt numTotalDof() const {return _M_totalDof;}
 
-  /*!
-    Returns the global numbering of a DOF, given an element and the local numbering
-    \param ELId the element ID
-    \param localNode the local DOF numbering (starting from 1)
-    \return The numbering of the DOF (starting from 1)
+    /*!
+      Returns the global numbering of a DOF, given an element and the local numbering
+      \param ELId the element ID
+      \param localNode the local DOF numbering (starting from 1)
+      \return The numbering of the DOF (starting from 1)
 
-    BEWARE: works only for 1D mesh with increasing numbering
-    of nodes and elements. (The canonic numbering!)
-    Has only been tested for P1Seg elements! (VM)
-  */
-  ID localToGlobal(const ID ElId, const ID localNode) const {
-    return ElId + localNode;
-  }
+      BEWARE: works only for 1D mesh with increasing numbering
+      of nodes and elements. (The canonic numbering!)
+      Has only been tested for P1Seg elements! (VM)
+    */
+    ID localToGlobal(const ID ElId, const ID localNode) const {
+        return ElId + localNode;
+    }
 
 protected:
-  //! number of total dof
-  UInt _M_totalDof;
+    //! number of total dof
+    UInt _M_totalDof;
 };
 
 }
