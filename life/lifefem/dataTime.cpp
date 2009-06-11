534dd15c53128f215347560852460654b978fae1
malossi <malossi>
2009-06-11 10:22:37 +0000

    A complete restyle of this class. Now it contains all the values for time discretization and has his own get and set functions.

--- life/lifefem/dataTime.cpp
+++ life/lifefem/dataTime.cpp
@@ -16,53 +16,51 @@ You should have received a copy of the GNU Lesser General Public
 License along with this library; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
+/*!
+  \file dataTime.cpp
+
+  \version 1.0
+  \date 01/2003
+  \author M.A. Fernandez
+
+  \version 1.9
+  \date 06/2009
+  \author Cristiano Malossi<cristiano.malossi@epfl.ch>
+
+  \brief File containing a class for handling temporal discretization
+*/
 #include <life/lifefem/dataTime.hpp>
 
 namespace LifeV
 {
-// Constructor
-DataTime::DataTime( const GetPot& dfile, const std::string& section )
+
+// Constructors
+DataTime::DataTime( const GetPot& dfile, const std::string& section ) :
+	M_initialTime	( dfile(( section + "/initialtime" 	).data(), 0.) ),
+	M_endTime		( dfile(( section + "/endtime" 		).data(), 1.) ),
+	M_time			( 0. ),
+	M_timeStep		( dfile(( section + "/timestep" ).data(), 1.) ),
+	M_BDF_order		( dfile(( section + "/BDF_order" ).data(), 1 ) )
 {
-    _dt        = dfile( ( section + "/timestep" ).data(), 1. );
-    _order_bdf = dfile( ( section + "/order_bdf" ).data(), 1 );
 }
 
 DataTime::DataTime(const DataTime& dataTime)
 {
-    _dt        = dataTime._dt;
-    _order_bdf = dataTime._order_bdf;
-}
-
-// Destructor
-DataTime::~DataTime()
-{}
-
-
-// The time step
-Real DataTime::timestep() const
-{
-    return _dt;
-}
-
-
-// setter
-void DataTime::setTimeStep(const Real& dt)
-{
-  _dt = dt;
-}
-
-
-// Order of the bdf formula
-unsigned int DataTime::order_bdf() const
-{
-    return _order_bdf;
+	M_initialTime	= dataTime.M_initialTime;
+	M_endTime		= dataTime.M_endTime;
+	M_time			= dataTime.M_time;
+	M_timeStep		= dataTime.M_timeStep;
+	M_BDF_order		= dataTime.M_BDF_order;
 }
 
 // Output
-void DataTime::showMe( std::ostream& c ) const
+void DataTime::showMe( std::ostream& output ) const
 {
-    // time step
-    c << "timestep  = " << _dt << std::endl;
-    c << "order bdf = " << _order_bdf << std::endl;
+	output << "Initial time = " << M_initialTime	<< std::endl;
+	output << "End time     = " << M_endTime		<< std::endl;
+	output << "Time         = " << M_time			<< std::endl;
+	output << "Timestep     = " << M_timeStep		<< std::endl;
+	output << "BDF order    = " << M_BDF_order  	<< std::endl;
 }
+
 }
--- life/lifefem/dataTime.hpp
+++ life/lifefem/dataTime.hpp
@@ -17,63 +17,104 @@ License along with this library; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 /*!
-  \file dataTime.h
-  \author M.A. Fernandez
-  \date 01/2003
+  \file dataTime.hpp
+
   \version 1.0
+  \date 01/2003
+  \author M.A. Fernandez
 
-  \brief File containing a class for handling temporal discretization with GetPot
+  \version 1.9
+  \date 06/2009
+  \author Cristiano Malossi<cristiano.malossi@epfl.ch>
 
+  \brief File containing a class for handling temporal discretization
 */
+
 #ifndef _DATATIME_H_
 #define _DATATIME_H_
-#include <string>
-#include <iostream>
+
+#include <ostream>
 #include <life/lifecore/GetPot.hpp>
 #include <life/lifecore/life.hpp>
-
 
 namespace LifeV
 {
 
 /*!
-  \class DataTime
-
-  Base class which holds data concerning temporal discretization
-
-*/
-
+ * \class DataTime
+ * \brief Base class which holds data concerning temporal discretization
+ *
+ * @author M.A. Fernandez, Cristiano Malossi
+ * @see
+ */
 class DataTime
 {
 public:
 
-    //! Constructor
-    DataTime( const GetPot& dfile, const std::string& section = "discretization" );
+    /** @name Constructors, Destructor
+     */
+	//@{
+
+    DataTime( const GetPot& dfile, const std::string& section = "time" );
 
     DataTime( const DataTime& dataTime);
-    //! Ouptut
-    virtual void showMe( std::ostream& c = std::cout ) const;
-
-    //! Time step
-    Real timestep() const;
-
-    //! setter
-    void setTimeStep(const Real& dt);
-    void setTime (Real time){ _time = time;}
-    //! getter
-
-    /*const*/ Real time() const {return _time;}
-    //! Order BDF formula
-    unsigned int order_bdf() const;
 
     //! Virtual destructor
-    virtual ~DataTime();
+    virtual ~DataTime() {}
+
+    //@}
+
+
+
+    /** @name Get Functions
+     */
+	//@{
+
+    Real getInitialTime()	const { return M_initialTime; }
+
+    Real getEndTime()		const { return M_endTime; }
+
+    Real getTime()			const { return M_time; }
+
+    Real getTimeStep()		const { return M_timeStep; }
+
+    UInt getBDF_order()		const { return M_BDF_order; }
+
+    //@}
+
+
+
+    /** @name Set Functions
+     */
+	//@{
+
+    void setInitialTime	( const Real& initialTime ) { M_initialTime = initialTime; }
+
+    void setEndTime		( const Real& endTime ) 	{ M_endTime = endTime; }
+
+    void setTime		( const Real& time ) 		{ M_time = time; }
+
+    void setTimeStep	( const Real& timeStep ) 	{ M_timeStep = timeStep; }
+
+    void setBDF_order	( const UInt& BDF_order ) 	{ M_BDF_order = BDF_order; }
+
+    //@}
+
+
+
+    //! Ouptut
+    virtual void showMe( std::ostream& output = std::cout ) const;
 
 private:
 
-    Real         _time;
-    Real         _dt; // time step
-    unsigned int _order_bdf; //order of the time discretization formula
+	Real					M_initialTime;	// initial time
+    Real					M_endTime;		// end time
+    Real					M_time;			// time
+    Real					M_timeStep; 	// time step
+    UInt					M_BDF_order; 	// order of the time discretization formula
+
 };
-}
+
+} // namespace LifeV
+
 #endif
