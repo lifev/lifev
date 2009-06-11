e187972579b3eb248278f8d7a02b5196e012341d
gfourestey <gfourestey>
2009-06-11 16:27:33 +0000

    New trilinos solver interface : Amesos direct solvers ( superlu_dist v2.3 working )

---
+++ life/lifealg/SolverAmesos.cpp
@@ -0,0 +1,375 @@
+/* -*- Mode : c++; c-tab-always-indent: t; indent-tabs-mode: nil; -*-
+
+  <short description here>
+
+  Gilles Fourestey gilles.fourestey@epfl.ch
+
+*/
+/** \file SolverAmesos.cpp
+
+*/
+
+
+
+
+/* -*- mode: c++ -*-
+
+ This file is part of the LifeV library
+
+ Author(s): Gilles Fourestey <christophe.prudhomme@epfl.ch>
+      Date: 2009-06-09
+
+ Copyright (C) 2004 EPFL
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+/**
+   \file SolverAmesos.cpp
+   \author Gilles Fourestey <gilles.fourestey@epfl.ch>
+   \date 2004-08-29
+*/
+
+#include <life/lifecore/debug.hpp>
+
+#include <life/lifecore/GetPot.hpp>
+
+#include <life/lifealg/SolverAmesos.hpp>
+#include "Epetra_Comm.h"
+
+#include <iomanip>
+
+namespace LifeV
+{
+// namespace Epetra
+// {
+
+
+SolverAmesos::SolverAmesos(Epetra_Comm& comm)
+    :
+        //    M_prec                 (),
+        //M_matrix               (),
+    M_factory              (),
+    M_problem              (),
+    M_TrilinosParameterList(),
+    M_redistribute         (true),
+    M_printTiming          (false),
+    M_printStatus          (false),
+    M_Displayer            (comm),
+    M_comm                 (comm)
+{
+}
+
+// SolverAmesos::SolverAmesos():
+//     M_prec                 (),
+//     M_factory              (),
+//     M_TrilinosParameterList(),
+//     M_Displayer            (),
+
+// {
+//     assert(false);
+// }
+
+
+int
+SolverAmesos::NumIters()
+{
+    return 1;
+}
+
+
+double
+SolverAmesos::TrueResidual()
+{
+    return 0.;//M_factory.TrueResidual();
+}
+
+void SolverAmesos::setMatrix(matrix_type& m)
+{
+    //    m.getEpetraMatrix().FillComplete();
+    M_problem.SetOperator(&m.getEpetraMatrix());
+}
+
+void SolverAmesos::setOperator( Epetra_Operator& op)
+{
+
+    //M_factory.SetUserOperator(&op);
+}
+
+//! set Epetra_Operator preconditioner
+// void SolverAmesos::setPreconditioner( prec_type _prec )
+// {
+//     //M_prec = _prec;
+//     return;
+// }
+
+
+void SolverAmesos::SetParameters(bool cerr_warning_if_unused)
+{
+
+}
+
+
+
+void SolverAmesos::setDataFromGetPot( const GetPot& dfile, const std::string& section )
+{
+    M_solverType    = dfile(( section + "/amesos/solvertype").data(), "Superludist");
+    M_redistribute  = dfile(( section + "/amesos/redistribute").data(), true);
+    M_printTiming   = dfile(( section + "/amesos/print_timing").data(), false);
+    M_printStatus   = dfile(( section + "/amesos/print_status").data(), false);
+}
+
+
+void SolverAmesos::setTolMaxiter(const double tol, const int maxiter)
+{
+    if (tol > 0)
+        {
+            M_tol = tol;
+            M_TrilinosParameterList.set("tol", M_tol);
+        }
+
+    if (maxiter >= 0)
+        {
+            M_maxIter = maxiter;
+            M_TrilinosParameterList.set("max_iter", M_maxIter);
+        }
+
+}
+
+void  SolverAmesos::SetVerbose(const VerboseLevel verb)
+{
+    switch (verb) {
+    case NONE:
+        M_TrilinosParameterList.set("output", "none");
+        return;
+    case SUMMARY:
+        M_TrilinosParameterList.set("output", "summary");
+        return;
+    case LAST:
+    default:
+        M_TrilinosParameterList.set("output", "last");
+        return;
+    }
+
+
+}
+
+
+// int
+// SolverAmesos::solve( vector_type& x, vector_type& b )
+// {
+
+
+
+//     //M_factory.SetLHS(&x.getEpetraVector());
+//     //M_factory.SetRHS(&b.getEpetraVector());
+//     //M_Displayer.comm().Barrier();
+
+//     //SetParameters(true);
+
+// //    M_TrilinosParameterList.set("kspace", 100);
+// //    M_TrilinosParameterList.TrilinosParameterListShowMe();
+
+//     //int    maxiter(M_maxIter);
+//     //double mytol  (M_tol);
+//     //int status;
+
+
+//     if ( precSet() )
+// //         {
+//          M_factory.SetPrecOperator(M_prec->getPrec());
+
+//      status = M_factory.Iterate(maxiter, mytol);
+//      //        }
+// //     else
+// //         {
+// //              status = M_factory.AdaptiveIterate(maxiter, 3, mytol);
+// //         }
+//     /* if status:
+//        0  AZ_normal
+//        1  AZ_maxits
+//        if < 0 see AztecOO.cpp
+//     */
+
+// #ifdef DEBUG
+
+//      M_Displayer.comm().Barrier();
+//      M_Displayer.leaderPrint( "  o-  Number of iterations = ", M_factory.NumIters());
+//      M_Displayer.leaderPrint( "  o-  Norm of the true residual = ", M_factory.TrueResidual());
+//      M_Displayer.leaderPrint( "  o-  Norm of the true ratio    = ",  M_factory.ScaledResidual());
+// #endif
+
+//      /* try to solve again (reason may be:
+//        -2 "Aztec status AZ_breakdown: numerical breakdown"
+//        -3 "Aztec status AZ_loss: loss of precision"
+//        -4 "Aztec status AZ_ill_cond: GMRES hessenberg ill-conditioned"
+//      */
+//      if (status <= -2 )
+//      {
+//          maxiter = M_maxIter;
+//          mytol = M_tol;
+//          int olditer = M_factory.NumIters();
+//          status = M_factory.Iterate(maxiter, mytol);
+
+// #ifdef DEBUG
+//          M_Displayer.comm().Barrier();
+//          M_Displayer.leaderPrint( "  o-  Second run: number of iterations = ", M_factory.NumIters());
+//          M_Displayer.leaderPrint( "  o-  Norm of the true residual = ",  M_factory.TrueResidual());
+//          M_Displayer.leaderPrint( "  o-  Norm of the true ratio    = ",  M_factory.ScaledResidual());
+// #endif
+//          return(M_factory.NumIters() + olditer);
+//      }
+
+//      return(M_factory.NumIters());
+
+// }
+
+double
+SolverAmesos::computeResidual( vector_type& x, vector_type& b )
+{
+    vector_type Ax(x.getMap());
+    vector_type res(b);
+
+    //M_matrix->getEpetraMatrix().Apply(x.getEpetraVector(),Ax.getEpetraVector());
+
+    res.getEpetraVector().Update(1, Ax.getEpetraVector(), -1);
+
+    double residual;
+
+    res.Norm2(&residual);
+
+    return residual;
+}
+
+
+
+std::string
+SolverAmesos::printStatus()
+{
+
+
+    // 1) The symbolic factorization
+    //    (parameter doesn't always exist)
+    std::cout << "  Amesos: Total symbolic factorization time " << M_sfact_time << std::endl;
+
+    // 2) The numeric factorization
+    //    (always exists if NumericFactorization() is called)
+    std::cout << "  Amesos: Total numeric factorization time  " << M_nfact_time << std::endl;
+    // 3) Solving the linear system
+    //    (always exists if Solve() is called)
+    std::cout << "  Amesos: Total solve time                  " << M_solve_time << std::endl;
+
+    // 4) Converting the matrix to the accepted format for the solver
+    //    (always exists if SymbolicFactorization() is called)
+    std::cout << "  Amesos: matrix convertion time            " << M_mtx_conv_time << std::endl;
+
+    // 5) Redistributing the matrix for each solve to the accepted format for the solver
+    std::cout << "  Amesos: Total matrix redistribution time  " << M_mtx_redist_time << std::endl;
+
+    // 6) Redistributing the vector for each solve to the accepted format for the solver
+    std::cout << "  Amesos: Total vector redistribution time  " << M_vec_redist_time << std::endl;
+}
+
+
+int SolverAmesos::solveSystem(  vector_type&      rhsFull,
+                                vector_type&      sol,
+                                matrix_ptrtype&   basePrecMatrix,
+                                bool const        reuse,
+                                bool const        retry)
+{
+
+    if (M_comm.MyPID() == 0)
+         std::cout << "       Solving the system ... " << std::endl;
+
+    Amesos_BaseSolver* Solver;
+
+
+    //std::cout << M_problem.GetOperator().FillComplete() << std::end;
+    //Epetra_LinearProblem Problem();
+    //&M_matrix->getEpetraMatrix(), &rhsFull.getEpetraVector(), &sol.getEpetraVector());
+
+    M_problem.SetLHS(&sol.getEpetraVector());
+    M_problem.SetRHS(&rhsFull.getEpetraVector());
+
+    //std::string SolverType = "Klu";
+    Solver = M_factory.Create(M_solverType, M_problem);
+
+    if (Solver == 0) {
+        std::cerr << std::endl  << std::endl;
+        std::cerr << "SolverAmesos: Selected solver << " << M_solverType << " is not available. Bailing out." << std::endl;
+        // return ok not to break the test harness
+#ifdef HAVE_MPI
+        MPI_Finalize();
+#endif
+        exit(EXIT_SUCCESS);
+    }
+
+    Teuchos::ParameterList List;
+
+    List.set("PrintTiming",  M_printTiming);
+    List.set("PrintStatus",  M_printStatus);
+    List.set("Redistribute", M_redistribute);
+
+    Solver->SetParameters(List);
+
+    if (M_comm.MyPID() == 0)
+        std::cout << "          Starting symbolic factorization  ..." << std::flush;
+    Solver->SymbolicFactorization();
+    if (M_comm.MyPID() == 0)
+        std::cout << " ok." << std::endl;
+
+    //std::cout << "m shape = " << Solver->MatrixShapeOK() << std::endl;
+  // you can change the matrix values here
+    if (M_comm.MyPID() == 0)
+        std::cout << "          Starting numeric factorization   ..." << std::flush;
+    Solver->NumericFactorization();
+    if (M_comm.MyPID() == 0)
+        std::cout << " ok." << std::endl;
+
+    // you can change LHS and RHS here
+    if (M_comm.MyPID() == 0)
+        std::cout << "          Starting solution phase          ..." << std::flush;
+    Solver->Solve();
+    if (M_comm.MyPID() == 0)
+        std::cout << " ok." << std::endl;
+
+    // you can get the timings here
+    Teuchos::ParameterList TimingsList;
+    Solver->GetTiming( TimingsList );
+
+    if (M_printTiming) Solver->PrintTiming();
+    if (M_printStatus) Solver->PrintStatus();
+    //printStatus();
+
+    return 0;
+}
+
+void SolverAmesos::setUpPrec(const GetPot& dataFile,  const std::string& section)
+{
+    return;
+}
+//     //     std::string precType = dataFile( (section + "/prectype").data(), "Ifpack");
+
+//     //     M_prec.reset( PRECFactory::instance().createObject( precType ) );
+//     //     ASSERT(M_prec.get() != 0, "Oseen : Preconditioner not set");
+//     //     M_prec->setDataFromGetPot( dataFile, section );
+// }
+
+// void SolverAmesos::setPrec(prec_raw_type* prec)
+// {
+//     return;
+// }
+
+} // namespace LifeV
+
---
+++ life/lifealg/SolverAmesos.hpp
@@ -0,0 +1,214 @@
+/* -*- Mode : c++; c-tab-always-indent: t; indent-tabs-mode: nil; -*-
+
+  <short description here>
+
+  Gilles Fourestey gilles.fourestey@epfl.ch
+
+*/
+/** \file SolverAmesos.hpp
+
+*/
+
+
+
+#ifndef _SolverAmesos_H
+#define _SolverAmesos_H
+
+#include "Amesos.h"
+#include "Amesos_BaseSolver.h"
+
+
+#include "Teuchos_ParameterList.hpp"
+#include "life/lifearray/EpetraVector.hpp"
+#include "life/lifearray/EpetraMatrix.hpp"
+
+// #include "life/lifealg/EpetraPreconditioner.hpp"
+// #include "life/lifealg/IfpackPreconditioner.hpp"
+
+#include <life/lifecore/chrono.hpp>
+#include <life/lifecore/displayer.hpp>
+
+
+
+
+class GetPot;
+
+namespace LifeV
+{
+class SolverAmesos
+{
+public:
+
+
+        /** @name Typedefs
+     */
+    //@{
+
+    typedef double                           value_type;
+
+    typedef SolverAmesos                     solver_type;
+
+    typedef EpetraMatrix<double>             matrix_type;
+    typedef EpetraVector                     vector_type;
+
+    typedef void                             prec_raw_type;
+    typedef void                             prec_type;
+    typedef boost::shared_ptr<matrix_type>   matrix_ptrtype;
+    typedef boost::shared_ptr<EpetraVector>  vector_ptrtype;
+
+    //@}
+
+    /** @name Constructors, destructor
+     */
+    //@{
+
+    //! default constructor
+    //! @param filename GetPot data file containing options for solver in
+    //!        section "aztec"
+    SolverAmesos(Epetra_Comm&);
+
+    //SolverAmesos();
+    //@}
+
+    /** @name Accessors
+     */
+    //@{
+
+    //! Total number of iterations
+    int    NumIters();
+
+    //! True Residual
+    double TrueResidual();
+
+    //@}
+
+    /** @name  Mutators
+     */
+    //@{
+
+    //! set matrix from EpetraMatrix
+    void setMatrix(matrix_type& m);
+
+    void setOperator(Epetra_Operator& op);
+
+    //! set Epetra_Operator preconditioner
+    //void setPreconditioner( prec_type _prec );
+
+    void setDataFromGetPot( const GetPot& dfile, const std::string& section );
+    void SetParameters(bool cerr_warning_if_unused = false);
+
+    void setTolMaxiter(const double tol, const int maxiter=-1);
+
+
+    //! describes the verobisty level
+    enum VerboseLevel { NONE, SUMMARY, LAST };
+    //! sets verbosity level
+    void SetVerbose(const VerboseLevel verb=NONE);
+
+
+    //@}
+
+
+    /** @name  Methods
+     */
+    //@{
+
+    /*
+      solve the problem \f$ A x = b \f$
+
+      \c A has been entered via \c setMatrix .
+
+      return the number of iterations, M_maxIter+1 if solve failed
+
+    */
+    //int solve( vector_type& x, vector_type& b );
+
+    double computeResidual( vector_type& __X, vector_type& __B );
+
+    bool precSet() const {return true;}
+    void precReset() { return; }
+
+
+    // return the Aztec status
+
+
+    std::string printStatus();
+
+//     void getStatus( double status[AZ_STATUS_SIZE])
+//     { M_solver.GetAllAztecStatus( status );}
+
+    //@}
+
+    /** Solves the system and returns the number of iterations.
+        @param  matrFull,
+        @param  rhsFull,
+        @param  sol,
+        @param  prec,
+        @param  reuse,
+        @param  retry = true
+
+        returns number of iterations. If negative, the solver did not converge,
+        the preconditionar has been recomputed, and a second solution is tried
+    */
+    int solveSystem(  vector_type&     rhsFull,
+                      vector_type&     sol,
+                      matrix_ptrtype&  prec,
+                      bool const       reuse,
+                      bool const       retry=true);
+
+    void setUpPrec    (const GetPot& dataFile,  const std::string& section);
+//     void setPrec      (prec_raw_type* prec);
+
+    //prec_type& getPrec(){return 0;}
+
+private:
+
+    //prec_type              M_prec;
+    //matrix_ptrtype         M_matrix;
+
+    Amesos                 M_factory;
+
+    Epetra_LinearProblem   M_problem;
+    //Amesos                M_solver;
+
+    std::string            M_solverType;
+    Teuchos::ParameterList M_TrilinosParameterList;
+    //Teuchos::ParameterList M_timingsList;
+
+
+    // timing variable
+
+    double                 M_sfact_time;
+    double                 M_nfact_time;
+    double                 M_solve_time;
+
+    //
+
+    double                 M_mtx_conv_time;
+    double                 M_mtx_redist_time;
+    double                 M_vec_redist_time;
+
+    //
+
+    bool                   M_redistribute;
+    bool                   M_printTiming;
+    bool                   M_printStatus;
+
+    //
+
+    int                    M_maxIter;
+    double                 M_tol;
+    int                    M_maxIterSolver;
+    int                    M_maxIterForReuse;
+
+    Displayer              M_Displayer;
+    Epetra_Comm&           M_comm;
+
+
+
+};
+
+// } // namespace Epetra
+} // namespace LifeV
+
+#endif /* __SolverAmesos_H */
