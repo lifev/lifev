638e47aea71cd5cac79e4bb8183db14dde666f15
grasseau <grasseau>
2009-07-20 12:42:56 +0000

    Added cases run header

---
+++ testsuite/test_rk/ctRK.hpp
@@ -0,0 +1,78 @@
+#ifndef __CTRK_H
+#define __CTRK_H 1
+
+#include <life/lifecore/application.hpp>
+
+/*!
+ * \class CTRK
+ * \brief Class for running Chorin-Temam methods with RK2 time stepping. 
+ *        Uses opaque pointer to hide implementation of specific case study stuff
+ *        from the run itself. 
+ *
+ * @author
+ * @see
+ */
+
+struct CTRKcaseBase;
+
+class CTRK
+{
+  public:
+
+
+    /** @name Typedefs
+     */
+    //@{
+
+    //@}
+
+    /** @name Constructors, destructor
+     */
+    //@{
+
+    CTRK( int argc,
+        char** argv,
+        LifeV::AboutData const& ad,
+        LifeV::po::options_description const& od );
+
+    ~CTRK()
+        {}
+
+    //@}
+
+    /** @name Operator overloads
+     */
+    //@{
+
+
+    //@}
+
+    /** @name Accessors
+     */
+    //@{
+
+
+    //@}
+
+    /** @name  Mutators
+     */
+    //@{
+
+    //@}
+
+    /** @name  Methods
+     */
+    //@{
+
+    void run();
+
+    //@}
+
+
+  private:
+    Epetra_Comm *M_comm;
+    boost::shared_ptr<CTRKcaseBase> C_case;
+
+};
+
+#endif /* __CTRK_H */
