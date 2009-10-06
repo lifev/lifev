143ec53494f5ba503d581960198e080a47d17757
crosetto <crosetto>
2009-10-06 16:39:53 +0000

    adding a class handling the Anasazi BlockKrylovSchur eigensolver. It works for any Epetra_Operator: in particular in Lifev it shuld be used with the ComposedPreconditioners, that inherit direcly from the Epetra_Operator. Since the class ComposedPreconditioner is templated it can contain both matrices or preconditioners. See Monolithic.cpp for an example using the ComposedPreconditioner.

---
+++ eigenSolver.cpp
@@ -0,0 +1,106 @@
+/* -*- mode: c++ -*-
+
+  This file is part of the LifeV library
+
+  Author(s): Paolo Crosetto <paolo.crosetto@epfl.ch>
+       Date: 2009-03-25
+
+  Copyright (C) 2009 EPFL
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+/**
+   \file eigenSolver.cpp
+   \author Paolo Crosetto <paolo.crosetto@epfl.ch>
+   \date 2009-03-25
+ */
+
+//#include <life/lifecore/GetPot.hpp>
+#include <lifemc/lifealg/eigenSolver.hpp>
+#include <string>
+#include "Teuchos_RefCountPtrDecl.hpp"
+#include "Teuchos_RCPBoostSharedPtrConversions.hpp"
+
+namespace LifeV
+{
+EigenSolver::EigenSolver(boost::shared_ptr<Solver> const matrix, Epetra_BlockMap const& block_map, long unsigned int numvec):
+    MyProblem( new Anasazi::BasicEigenproblem<DataType, Vector, Solver>(Teuchos::rcp(matrix), M_eigenVectors) ),
+    M_eigenVectors(new Epetra_MultiVector(block_map, numvec)),
+    MyPL(),
+    MySolver()
+{
+}
+
+
+
+void
+EigenSolver::setDataFromGetPot(GetPot const& data , const std::string& section)
+{
+    int block_size = data((section + "block_size").c_str(), 1);
+    int max_blocks = data((section + "max_blocks").c_str(), 20);
+    int max_restarts = data((section + "max_restarts").c_str(), 50);
+    double tol = data((section + "tol").c_str(), 1e-8);
+    std::string which = data((section + "which").c_str(), "ML");
+    int nev = data((section + "neval").c_str(), 10);//number of eigenvalues
+    bool verbose = data((section + "verbose").c_str(), true);
+    bool debug = data((section + "debug").c_str(), true);
+
+
+    int verbosity = Anasazi::Errors + Anasazi::Warnings;
+    if (verbose) {
+        verbosity += Anasazi::FinalSummary + Anasazi::TimingDetails;
+    }
+    if (debug) {
+        verbosity += Anasazi::Debug;
+    }
+  //
+  // Create parameter list to pass into solver manager
+  //
+
+    MyPL.set( "Verbosity", verbosity );
+    MyPL.set( "Block Size", block_size );
+    MyPL.set( "Max Blocks", max_blocks );
+    MyPL.set( "Max Restarts", max_restarts );
+    MyPL.set( "Tol", tol );
+    //Teuchos::RCP<Anasazi::SortManager<Real> > MySort = Teuchos::rcp( new Anasazi::BasicSort<Real>( which.c_str() ) );
+    //MyPL.set( "Sort Manager", MySort);
+
+    MyProblem->setNEV(nev);
+}
+
+
+void
+EigenSolver
+::eigenvalues(std::vector<DataType>& realPart, std::vector<DataType>& imgPart)
+{
+    Anasazi::Eigensolution<DataType,Vector> sol = MyProblem->getSolution();
+    std::vector<Anasazi::Value<DataType> > evals = sol.Evals;
+    for(int i=0; i<evals.size(); ++i)
+        {
+            realPart.push_back(evals[i].realpart);
+            imgPart.push_back(evals[i].imagpart);
+        }
+}
+
+ int
+ EigenSolver
+ ::solve()
+ {
+     bool out=MyProblem->setProblem();
+     std::cout<<"problem set: "<<out<<std::endl;
+     MySolver.reset(new eigensolver_raw_type(MyProblem, MyPL));
+     return MySolver->solve();
+ }
+}
---
+++ eigenSolver.hpp
@@ -0,0 +1,116 @@
+/* -*- mode: c++ -*-
+
+  This file is part of the LifeV library
+
+  Author(s): Paolo Crosetto <paolo.crosetto@epfl.ch>
+       Date: 2009-03-25
+
+  Copyright (C) 2009 EPFL
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+/**
+   \file eigenSolver.hpp
+   \author Paolo Crosetto <paolo.crosetto@epfl.ch>
+   \date 2009-03-25
+ */
+
+
+/**
+class handling the Anasazi BlockKrylovSchur eigensolver. It works for any Epetra_Operator: in particular in Lifev it shuld be used with the ComposedPreconditioners, that inherits direcly from the Epetra_Operator. Since the class ComposedPreconditioner is templated it can contain both matrices or preconditioners. See Monolithic.cpp for an example using the ComposedPreconditioner.
+ */
+#ifndef EIGENSOLVER_HPP
+#define EIGENSOLVER_HPP
+
+//#include <life/lifecore/GetPot.hpp>
+#include "Epetra_MultiVector.h"
+#include "Epetra_SerialDenseVector.h"
+#include "Epetra_Operator.h"
+#include "Epetra_CrsMatrix.h"
+
+#include "Teuchos_RefCountPtrDecl.hpp"
+
+#include "AnasaziBlockDavidson.hpp"
+#include "AnasaziLOBPCG.hpp"
+#include "AnasaziBasicOutputManager.hpp"
+#include "AnasaziBasicSort.hpp"
+#include "AnasaziConfigDefs.hpp"
+#include "AnasaziBasicEigenproblem.hpp"
+#include "AnasaziBlockKrylovSchurSolMgr.hpp"
+#include "AnasaziLOBPCGSolMgr.hpp"
+#include "AnasaziEpetraAdapter.hpp"
+
+#include <cstdlib>
+#include <boost/shared_ptr.hpp>
+
+#include <life/lifecore/life.hpp>
+#include <life/lifecore/GetPot.hpp>
+
+namespace LifeV
+{
+
+class UNDEF_EIGENSOLVER_EXCEPTION;
+
+// namespace Epetra
+// {
+
+//  template <typename DataType, typename Solver, typename Vector>
+  class EigenSolver
+  {
+  public:
+      typedef double DataType;
+      typedef Epetra_Operator Solver;
+      typedef Epetra_MultiVector Vector;
+
+      typedef Anasazi::BasicEigenproblem<DataType, Vector, Solver>                     eigenpb_raw_type;
+      typedef Teuchos::RCP<Anasazi::BasicEigenproblem<DataType, Vector, Solver> >      eigenpb_type;
+      typedef Anasazi::BlockKrylovSchurSolMgr <DataType,Vector,Solver>                 eigensolver_raw_type;
+      typedef boost::shared_ptr<eigensolver_raw_type>                                  eigensolver_type;
+
+      EigenSolver(boost::shared_ptr<Solver> const matrix, Epetra_BlockMap const& block_map, long unsigned int numvec);
+
+      virtual ~EigenSolver()
+      {}
+
+      /**sets the parameters for the eigenproblem:
+       */
+      void setDataFromGetPot( GetPot const& dataFile, std::string const& section/*="eigensolver"*/ );
+
+      /** set to true if the operator is symmetric*/
+      void setHermitian(bool flag){ MyProblem->setHermitian(flag);}
+
+      /** fills the input vectors with the real and imaginary part of the eigenvalues*/
+      void eigenvalues(std::vector< DataType>& realPart, std::vector< DataType>& imgPart);
+
+      /** solves the eigenproblem*/
+      int solve();
+
+
+  private :
+
+      Teuchos::RCP<Vector> M_eigenVectors;
+      eigenpb_type MyProblem;
+      Teuchos::ParameterList MyPL;
+      eigensolver_type MySolver;
+  };
+
+class UNDEF_EIGENSOLVER_EXCEPTION{
+public:
+    UNDEF_EIGENSOLVER_EXCEPTION(){}
+    virtual ~UNDEF_EIGENSOLVER_EXCEPTION(){}
+};
+
+}
+#endif
