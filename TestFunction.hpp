096d3f62f3fd2d70c38fd723b2068d72794e513d
malossi <malossi>
2009-10-15 14:49:22 +0000

    Added a test for the containerOfVectors. Look at it as a tutorial

---
+++ TestFunction.hpp
@@ -0,0 +1,194 @@
+/* -*- mode: c++ -*-
+
+  This file is part of the LifeV Applications.
+
+  Author(s): Cristiano Malossi <cristiano.malossi@epfl.ch>
+       Date: 2009-10-06
+
+  Copyright (C) 2009 EPFL
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2.1 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+  USA
+*/
+/**
+   \file TestFunction.hpp
+   \author Cristiano Malossi <cristiano.malossi@epfl.ch>
+   \date 2009-10-06
+ */
+
+#include <iomanip>
+#include <string>
+
+#include <life/lifecore/life.hpp>
+
+#include <lifemc/lifearray/ContainerOfVectors.hpp>
+
+#include <boost/shared_ptr.hpp>
+
+
+
+using namespace LifeV;
+
+template < class VectorType >
+UInt
+TestFunction( boost::shared_ptr<VectorType> A1, boost::shared_ptr<VectorType> B1,
+              boost::shared_ptr<VectorType> A2, boost::shared_ptr<VectorType> B2,
+              boost::shared_ptr<VectorType> A3, boost::shared_ptr<VectorType> B3,
+              boost::shared_ptr<VectorType> A4, boost::shared_ptr<VectorType> B4 )
+{
+	//CONTAINER OF BASE VECTORS
+	typedef ContainerOfVectors< VectorType >             ContainerOfBaseVectors;
+	typedef boost::shared_ptr<ContainerOfBaseVectors>    ContainerOfBaseVectors_ptr;
+
+	ContainerOfBaseVectors_ptr V1, V2, V3, V4;
+	Real scalar = 1.0;
+
+	V1.reset( new ContainerOfBaseVectors() );
+	V1->push_back( A1 );
+	V1->push_back( B1 );
+	std::cout << "V1" << std::endl;
+	V1->ShowMe();
+
+	V2.reset( new ContainerOfBaseVectors() );
+	V2->push_back( A2 );
+	V2->push_back( B2 );
+	std::cout << "V2" << std::endl;
+	V2->ShowMe();
+
+    V3.reset( new ContainerOfBaseVectors() );
+    V3->push_back( A3 );
+    V3->push_back( B3 );
+    std::cout << "V3" << std::endl;
+    V3->ShowMe();
+
+    V4.reset( new ContainerOfBaseVectors() );
+    V4->push_back( A4 );
+    V4->push_back( B4 );
+    std::cout << "V4" << std::endl;
+    V4->ShowMe();
+
+	// Test operator= (initialize a vector as a copy of another vector)
+	ContainerOfBaseVectors_ptr VV1, VV2, VV3;
+	VV1.reset( new ContainerOfBaseVectors() );
+	*VV1 = *V1;
+	std::cout << "VV1 = V1" << std::endl;
+	VV1->ShowMe();
+
+	// Test operator= (for initialize the vector with a scalar - cannot be done for an empty vector!)
+	*VV1 = scalar;
+	std::cout << "VV1 = 1.0" << std::endl;
+	VV1->ShowMe();
+
+	// Test operator+=
+	*VV1 += *V1;
+	std::cout << "VV1 += V1" << std::endl;
+	VV1->ShowMe();
+
+	// Test operator-
+	*VV1 = *VV1 - *V2;
+	std::cout << "VV1 = VV1 - V2" << std::endl;
+	VV1->ShowMe();
+
+	// Test operator*= (multiplication: scalar * vector)
+	scalar = 1.23456789;
+	*VV1 = *V1;
+	*VV1 = scalar * *V1;
+	std::cout << "VV1 = 1.23456789 * V1" << std::endl;
+	VV1->ShowMe();
+
+	// Test operator* (scalar product multiplication)
+	scalar = VV1->Dot( *V1 );
+	std::cout << "scalarProduct = VV1.Dot(V1) = " << scalar << std::endl << std::endl;
+
+	// Concatenate two vector of vectors
+	VV1->push_back( *V1 );
+	VV1->push_back( *V2 );
+	std::cout << "VV1->push_back( V1 ); VV1->push_back( V2 )" << std::endl;
+	VV1->ShowMe();
+
+	VV2.reset( new ContainerOfBaseVectors() );
+    VV2->push_back( *V3 );
+    VV2->push_back( *V4 );
+    VV2->push_back( *V3 );
+    std::cout << "VV2->push_back( V3 ); VV2->push_back( V4 ); VV2->push_back( V3 )" << std::endl;
+    VV2->ShowMe();
+
+    // Element by element Multiplication
+	std::cout << "VV2 *= VV1" << std::endl;
+    *VV2 *= *VV1;
+    VV2->ShowMe();
+    std::cout << "VV1 =" << std::endl;
+    VV1->ShowMe();
+
+    // Element by element Division
+    std::cout << "VV2 /= VV1" << std::endl;
+    *VV2 /= *VV1;
+    VV2->ShowMe();
+    std::cout << "VV1 =" << std::endl;
+    VV1->ShowMe();
+
+    // Element by element Multiplication (NaN is interpreted as a zero)
+    std::cout << "VV2 && VV1" << std::endl;
+    (*VV2 && *VV1).ShowMe();
+
+    // Element by element Comparison ==
+    std::cout << "VV1 == 0" << std::endl;
+    VV3.reset( new ContainerOfBaseVectors() );
+    *VV3 = ( *VV1 == 0.0 );
+    VV3->ShowMe();
+
+    // Element by element Comparison >=
+    std::cout << "VV1 >= 2.2" << std::endl;
+    *VV3 = ( *VV1 >= 2.2 );
+    VV3->ShowMe();
+
+    // Abs
+    std::cout << "VV2 *= -1" << std::endl;
+    scalar = -1.0;
+    *VV2 *= scalar;
+    VV2->ShowMe();
+
+    std::cout << "VV2->abs( *VV3 )" << std::endl;
+    VV2->Abs( *VV3 );
+    std::cout << "VV2:" << std::endl;
+    VV2->ShowMe();
+    std::cout << "VV3:" << std::endl;
+    VV3->ShowMe();
+
+
+
+
+
+
+
+    // Replace a vector
+    UInt pos = 2;
+    (*VV1)( pos ) = B1;
+    std::cout << "VV1( 2 ) = B1" << std::endl;
+    VV1->ShowMe();
+
+    // Compute the weight norm2 of the vector
+    VV1->WeightNorm2();
+    std::cout << "WeightNorm2(VV1) = " << VV1->WeightNorm2() << std::endl << std::endl;
+
+    // operator!
+    std::cout << "!VV1 " << std::endl;
+    (!(*VV1)).ShowMe();
+
+    std::cout << "!VV3 " << std::endl;
+    (!(*VV3)).ShowMe();
+
+	return EXIT_SUCCESS;
+}
---
+++ main.cpp
@@ -0,0 +1,197 @@
+/* -*- mode: c++ -*-
+
+  This file is part of the LifeV Applications.
+
+  Author(s): Cristiano Malossi <cristiano.malossi@epfl.ch>
+       Date: 2009-04-22
+
+  Copyright (C) 2009 EPFL
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2.1 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+  USA
+*/
+/**
+   \file main.cpp
+   \author Cristiano Malossi <cristiano.malossi@epfl.ch>
+   \date 2009-09-29
+ */
+
+#include "Epetra_ConfigDefs.h"
+#ifdef HAVE_MPI
+	#include <mpi.h>
+#endif
+
+#include <iomanip>
+#include <string>
+
+#include <life/lifecore/application.hpp>
+#include <life/lifecore/life.hpp>
+
+#include <life/lifealg/EpetraMap.hpp>
+#include <life/lifearray/EpetraVector.hpp>
+
+#include <boost/numeric/ublas/vector.hpp>
+#include <boost/shared_ptr.hpp>
+
+#include "TestFunction.hpp"
+
+
+
+LifeV::AboutData
+makeAbout()
+{
+    LifeV::AboutData about( "ContainerOfVectors Test" ,
+                            "A ContainerOfVectors tester" ,
+                            "1.0",
+                            "Some practical examples about how the ContainerOfVectors is working!",
+                            LifeV::AboutData::License_GPL,
+                            "Copyright (c) 2009 EPFL");
+
+    about.addAuthor("Cristiano Malossi", "Developer", "cristiano.malossi@epfl.ch", "");
+    return about;
+}
+
+
+
+using namespace LifeV;
+
+int
+main( int argc, char** argv )
+{
+	#ifdef HAVE_MPI
+		std::cout << "MPI Initialization" << std::endl;
+		MPI_Init( &argc, &argv );
+	#endif
+
+	// ===================================================
+	//! TEST: LIFEV::EPETRAVECTOR
+	// ===================================================
+
+	//Setup main communicator
+	boost::shared_ptr<Epetra_Comm>	comm;
+
+	int	nprocs;
+	int rank;
+
+	//MPI Preprocessing
+	#ifdef EPETRA_MPI
+
+		MPI_Comm_size( MPI_COMM_WORLD, &nprocs );
+		MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+
+		if ( rank == 0 )
+		{	std::cout << "MPI processes: " << nprocs << std::endl;
+
+			std::cout << "MPI Epetra Initialization ... " << std::endl;
+		}
+		comm.reset( new Epetra_MpiComm( MPI_COMM_WORLD ) );
+
+		comm->Barrier();
+
+	#else
+
+		std::cout << "MPI SERIAL Epetra Initialization ... " << std::endl;
+		comm.reset( new Epetra_SerialComm() );
+	#endif
+
+    std::cout << std::setprecision(15) << std::endl;
+
+	//BASE VECTORS
+	typedef EpetraVector                        Vector;
+	typedef boost::shared_ptr<Vector>           Vector_ptr;
+
+	int MyGlobalIElementsA[3],MyGlobalIElementsB[2];
+	MyGlobalIElementsA[0] = 0;  MyGlobalIElementsA[1] = 1;  MyGlobalIElementsA[2] = 2;
+	MyGlobalIElementsB[0] = 0;  MyGlobalIElementsB[1] = 1;
+
+	EpetraMap mapA( 3, 3, &MyGlobalIElementsA[0], 0, *comm);
+	EpetraMap mapB( 2, 2, &MyGlobalIElementsB[0], 0, *comm);
+
+	Vector_ptr A1, B1, A2, B2, A3, B3, A4, B4;
+	A1.reset( new Vector( mapA, Unique ) );
+	B1.reset( new Vector( mapB, Unique ) );
+	A2.reset( new Vector( mapA, Unique ) );
+	B2.reset( new Vector( mapB, Unique ) );
+	A3.reset( new Vector( mapA, Unique ) );
+	B3.reset( new Vector( mapB, Unique ) );
+    A4.reset( new Vector( mapA, Unique ) );
+    B4.reset( new Vector( mapB, Unique ) );
+
+	//Fill the vectors
+	for ( UInt i(0) ; i < static_cast<UInt> (A1->size()) ; ++i )
+		(*A1)[i] = i;
+	for ( UInt i(0) ; i < static_cast<UInt> (B1->size()) ; ++i )
+		(*B1)[i] = A1->size() + i;
+
+	for ( UInt i(0) ; i < static_cast<UInt> (A2->size()) ; ++i )
+		(*A2)[i] = i*10;
+	for ( UInt i(0) ; i < static_cast<UInt> (B2->size()) ; ++i )
+		(*B2)[i] = (A2->size() + i)*10;
+
+    for ( UInt i(0) ; i < static_cast<UInt> (A3->size()) ; ++i )
+        (*A3)[i] = i*10;
+    for ( UInt i(0) ; i < static_cast<UInt> (B3->size()) ; ++i )
+        (*B3)[i] = (A3->size() + i)*100;
+
+    for ( UInt i(0) ; i < static_cast<UInt> (A4->size()) ; ++i )
+        (*A4)[i] = i*10;
+    for ( UInt i(0) ; i < static_cast<UInt> (B4->size()) ; ++i )
+        (*B4)[i] = (A4->size() + i)*1000;
+
+	UInt result=TestFunction( A1, B1, A2, B2, A3, B3, A4, B4 );
+
+
+
+	// ===================================================
+	//! TEST: BOOST::NUMERIC::UBLAS::VECTOR
+	// ===================================================
+
+	typedef boost::numeric::ublas::vector<Real> VectorBOOST;
+	typedef boost::shared_ptr<VectorBOOST>      VectorBOOST_ptr;
+
+	// BASE Vectors
+	VectorBOOST_ptr A1_BOOST, B1_BOOST, A2_BOOST, B2_BOOST;
+
+	A1_BOOST.reset( new VectorBOOST(2) );
+	(*A1_BOOST)[0] = 0;
+	(*A1_BOOST)[1] = 1;
+
+	B1_BOOST.reset( new VectorBOOST(3) );
+	(*B1_BOOST)[0] = 2;
+	(*B1_BOOST)[1] = 3;
+	(*B1_BOOST)[2] = 4;
+
+	A2_BOOST.reset( new VectorBOOST(2) );
+	(*A2_BOOST)[0] = 0;
+	(*A2_BOOST)[1] = 10;
+
+	B2_BOOST.reset( new VectorBOOST(3) );
+	(*B2_BOOST)[0] = 20;
+	(*B2_BOOST)[1] = 30;
+	(*B2_BOOST)[2] = 40;
+
+	// NOTE:
+	// To perform a test with BOOST we first have to implement a derived class
+	// from ContainerOfVectors in which re-implement operator= and operator*
+
+	//TestFunction( A1_BOOST, B1_BOOST, A2_BOOST, B2_BOOST );
+
+	#ifdef HAVE_MPI
+		std::cout << std::endl << "MPI Finalization" << std::endl;
+		MPI_Finalize();
+	#endif
+
+	return( result );
+}
