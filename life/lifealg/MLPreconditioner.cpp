383c7f48af993a57682dd84a6dbcc096b8a9f14f
passerini <passerini>
2009-11-04 05:11:21 +0000

    corrected "typo" (triple argument to GetPot operator)

--- life/lifealg/MLPreconditioner.cpp
+++ life/lifealg/MLPreconditioner.cpp
@@ -58,8 +58,8 @@ void
 MLPreconditioner::setDataFromGetPot( const GetPot&          dataFile,
                                      const std::string&     section)
 {
-    bool found;
-    M_analyze = dataFile((section + "/ML/analyze_smoother").data(), false, found);
+    bool found(false);
+    M_analyze = dataFile((section + "/ML/analyze_smoother").data(), found);
 
     createMLList(dataFile, section, M_List);
 
