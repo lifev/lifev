e9dfaed77d16119c364de3d34c318d65b7820da2
malossi <malossi>
2009-11-19 18:17:14 +0000

    Test multiscale updated

--- testsuite/test_MultiScale/main.cpp
+++ testsuite/test_MultiScale/main.cpp
@@ -1,39 +1,64 @@
-/* -*- mode: c++ -*-
+//@HEADER
+/*
+************************************************************************
 
-  This file is part of the LifeV Applications.
+ This file is part of the LifeV Applications.
+ Copyright (C) 2001-2009 EPFL, Politecnico di Milano, INRIA
 
-  Author(s): Cristiano Malossi <cristiano.malossi@epfl.ch>
-       Date: 2009-03-12
+ This library is free software; you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as
+ published by the Free Software Foundation; either version 2.1 of the
+ License, or (at your option) any later version.
 
-  Copyright (C) 2009 EPFL
+ This library is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
 
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2.1 of the License, or
-  (at your option) any later version.
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ USA
 
-  This program is distributed in the hope that it will be useful, but
-  WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-  USA
+************************************************************************
 */
-/**
-   \file main.cpp
-   \author Cristiano Malossi <cristiano.malossi@epfl.ch>
-   \date 2009-03-12
+//@HEADER
+
+/*!
+ * @file
+ * @brief MultiScale Test
+ *
+ * @author Cristiano Malossi <cristiano.malossi@epfl.ch>
+ * @date 12-03-2009
+ *
+ *  This is a very general main file to run a MultiScale simulation.
+ *
+ *  Models available:
+ *  <ol>
+ *      <li> Fluid3D (Oseen)
+ *      <li> MultiScale
+ *  </ol>
+ *
+ *  Couplings available:
+ *  <ol>
+ *      <li> Stress
+ *      <li> FluxStress
+ *  </ol>
+ *
+ *  Algorithms available:
+ *  <ol>
+ *      <li> Aitken (and fixpoint)
+ *      <li> Newton
+ *  </ol>
+ *
  */
 
-#include "Epetra_ConfigDefs.h"
+#include <Epetra_ConfigDefs.h>
 #ifdef EPETRA_MPI
-	#include <mpi.h>
-    #include "Epetra_MpiComm.h"
+    #include <mpi.h>
+    #include <Epetra_MpiComm.h>
 #else
-    #include "Epetra_SerialComm.h"
+    #include <Epetra_SerialComm.h>
 #endif
 
 #include <life/lifecore/application.hpp>
@@ -41,13 +66,11 @@
 
 #include <lifemc/lifesolver/MS_Solver.hpp>
 
-
-
 LifeV::AboutData
 makeAbout()
 {
     LifeV::AboutData about( "MultiScale" ,
-                            "LifeV MultiScale Assembler" ,
+                            "LifeV MultiScale Tester" ,
                             "0.1",
                             "Under development",
                             LifeV::AboutData::License_GPL,
@@ -57,69 +80,66 @@ makeAbout()
     return about;
 }
 
-
-
 using namespace LifeV;
-
-
 
 int
 main( int argc, char** argv )
 {
-	//Setup main communicator
-	boost::shared_ptr<Epetra_Comm>	comm;
+    //Setup main communicator
+    boost::shared_ptr<Epetra_Comm>	comm;
 
-	int	nprocs;
-	int rank;
-
-	#ifdef HAVE_MPI
-		std::cout << "MPI Initialization" << std::endl;
-		MPI_Init( &argc, &argv );
-	#endif
+#ifdef HAVE_MPI
+        std::cout << "MPI Initialization" << std::endl;
+        MPI_Init( &argc, &argv );
+#endif
 
 	//MPI Preprocessing
-	#ifdef EPETRA_MPI
+#ifdef EPETRA_MPI
 
-		MPI_Comm_size( MPI_COMM_WORLD, &nprocs );
-		MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+	    int nprocs;
+        int rank;
 
-		if ( rank == 0 )
-		{	std::cout << "MPI processes: " << nprocs << std::endl;
+        MPI_Comm_size( MPI_COMM_WORLD, &nprocs );
+        MPI_Comm_rank( MPI_COMM_WORLD, &rank );
 
-			std::cout << "MPI Epetra Initialization ... " << std::endl;
-		}
-		comm.reset( new Epetra_MpiComm( MPI_COMM_WORLD ) );
+        if ( rank == 0 )
+        {    std::cout << "MPI processes: " << nprocs << std::endl;
 
-		comm->Barrier();
+            std::cout << "MPI Epetra Initialization ... " << std::endl;
+        }
+        comm.reset( new Epetra_MpiComm( MPI_COMM_WORLD ) );
 
-	#else
+        comm->Barrier();
 
-		std::cout << "MPI SERIAL Epetra Initialization ... " << std::endl;
-		comm.reset( new Epetra_SerialComm() );
+#else
 
-	#endif
+        std::cout << "MPI SERIAL Epetra Initialization ... " << std::endl;
+        comm.reset( new Epetra_SerialComm() );
 
-	// Setup MultiScale problem
-	MS_Solver MS;
+#endif
 
-	//Command line parameters
-	GetPot commandLine( argc, argv );
-	std::string dataFile = commandLine.follow( "./MultiScale.dat", 2, "-f", "--file" );
-	bool verbose = commandLine.follow( false, 2, "-s", "--showme" );
+    // Setup MultiScale problem
+    bool exitFlag = EXIT_SUCCESS;
+    MS_Solver MS;
 
-	MS.SetCommunicator( comm );
+    //Command line parameters
+    GetPot commandLine( argc, argv );
+    std::string dataFile = commandLine.follow( "./MultiScale.dat", 2, "-f", "--file" );
+    bool verbose = commandLine.follow( false, 2, "-s", "--showme" );
 
-	MS.SetupProblem( dataFile );
+    MS.SetCommunicator( comm );
 
-	if ( verbose )
-		MS.ShowMe();
+    MS.SetupProblem( dataFile );
 
-	MS.SolveProblem();
+    if ( verbose )
+        MS.ShowMe();
 
-	#ifdef HAVE_MPI
-		std::cout << "MPI Finalization" << std::endl;
-		MPI_Finalize();
-	#endif
+    exitFlag = MS.SolveProblem();
 
-	return( EXIT_SUCCESS );
+#ifdef HAVE_MPI
+        std::cout << "MPI Finalization" << std::endl;
+        MPI_Finalize();
+#endif
+
+    return exitFlag;
 }
