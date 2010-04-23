f0094d0334f6785283db6b028f0a89788117b126
malossi <malossi>
2010-04-23 15:08:30 +0000

    Interface between MS_Solver and OneDimensionalModel updated. Still not ready for explicit coupling with the solver, but we are almost there...

--- MS_Model_1D.cpp
+++ MS_Model_1D.cpp
@@ -3,7 +3,7 @@
 ************************************************************************
 
  This file is part of the LifeV Applications.
- Copyright (C) 2001-2009 EPFL, Politecnico di Milano, INRIA
+ Copyright (C) 2001-2010 EPFL, Politecnico di Milano, INRIA
 
  This library is free software; you can redistribute it and/or modify
  it under the terms of the GNU Lesser General Public License as
@@ -28,8 +28,13 @@
  *  @file
  *  @brief MultiScale Model 1D
  *
- *  @author gilles fourestey <gilles.fourestey@epfl.ch>
+ *  @version 1.0
+ *  @author Gilles Fourestey <gilles.fourestey@epfl.ch>
  *  @date 02-25-2010
+ *
+ *  @version 1.1
+ *  @author Cristiano Malossi <cristiano.malossi@epfl.ch>
+ *  @date 14-04-2010
  */
 
 #include "MS_Model_1D.hpp"
@@ -41,69 +46,86 @@ namespace LifeV {
 // ===================================================
 MS_Model_1D::MS_Model_1D() :
     super                          (),
-    M_output                       (),
-    M_FESpace                      (),
-    M_solver                       (),
-    M_linearSolver                 (),
+    M_Output                       (),
+    M_Data                         ( new Data_Type() ),
+    M_Physics                      (),
+    M_Flux                         (),
+    M_Source                       (),
+    M_Solver                       (),
     M_BC                           (),
-    M_data                         (),
-    M_solution                     ()
+    M_Solution                     (),
+    M_FESpace                      (),
+    M_LinearSolver                 ()
 {
 
 #ifdef DEBUG
-    Debug( 8120 ) << "MS_Model_1D::MS_Model_1D() \n";
+    Debug( 8130 ) << "MS_Model_1D::MS_Model_1D() \n";
 #endif
 
     //M_type = oneD;
+
+    //Define the maps of the OneDimensionalModel objects
+    OneDimensionalModel_MapsDefinition();
+
+    //Register the objects
+    Factory_OneDimensionalModel_Physics::instance().registerProduct( OneD_LinearPhysics,    &Create_OneDimensionalModel_Physics_Linear );
+    Factory_OneDimensionalModel_Physics::instance().registerProduct( OneD_NonLinearPhysics, &Create_OneDimensionalModel_Physics_NonLinear );
+
+    Factory_OneDimensionalModel_Flux::instance().registerProduct(    OneD_LinearFlux,       &Create_OneDimensionalModel_Flux_Linear );
+    Factory_OneDimensionalModel_Flux::instance().registerProduct(    OneD_NonLinearFlux,    &Create_OneDimensionalModel_Flux_NonLinear );
+
+    Factory_OneDimensionalModel_Source::instance().registerProduct(  OneD_LinearSource,     &Create_OneDimensionalModel_Source_Linear );
+    Factory_OneDimensionalModel_Source::instance().registerProduct(  OneD_NonLinearSource,  &Create_OneDimensionalModel_Source_NonLinear );
 }
-
-
 
 // ===================================================
 // MultiScale PhysicalModel Virtual Methods
 // ===================================================
-
 void
-MS_Model_1D::SetupData()
+MS_Model_1D::SetupData( const std::string& FileName )
 {
-    return;
-}
 
-void
-MS_Model_1D::SetupData(const GetPot& dataFile, std::string section)
-{
-    Debug( 8120 ) << "MS_Model_1D::SetupData( ) \n";
+#ifdef DEBUG
+    Debug( 8130 ) << "MS_Model_1D::SetupData( ) \n";
+#endif
 
-    //Fluid data
-    //this->SetDataFile(dataFile);
+    super::SetupData( FileName );
 
-    M_data.reset  ( new DataType( dataFile, section ) );
-    M_params.reset(new Params(dataFile, section));
+    GetPot DataFile( FileName );
 
+    M_Data->setup( DataFile );
+    //M_Data->showMe();
 
-    M_linearSolver.reset(new solver_type(*M_comm));
-    M_linearSolver->setUpPrec        (dataFile, section + "/prec");
-    M_linearSolver->setDataFromGetPot(dataFile, section + "/solver");
-
+    M_LinearSolver.reset( new LinearSolver_Type( *M_comm ) );
+    M_LinearSolver->setUpPrec        ( DataFile, "1D_Model/prec" );
+    M_LinearSolver->setDataFromGetPot( DataFile, "1D_Model/solver");
 }
 
 void
 MS_Model_1D::SetupModel()
 {
-    Debug( 8120 ) << "MS_Model_1D::SetupProblem() \n";
 
-    M_flux.reset  (new Flux(*M_params));
-    M_source.reset(new Source(*M_params));
+#ifdef DEBUG
+    Debug( 8130 ) << "MS_Model_1D::SetupProblem() \n";
+#endif
 
-    setUpFESpace();
+    M_Physics = Physics_PtrType( Factory_OneDimensionalModel_Physics::instance().createObject( M_Data->PhysicsType() ) );
+    M_Physics->SetData( M_Data );
 
-    M_solver.reset (new OneDModelSolver<Params, Flux, Source> (*M_data, *M_params, *M_flux, *M_source, *M_FESpace, *(this->M_comm)));
-    M_solver->setup();
+    M_Flux = Flux_PtrType( Factory_OneDimensionalModel_Flux::instance().createObject( M_Data->FluxType() ) );
+    M_Flux->SetPhysics( M_Physics );
 
-    M_solver->setLinearSolver(M_linearSolver);
+    M_Source = Source_PtrType( Factory_OneDimensionalModel_Source::instance().createObject( M_Data->SourceType() ) );
+    M_Source->SetPhysics( M_Physics );
 
-    M_BC.reset(new BCType(M_solver->U_thistime(), *M_flux, M_FESpace->dim()));
+    SetupFESpace();
 
+    M_Solver.reset ( new Solver_Type ( M_Physics, M_Flux, M_Source, M_FESpace, M_comm ) );
+    M_Solver->setup();
+
+    M_Solver->setLinearSolver( M_LinearSolver );
+
+    M_BC.reset( new BC_Type( M_Solver->U_thistime(), M_Flux, M_FESpace->dim() ) );
 }
 
 void
@@ -111,33 +133,31 @@ MS_Model_1D::BuildSystem()
 {
 
 #ifdef DEBUG
-    Debug( 8120 ) << "MS_Model_1D::BuildSystem() \n";
+    Debug( 8130 ) << "MS_Model_1D::BuildSystem() \n";
 #endif
 
-    M_solver->initialize();
+    M_Solver->initialize();
 }
-
 
 void
 MS_Model_1D::UpdateSystem()
 {
 
-    Debug( 8120 ) << "MS_Model_1D::UpdateSystem() \n";
-    M_data->timeAdvance();
-    M_solver->timeAdvance( M_data->time() );
+#ifdef DEBUG
+    Debug( 8130 ) << "MS_Model_1D::UpdateSystem() \n";
+#endif
 
+    M_Solver->timeAdvance();
 }
 
 void
 MS_Model_1D::SolveSystem()
 {
+#ifdef DEBUG
+    Debug( 8130 ) << "MS_Model_1D::SolveSystem() \n";
+#endif
 
-    Debug( 8120 ) << "MS_Model_1D::SolveSystem() \n";
-
-    int    count = 0;
-
-    M_solver->iterate( *M_BC, M_data->time() , count );
-
+    M_Solver->iterate( *M_BC, M_Data->dataTime()->getTime() );
 }
 
 void
@@ -145,16 +165,14 @@ MS_Model_1D::SaveSolution()
 {
 
 #ifdef DEBUG
-    Debug( 8120 ) << "MS_Model_1D::SaveSolution() \n";
+    Debug( 8130 ) << "MS_Model_1D::SaveSolution() \n";
 #endif
 
     //Post-processing
 
 //     *M_FluidSolution = M_Fluid->solution();
-//     M_output->postProcess( M_FluidData->getTime() );
+//     M_Output->postProcess( M_FluidData->getTime() );
 
-    //MPI Barrier
-    //M_comm->Barrier();
 }
 
 void
@@ -166,7 +184,7 @@ MS_Model_1D::ShowMe()
 // Methods
 // ===================================================
 void
-MS_Model_1D::SetupLinearData()
+MS_Model_1D::SetupLinearData( const std::string& /*FileName*/ )
 {
 
 }
@@ -176,7 +194,7 @@ MS_Model_1D::SetupLinearModel()
 {
 
 #ifdef DEBUG
-    Debug( 8120 ) << "MS_Model_1D::SetupLinearModel( ) \n";
+    Debug( 8130 ) << "MS_Model_1D::SetupLinearModel( ) \n";
 #endif
 }
 
@@ -186,42 +204,85 @@ MS_Model_1D::UpdateLinearModel()
 }
 
 void
-MS_Model_1D::SolveLinearModel( bool& SolveLinearSystem )
+MS_Model_1D::SolveLinearModel( bool& /*SolveLinearSystem*/ )
 {
 
 #ifdef DEBUG
-    Debug( 8120 ) << "MS_Model_1D::SolveLinearModel() \n";
+    Debug( 8130 ) << "MS_Model_1D::SolveLinearModel() \n";
 #endif
+
 }
 
 // ===================================================
 // Get Methods
 // ===================================================
-MS_Model_1D::BCType&
+MS_Model_1D::BC_Type&
 MS_Model_1D::GetBC()
 {
     return *M_BC;
 }
 
-MS_Model_1D::FESpaceType&
+MS_Model_1D::Data_Type&
+MS_Model_1D::GetData()
+{
+    return *M_Data;
+}
+
+MS_Model_1D::Physics_PtrType&
+MS_Model_1D::GetPhysics()
+{
+    return M_Physics;
+}
+
+MS_Model_1D::Flux_PtrType&
+MS_Model_1D::GetFlux()
+{
+    return M_Flux;
+}
+
+MS_Model_1D::Source_PtrType&
+MS_Model_1D::GetSource()
+{
+    return M_Source;
+}
+
+MS_Model_1D::FESpace_Type&
 MS_Model_1D::GetFESpace()
 {
     return *M_FESpace;
+}
+
+MS_Model_1D::Solver_Type&
+MS_Model_1D::GetSolver()
+{
+    return *M_Solver;
+}
+
+// ===================================================
+// Set Methods
+// ===================================================
+void
+MS_Model_1D::SetBC( boost::shared_ptr<BC_Type>& BC )
+{
+    M_BC = BC;
 }
 
 // ===================================================
 // Private Methods
 // ===================================================
 void
-MS_Model_1D::setUpFESpace()
+MS_Model_1D::SetupFESpace()
 {
-    Debug( 8120 ) << "MS_Model_1D::setupFEspace() \n";
+
+#ifdef DEBUG
+    Debug( 8130 ) << "MS_Model_1D::setupFEspace() \n";
+#endif
 
     const RefFE*    refFE = &feSegP1;
     const QuadRule* qR    = &quadRuleSeg3pt;
     const QuadRule* bdQr  = &quadRuleSeg1pt;
 
-    M_FESpace.reset(new FESpace<MeshType, EpetraMap>(M_data->mesh(), *refFE, *qR, *bdQr, 1, *(this->M_comm)));
+    M_FESpace.reset( new FESpace_Type( M_Data->mesh(), *refFE, *qR, *bdQr, 1, *M_comm ) );
 }
 
 
