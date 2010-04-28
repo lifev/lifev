bcb7d624a13106ca6fed7615591137235582e152
rruiz <rruiz>
2010-04-28 13:59:29 +0000

    REO case removed

--- life/lifesolver/monodomainSolver.hpp
+++ life/lifesolver/monodomainSolver.hpp
@@ -752,16 +752,8 @@ void MonodomainSolver<Mesh, SolverType>::applyBoundaryConditions( matrix_type&  
         BCh.bdUpdate( *M_uFESpace.mesh(), M_uFESpace.feBd(), M_uFESpace.dof() );
     }
 
-    //vector_type rhsFull(*M_localMap.getMap(Repeated));
-
-
-
-//    rhsFull.Import(M_rhsNoBC, Zero); // ignoring non-local entries, Otherwise they are summed up lately
-
     bcManage( matrix, rhs, *M_uFESpace.mesh(), M_uFESpace.dof(), BCh, M_uFESpace.feBd(), 1.,
                  M_data.getTime() );
-
-   // rhs = rhsFull;
 
 
     if ( BCh.hasOnlyEssential() && M_diagonalize )
