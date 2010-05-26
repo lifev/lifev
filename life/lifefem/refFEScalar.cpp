3ff198e933d4d3069f38d07d04cdc08b71694ffb
quinodoz <quinodoz>
2010-05-26 15:36:15 +0000

    New class refFEScalar
    It derives from refFE and is basically what we called before refFE.
    To be used for "standard" finite elements

---
+++ life/lifefem/refFEScalar.cpp
@@ -0,0 +1,54 @@
+//@HEADER
+/*
+************************************************************************
+
+ This file is part of the LifeV Applications.
+ Copyright (C) 2001-2010 EPFL, Politecnico di Milano, INRIA
+
+ This library is free software; you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as
+ published by the Free Software Foundation; either version 2.1 of the
+ License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ USA
+
+************************************************************************
+*/
+//@HEADER
+
+/*!
+    @file
+    @brief A short description of the file content
+
+    @author Samuel Quinodoz <samuel.quinodoz@epfl.ch>
+    @date 10 May 2010
+
+    A more detailed description of the file (if necessary)
+ */
+
+#include <refFEScalar.hpp>
+
+namespace LifeV {
+
+RefFEScalar::RefFEScalar( std::string name, FE_TYPE type, ReferenceShapes shape,
+              int nbDofPerVertex, int nbDofPerEdge, int nbDofPerFace,
+              int nbDofPerVolume, int nbDof, int nbCoor, const Fct* phi,
+              const Fct* dPhi, const Fct* d2Phi, const Real* refCoor,
+              DofPatternType patternType,
+              const RefFE* bdRefFE ) :
+    RefFE( name, type, shape,nbDofPerVertex,nbDofPerEdge,nbDofPerFace,
+               nbDofPerVolume, nbDof, nbCoor,1, phi, dPhi, d2Phi, static_cast<Fct*>(NULL), refCoor,
+               patternType, bdRefFE )
+{
+    CONSTRUCTOR( "RefScalar" );
+}
+
+} // Namespace LifeV
---
+++ life/lifefem/refFEScalar.hpp
@@ -0,0 +1,128 @@
+//@HEADER
+/*
+************************************************************************
+
+ This file is part of the LifeV Applications.
+ Copyright (C) 2001-2010 EPFL, Politecnico di Milano, INRIA
+
+ This library is free software; you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as
+ published by the Free Software Foundation; either version 2.1 of the
+ License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ USA
+
+************************************************************************
+*/
+//@HEADER
+
+/*!
+    @file
+    @brief A short description of the file content
+
+    @author Samuel Quinodoz <samuel.quinodoz@epfl.ch>
+    @date 10 May 2010
+
+    A more detailed description of the file (if necessary)
+ */
+
+#ifndef REFFESCALAR_H
+#define REFFESCALAR_H 1
+
+
+#include <life/lifefem/refFE.hpp>
+
+namespace LifeV {
+
+//! refFEScalar - Short description of the class
+/*!
+  
+ */
+class RefFEScalar
+    : public RefFE
+{
+public:
+        //! Constructor of a reference Lagrangian finite element.
+    /*!
+      Constructor of a reference finite element. The arguments are:
+      @param name  the name of the f.e.
+      @param type  the type of the f.e. (FE_P1_2D,... see the #define at the
+      begining of refFE.h)
+      @param shape  the geometry belongs to enum ReferenceShapes {NONE, POINT,
+      LINE, TRIANGLE, QUAD, HEXA, PRISM, TETRA}; (see basisElSh.h)
+      @param nbDofPerVertex  the number of degrees of freedom per vertex
+      @param nbDofPerEdge  the number of degrees of freedom per edge
+      @param nbDofPerFace  the number of degrees of freedom per face
+      @param nbDofPerVolume  the number of degrees of freedom per volume
+      @param nbDof  the total number of d.o.f ( = nbDofPerVertex * nb vertex +
+      nbDofPerEdge * nb edges + etc...)
+      @param nbCoor  number of local coordinates
+      @param phi  the static array containing the basis functions (defined in
+      refEle.h)
+      @param dPhi  the static array containing the derivatives of the basis
+      functions (defined in refEle.h)
+      @param d2Phi  the static array containing the second derivatives of the
+      basis functions (defined in refEle.h)
+      @param refCoor  the static array containing the coordinates of the nodes on
+      the reference element (defined in refEle.h)
+      @param patternType  in most of cases STANDARD_PATTERN, except for elements
+      like P1isoP2 (to define a new pattern, add a new #define in refFE.h and
+      code it in refFE.cc following the example of P1ISOP2_TRIA_PATTERN)
+      @param bdRefFE  a pointer on the associated reference finite element on the boundary
+    */
+    RefFEScalar( std::string          name,
+               FE_TYPE              type,
+               ReferenceShapes      shape,
+               int                  nbDofPerVertex,
+               int                  nbDofPerEdge,
+               int                  nbDofPerFace,
+               int                  nbDofPerVolume,
+               int                  nbDof,
+               int                  nbCoor,
+               const Fct*           phi,
+               const Fct*           dPhi,
+               const Fct*           d2Phi,
+               const Real*          refCoor,
+               DofPatternType       patternType,
+               const RefFE*         bdRefFE );
+
+    ~RefFEScalar()
+    {
+        DESTRUCTOR( "RefFEScalar" );
+    };
+  
+};
+
+//--------------------------------------------------
+
+extern const RefFEScalar feSegP1;
+extern const RefFEScalar feSegP2;
+
+extern const RefFEScalar feTriaP0;
+extern const RefFEScalar feTriaP1;
+extern const RefFEScalar feTriaP2;
+
+extern const RefFEScalar feQuadQ0;
+extern const RefFEScalar feQuadQ1;
+extern const RefFEScalar feQuadQ2;
+
+extern const RefFEScalar feTetraP0;
+extern const RefFEScalar feTetraP1;
+extern const RefFEScalar feTetraP1bubble;
+extern const RefFEScalar feTetraP2;
+extern const RefFEScalar feTetraP2tilde;
+
+extern const RefFEScalar feHexaQ0;
+extern const RefFEScalar feHexaQ1;
+
+} // Namespace LifeV
+
+#endif /* REFFESCALAR_H */
