f18dd7fd13fe76385500ba11a5c72c2d377a125c
crosetto <crosetto>
2010-08-03 13:54:33 +0000

    adding a variant for the Robin-Neumann preconditioner

---
+++ BlockMatrixRN.cpp
@@ -0,0 +1,91 @@
+//@HEADER
+/*
+************************************************************************
+
+ This file is part of the LifeV Applications.
+ Copyright (C) 2001-2010 EPFL, Politecnico di Milano, INRIA
+
+ This library is free software; you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as
+ published by the Free Software Foundation; either version 2.1 of the
+ License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ USA
+
+************************************************************************
+*/
+//@HEADER
+
+/*!
+    @file BlockMatrixRN.cpp
+    @brief
+
+    @author Paolo Crosetto <crosetto@iacspc70.epfl.ch>
+    @date 09 Jul 2010
+
+ */
+
+#include <BlockMatrixRN.hpp>
+#include <EpetraExt_MatrixMatrix.h>
+
+namespace LifeV {
+
+
+void BlockMatrixRN::coupler(map_shared_ptrtype map,
+                            const std::map<ID, ID>& locDofMap,
+                            const vector_ptrtype numerationInterface,
+                            const Real& timeStep)
+{
+    super::coupler( map,/* M_FESpace[0], M_offset[0], M_FESpace[1], M_offset[1],*/ locDofMap, numerationInterface, timeStep );
+    M_robinCoupling.reset(new matrix_type(M_coupling->getMap(), 0));
+    robinCoupling( M_robinCoupling, M_alphaf, M_alphas, 7, M_FESpace[1], M_offset[1], M_FESpace[0], M_offset[0], locDofMap, numerationInterface );
+    M_robinCoupling->GlobalAssemble( );
+}
+
+
+
+void BlockMatrixRN::blockAssembling()
+{
+    M_globalMatrix.reset(new matrix_type(M_coupling->getMap()));
+    *M_globalMatrix += *M_coupling;
+    for(UInt k=0; k<super::M_blocks.size(); ++k)
+    {
+        super::M_blocks[k]->GlobalAssemble();
+    }
+
+    applyRobinCoupling(M_blocks);
+    M_robinPart->GlobalAssemble();
+
+    matrix_ptrtype fluidRobinBlock(new matrix_type(M_robinCoupling->getMap(), 1));
+    *fluidRobinBlock += *M_blocks[1];
+    *fluidRobinBlock += *M_robinPart;
+    fluidRobinBlock->GlobalAssemble();
+    M_blocks[1]->swapCrsMatrix( fluidRobinBlock->getMatrixPtr() );
+
+    for(UInt k=0; k<M_blocks.size(); ++k)
+    {
+        *M_globalMatrix += *M_blocks[k];
+    }
+}
+
+void BlockMatrixRN::GlobalAssemble()
+{
+    *M_rhsVec += (*M_robinCoupling)*(*M_rhsVec);
+    super::GlobalAssemble();
+}
+
+void BlockMatrixRN::setDataFromGetPot( const GetPot& data, const std::string& section )
+{
+    super::setDataFromGetPot(data, section);
+    setRobinData(data, section + "/robin");
+}
+
+} // Namespace LifeV
---
+++ BlockMatrixRN.hpp
@@ -0,0 +1,126 @@
+//@HEADER
+/*
+************************************************************************
+
+ This file is part of the LifeV Applications.
+ Copyright (C) 2001-2010 EPFL, Politecnico di Milano, INRIA
+
+ This library is free software; you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as
+ published by the Free Software Foundation; either version 2.1 of the
+ License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ USA
+
+************************************************************************
+*/
+//@HEADER
+
+/*!
+    @file BlockMatrix.hpp
+    @brief file containing a class for handling a 2-blocks matrix with Robin-Neumann coupling
+
+    @author Paolo Crosetto <crosetto@iacspc70.epfl.ch>
+    @date 09 Jul 2010
+
+ */
+
+#ifndef BLOCKMATRIXRN_H
+#define BLOCKMATRIXRN_H 1
+
+#include <life/lifecore/life.hpp>
+#include <lifemc/lifesolver/BlockMatrix.hpp>
+#include <lifemc/lifesolver/RobinInterface.hpp>
+
+namespace LifeV {
+
+//! BlockMatrixRN - class for handling a 2-blocks matrix with Robin-Neumann coupling
+/*!
+    @author Paolo Crosetto
+
+    This class derives both from BlockInterface, which is the base class for the block operators, and from RobinInterface,
+    which is a class holding some general methods and attributes for the robin coupling.
+
+    NOTE: this class has been tested only for the GE time discretization (method = monolithicGE)
+    NOTE: as the same block matrices are shared between the system matrix and the preconditioner, the preconditioner
+    choices available are in principle automatically adapted to the RN case. The preconditioners tested for this case
+    are the modular composedDN and the algebraic additive Schwarz AdditiveSchwarz.
+ */
+class BlockMatrixRN : public BlockMatrix, RobinInterface
+{
+public:
+
+    //! @name Public Types
+    //@{
+    typedef BlockMatrix super;
+    typedef RobinInterface  superRobin;
+    //@}
+
+
+    //! @name Constructor & Destructor
+    //@{
+
+    //! Empty Constructor
+    BlockMatrixRN(UInt flag):
+        super(flag),
+        superRobin()
+    {}
+
+    //! Destructor
+    ~BlockMatrixRN(){}
+    //@}
+
+    //! @name Public methods
+    //@{
+
+    //! Adds to the r.h.s. the part due to the Robin Coupling and runs BlockMatrix::GlobalAssemble()
+    void GlobalAssemble();
+
+    //! Sums all the blocks and the couplings into the system matrix, adds the robin coupling part
+    void blockAssembling();
+
+
+    //! Computes the coupling
+    /*!
+      computes all the coupling blocks specific for the chosen preconditioner. The coupling is handled
+      through an augmented formulation, introducing new variables (multipliers). Needs as input: the global map of the problem,
+      the FESpaces of the subproblems to be coupled with their offsets, a std::map holding the two numerations of the
+      interface between the two subproblems (the numeration can be different but the nodes must be matching in each
+      subdomain), an EpetraVector defined on the multipliers map containing the corresponding dof at the interface (NB: the multipliers map should be constructed from the second numeration in the std::map).
+      Note that the FESpaces and the offsets have to be set before calling this method.
+      @param map the map of the global problem
+      @param locDofMap std::map with the correspondence between the interface dofs for the two different maps in
+      the subproblems
+      @param numerationInterface vector containing the correspondence of the Lagrange multipliers with the interface dofs
+     */
+    void coupler(map_shared_ptrtype map,
+                 const std::map<ID, ID>& locDofMap,
+                 const vector_ptrtype numerationInterface,
+                 const Real& timeStep);
+
+    //! sets the data relative to Robin (e.g. the coefficients \f$\alpha_f\f$ and \f$\alpha_s\f$).
+    void setDataFromGetPot( const GetPot& data, const std::string& section );
+
+    //! sets the matrix where the Robin contribution will be assembled (which have to passed from outside) and the \
+    /*! right hand side vector of the linear system, which will be updated with the Robin part.
+     */
+    void setRobin( matrix_ptrtype& matrix, vector_ptrtype& vec ){setRobinMatrix( matrix ); setRobinRhs( vec );}
+
+    //! sets the matrix where the Robin contribution will be assembled
+    void setRobin( matrix_ptrtype& matrix ){setRobinMatrix( matrix ); }
+    //@}
+private:
+
+};
+
+} // Namespace LifeV
+
+#endif /* BLOCKMATRIXRN_H */
