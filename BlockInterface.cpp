045d3e60d037f7156dab1fa8014d1de7a9cc84ba
crosetto <crosetto>
2010-08-03 13:27:09 +0000

    adding a pure virtual interface to deal with monolithically coupled problems

---
+++ BlockInterface.cpp
@@ -0,0 +1,212 @@
+//@HEADER
+/*
+************************************************************************
+
+ This file is part of the LifeV Applications.
+ Copyright (C) 2001-2010 EPFL, Politecnico di Milano, INRIA
+
+ This library is free software; you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as
+ published by the Free Software Foundation; either version 2.1 of the
+ License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ USA
+
+************************************************************************
+*/
+//@HEADER
+
+/*!
+    @file
+    @brief This file contains the implementation of some methods common to all the linear systems with
+    a block structure.
+
+    @author Paolo Crosetto <crosetto@iacspc70.epfl.ch>
+    @date 07 Jun 2010
+ */
+
+#include <BlockInterface.hpp>
+
+namespace LifeV {
+
+
+void BlockInterface::couplingMatrix(matrix_ptrtype & bigMatrix,
+                                    ID coupling,
+                                    const std::vector<fespace_ptrtype> problem,
+                                    const std::vector<UInt> offset,
+                                    const std::map<ID, ID>& locDofMap,
+                                    const vector_ptrtype& numerationInterface,
+                                    const Real& timeStep,
+                                    const Real& value) // not working with non-matching grids
+{// coupling from 1 to 31, working as chmod
+    //std::map<ID, ID> const& locDofMap = M_dofStructureToHarmonicExtension->locDofMap();
+    std::map<ID, ID>::const_iterator ITrow;
+    int flag(coupling);
+    UInt interface(numerationInterface->getMap().getMap(Unique)->NumGlobalElements());
+    UInt totalSize(offset[0]+problem[0]->map().getMap(Unique)->NumGlobalElements());
+
+    if(flag-16>=0)
+    {
+        UInt interface( numerationInterface->getMap().getMap( Unique )->NumGlobalElements() );
+        UInt solidFluidInterface( offset[2] );
+        std::map<ID, ID>::const_iterator ITrow;
+        for( UInt dim = 0; dim < nDimensions; ++dim )
+        {
+            for( ITrow = locDofMap.begin(); ITrow != locDofMap.end() ; ++ITrow )
+            {
+                if( numerationInterface->getMap().getMap(Unique)->LID(ITrow->second /*+ dim*solidDim*/) >= 0 )//to avoid repeated stuff
+                {
+                    bigMatrix->set_mat_inc(solidFluidInterface + ITrow->first + dim*problem[2]->dof().numTotalDof() - 1, offset[0] + ITrow->second-1 + dim* problem[0]->dof().numTotalDof(), (-value)*timeStep/**1.e-2*//*scaling of the solid matrix*/ );
+                }
+            }
+        }
+        flag-=16;
+    }
+
+    for(UInt dim = 0; dim < nDimensions; ++dim)
+    {
+        for( ITrow = locDofMap.begin(); ITrow != locDofMap.end() ; ++ITrow, flag=coupling)
+        {
+            if(numerationInterface->getMap().getMap(Unique)->LID(ITrow->second /*+ dim*solidDim*/) >= 0 )//to avoid repeated stuff
+            {
+
+                if(flag-8>=0)//right low
+                {
+                    bigMatrix->set_mat_inc( offset[0] + ITrow->second-1 + dim* problem[0]->dof().numTotalDof(),(int)(*numerationInterface)[ITrow->second/*+ dim*solidDim*/ ] - 1 + dim*interface + totalSize, value );//right low
+                    flag -= 8;
+                }
+                if(flag-4>=0)// right up
+                {
+                    bigMatrix->set_mat_inc( offset[1] + ITrow->first-1 + dim* problem[1]->dof().numTotalDof(), (int)(*numerationInterface)[ITrow->second/*+ dim*solidDim*/ ] - 1 + dim*interface + totalSize, -value );//right up
+                    flag -= 4;
+                }
+                if(flag-2>=0)//low left
+                {
+                    bigMatrix->set_mat_inc( (int)(*numerationInterface)[ITrow->second/*+ dim*solidDim*/ ] - 1 + dim*interface + totalSize, (ITrow->first)-1 + dim* problem[1]->dof().numTotalDof(), value);//low left
+                    flag -= 2;
+                }
+                if(flag-1>=0)//low right
+                    bigMatrix->set_mat_inc( (int)(*numerationInterface)[ITrow->second/*+ dim*solidDim*/ ] - 1 + dim*interface + totalSize, (offset[0] + ITrow->second)-1 + dim* problem[0]->dof().numTotalDof(), -value);//low right
+
+                bigMatrix->set_mat_inc( (int)(*numerationInterface)[ITrow->second/*+ dim*solidDim*/ ] - 1 + dim*interface + totalSize , (int)(*numerationInterface)[ITrow->second /*+ dim*solidDim*/ ] - 1 + dim*interface + totalSize, 0.0);
+            }
+        }
+    }
+}
+
+void BlockInterface::applyBoundaryConditions(const Real& time)
+{
+    for(ID i=0; i<M_bch.size(); ++i)
+    {
+        applyBoundaryConditions(time, i);
+    }
+}
+
+
+void BlockInterface::applyBoundaryConditions(const Real& time, const UInt i)
+{
+    M_blocks[i]->openCrsMatrix();
+    if ( !M_bch[i]->bdUpdateDone() )
+    {
+        M_bch[i]->bdUpdate( *M_FESpace[i]->mesh(), M_FESpace[i]->feBd(), M_FESpace[i]->dof() );
+        M_bch[i]->setOffset(M_offset[i]);
+    }
+    bcManageMatrix( *M_blocks[i] , *M_FESpace[i]->mesh(), M_FESpace[i]->dof(), *M_bch[i], M_FESpace[i]->feBd(), 1., time);
+}
+
+void BlockInterface::setConditions( std::vector<bchandler_ptrtype>& vec )
+{
+    M_bch = vec;
+}
+
+void
+BlockInterface::setSpaces(std::vector<fespace_ptrtype>& vec )
+{
+    M_FESpace = vec;
+}
+
+void
+BlockInterface::setOffsets(UInt blocks, ...)
+{
+    using namespace std;
+    va_list arguments;
+    va_start(arguments, blocks);
+    M_offset.resize(0);
+
+    for (ID i=0; i<M_blocks.size(); ++i)
+        M_offset.push_back(va_arg(arguments, UInt));
+    va_end(arguments);
+}
+
+
+void
+BlockInterface::robinCoupling( matrix_ptrtype matrix,
+                            Real  alphaf,
+                            Real  alphas,
+                            UInt  coupling,
+                            const BlockInterface::fespace_ptrtype FESpace1,
+                            const UInt& offset1,
+                            const BlockInterface::fespace_ptrtype FESpace2,
+                            const UInt& offset2,
+                            const std::map<ID, ID>& locDofMap,
+                            const BlockInterface::vector_ptrtype& numerationInterface ) // not working with non-matching grids
+{//coupling: flag from 1 to 4 working as chmod
+    UInt interface(numerationInterface->getMap().getMap(Unique)->NumGlobalElements());
+    std::map<ID, ID>::const_iterator ITrow;
+    UInt totalSize(offset2+FESpace2->map().getMap(Unique)->NumGlobalElements());
+
+
+    if(coupling-4 >= 0)
+    {
+            for( ITrow = locDofMap.begin(); ITrow != locDofMap.end() ; ++ITrow)
+            {
+                if(numerationInterface->getMap().getMap(Unique)->LID(ITrow->second /*+ dim*solidDim*/) >= 0 )//to avoid repeated stuff
+                {
+                    for(UInt dim = 0; dim < nDimensions; ++dim)
+                    {
+                        matrix->set_mat_inc( (int)(*numerationInterface)[ITrow->second ] - 1 + dim*interface + totalSize, (offset2 + ITrow->second)-1 + dim* FESpace2->dof().numTotalDof(), alphas);//low right
+                    }
+                }
+            }
+            coupling -= 4;
+    }
+    if(coupling-2 >= 0)
+    {
+        for( ITrow = locDofMap.begin(); ITrow != locDofMap.end() ; ++ITrow)
+        {
+            if(numerationInterface->getMap().getMap(Unique)->LID(ITrow->second /*+ dim*solidDim*/) >= 0 )//to avoid repeated stuff
+            {
+                for(UInt dim = 0; dim < nDimensions; ++dim)
+                {
+                    matrix->set_mat_inc( ITrow->first-1 + dim* FESpace1->dof().numTotalDof(), (int)(*numerationInterface)[ITrow->second ] - 1 + dim*interface + totalSize, alphaf );//right up
+                }
+            }
+        }
+        coupling -= 2;
+    }
+    if(coupling-1 >= 0)
+    {
+            for( ITrow = locDofMap.begin(); ITrow != locDofMap.end() ; ++ITrow)
+            {
+                if(numerationInterface->getMap().getMap(Unique)->LID(ITrow->second /*+ dim*solidDim*/) >= 0 )//to avoid repeated stuff
+                {
+                    for(UInt dim = 0; dim < nDimensions; ++dim)
+                    {
+                    matrix->set_mat_inc( (int)(*numerationInterface)[ITrow->second ] - 1 + dim*interface + totalSize, (ITrow->first)-1 + dim* FESpace1->dof().numTotalDof(), alphas);//low left
+                    }
+                }
+            }
+    }
+}
+
+
+
+} // Namespace LifeV
---
+++ BlockInterface.hpp
@@ -0,0 +1,390 @@
+//@HEADER
+/*
+************************************************************************
+
+ This file is part of the LifeV Applications.
+ Copyright (C) 2001-2010 EPFL, Politecnico di Milano, INRIA
+
+ This library is free software; you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as
+ published by the Free Software Foundation; either version 2.1 of the
+ License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ USA
+
+************************************************************************
+*/
+//@HEADER
+
+/*!
+    @file
+    @brief This file contains a pure virtual class for the linear operators with a block structure
+    (i.e. block matrices and preconditioners). The specializations of this class can be used to handle many
+    types of composed preconditioners and composed operators.
+
+    @author Paolo Crosetto <crosetto@iacspc70.epfl.ch>
+    @date 07 Jun 2010
+ */
+
+#ifndef BLOCKINTERFACE_H
+#define BLOCKINTERFACE_H 1
+
+#include <cstdarg>
+#include <life/lifecore/life.hpp>
+#include <life/lifecore/GetPot.hpp>
+
+#include <life/lifefem/FESpace.hpp>
+
+#include <life/lifealg/SolverTrilinos.hpp>
+#include <life/lifealg/IfpackPreconditioner.hpp>
+#include <lifemc/lifealg/ComposedPreconditioner.hpp>
+#include <life/lifefem/bcManage.hpp>
+
+namespace LifeV {
+
+//! BlockInterface - This is a pure virtual class for the linear operators with a block structure
+/*!    (i.e. block matrices and preconditioners).
+    @author Paolo Crosetto
+
+    The specializations of this class can be used to handle many
+    types of composed preconditioners and composed operators. It conteins a vector of pointers to EpetraMatrix
+    holding the diferent blocks. These matrices should be zero everywhere but on the diagonal block corresponding
+    to the corresponding problem. This class also handles the coupling through a vector of pointer to coupling matrices.
+ */
+
+class BlockInterface
+{
+public:
+
+    //! @name Public Types
+    //@{
+
+    /*! @enum listOfTemplatesOptions
+        Description of the purpose of the enumerator list.
+     */
+    typedef EpetraVector                                               vector_type;
+    typedef boost::shared_ptr< vector_type >                           vector_ptrtype;
+    typedef EpetraMatrix< Real >                                       matrix_type;
+    typedef boost::shared_ptr< matrix_type >                           matrix_ptrtype;
+    typedef boost::shared_ptr< Epetra_Operator >                       epetra_operator_ptrtype;
+    typedef boost::shared_ptr< EpetraPreconditioner >                  epetra_preconditioner_ptrtype;
+    typedef matrix_type::matrix_type                                   epetra_matrix_type;
+    typedef SolverTrilinos                                             solver_type;
+    typedef boost::shared_ptr< SolverTrilinos >                        solver_ptrtype;
+    typedef boost::shared_ptr< FESpace<RegionMesh3D<LinearTetra>, EpetraMap> >  fespace_shared_ptrtype;
+    typedef fespace_shared_ptrtype                                     fespace_ptrtype;
+    //    typedef FESpace<RegionMesh3D<LinearTetra>, EpetraMap>*                 fespace_ptrtype;
+    typedef EpetraMap*                                                 map_ptrtype;
+    typedef boost::shared_ptr< EpetraMap >                             map_shared_ptrtype;
+    //typedef BCHandler*                                                 bchandler_ptrtype;
+    typedef boost::shared_ptr< BCHandler >                             bchandler_ptrtype;
+    //@}
+
+
+    //! @name Constructor & Destructor
+    //@{
+    //! Empty Constructor
+    BlockInterface():
+        M_bch(),
+        M_blocks(),
+        M_FESpace()
+    {}
+    //! Destructor
+    ~BlockInterface()
+{
+//     free(M_offset);
+//     free(M_FESpace);
+}
+    //@}
+
+
+
+    //! @name Pure virtual methods
+    //@{
+    //! Solves the preconditioned linear system (used only when dealing with a preconditioner)
+    /*!
+      Provided the linear solver and the right hand side this method computes the solution and returns it into
+      the result vector.
+        @param rhs right hand side of the linear system
+        @param result output result
+        @param linearSolver the linear system
+     */
+    virtual int  solveSystem( const vector_type& rhs, vector_type& result, solver_ptrtype& linearSolver)=0;
+
+    //! Sets the parameters needed by the preconditioner from data file
+    /*!
+        @param data GetPot object reading the text data file
+        @param section string specifying the path in the data file where to find the options for the operator
+     */
+    virtual void setDataFromGetPot(const GetPot& data, const std::string& section)=0;
+
+//     //! sets up a vector of raw pointers to the EpetraMaps of each block
+//     /*!
+//       The number of maps in this vector is not ser a-priori.
+//      */
+//     virtual void setBlockMaps(const UInt multipliers, ...)=0;
+
+    //! @name Pure virtual methods
+    //@{
+    //! pushes a block at the end of the vector
+    /*!
+      adds a new block
+        @param Mat block matrix to push
+        @param recompute flag stating wether the preconditioner for this block have to be recomputed at every time step
+     */
+    virtual void push_back_matrix( const matrix_ptrtype& Mat, const bool recompute) =0;
+
+    //! replaces a block
+    /*!
+      replaces a block on a specified position in the vector
+        @param Mat block matrix to push
+        @param index position in the vector
+     */
+    virtual void replace_matrix( const matrix_ptrtype& Mat, UInt index)=0;
+
+    //! runs GlobalAssemble on the blocks
+    /*!
+      closes and distributes all the matrices before computing the preconditioner
+     */
+    virtual void GlobalAssemble()=0;
+
+    //! Computes the coupling
+    /*!
+      computes all the coupling blocks specific for the chosen preconditioner. The coupling is handled
+      through an augmented formulation, introducing new variables (multipliers). Needs as input: the global map of the problem,
+      the FESpaces of the subproblems to be coupled with their offsets, a std::map holding the two numerations of the
+      interface between the two subproblems (the numeration can be different but the nodes must be matching in each
+      subdomain), an EpetraVector defined on the multipliers map containing the corresponding dof at the interface (NB: the multipliers map should be constructed from the second numeration in the std::map).
+      Note that the FESpaces and the offsets have to be set before calling this method.
+      @param map the map of the global problem
+      @param locDofMap std::map with the correspondence between the interface dofs for the two different maps in
+      the subproblems
+      @param numerationInterface vector containing the correspondence of the Lagrange multipliers with the interface dofs
+     */
+    virtual void coupler(map_shared_ptrtype map,
+                         const std::map<ID, ID>& locDofMap,
+                         const vector_ptrtype numerationInterface,
+                         const Real& timeStep)=0;
+
+
+    //! returns true if the operator is set
+    /*!
+    */
+    virtual bool set()=0;
+    //@}
+
+    //@name virtual methods
+    //@{
+    //!replace a block preconditioner
+    /*!
+      (only used if the operator is a preconditioner)
+     */
+    virtual void replace_precs ( const epetra_operator_ptrtype& Mat, UInt index)
+    {ERROR_MSG("this method should not be implemented");}
+
+    //!pushes back a block preconditioner
+    /*!
+      (only used if the operator is a preconditioner)
+     */
+    virtual void push_back_precs (const epetra_operator_ptrtype& Mat)
+    {ERROR_MSG("this method should not be implemented");}
+    //@}
+
+    //!replaces a BCHandler
+    /*!
+      replaces a BCHandler in the vector of bcHandlers at a specified position
+      \param bch: input BCHandler to replace
+      \param position: position
+     */
+    virtual void replace_bch(bchandler_ptrtype& bch, UInt position){};
+
+    //!Applies the correspondent boundary conditions to every block
+    /*!
+      note that this method must be called after blockAssembling(), that sums the coupling conditions to the blocks
+      \param time: time
+     */
+    virtual void applyBoundaryConditions(const Real& time);
+
+    //!Applies the correspondent boundary conditions to a specified block
+    /*!
+      note that this method must be called after blockAssembling(), that sums the coupling conditions to the blocks
+      \param time: time
+      \param block: number of the block
+     */
+    virtual void applyBoundaryConditions(const Real& time, const UInt block);
+
+    //!sums the coupling matrices with the corresponding blocks
+    /*!
+      Everything (but the boundary conditions) must have been set before calling this
+     */
+    virtual void blockAssembling(){}
+
+    //!resets the blocks, boundary conditions, FE spaces.
+    /*!
+     */
+    virtual void resetPrec()
+    {
+        M_blocks.clear();
+        M_bch.clear();
+        M_FESpace.clear();
+        M_offset.clear();
+    }
+
+    //!Applies the robin preconditioners
+    /*!
+      Applies the robin preconditioners when needed, otherwise does nothing
+     */
+    virtual void setRobin(matrix_ptrtype& mat, vector_ptrtype& rhs){}
+
+
+    //! builds the coupling matrix.
+    /*!
+      Computes the matrix that couples 2 blocks using an augmented formulation (Lagrange multipliers).
+      It adds from 1 to 4 coupling blocks, depending on the flag 'coupling'. This flag is an Int, it works as
+      the bash command chmod. It ranges from 1 to 15.
+       \param bigMatrix: the coupling matrix to be built
+       \param coupling: flag specifying what wether to consider all the coupling or to neglect one part
+       \param problem1: FESpace of the first block
+       \param offset1: offset of the first block
+       \param problem2: FESpace of the second block
+       \param offset2: offset of the second block
+       \param locDofMap: std::map with the correspondence between the interface dofs for the two different maps in
+       the subproblems
+       \param numerationInterface vector containing the correspondence of the Lagrange multipliers with the interface dofs
+       \param value value to insert in the coupling blocks
+     */
+    void couplingMatrix(matrix_ptrtype & bigMatrix,
+                        ID coupling,
+                        const std::vector<fespace_ptrtype> problem,
+                        //fespace_ptrtype* problem,
+                        const std::vector<UInt> offset,
+                        //UInt* offset,
+                        const std::map<ID, ID>& locDofMap,
+                        const vector_ptrtype& numerationInterface,
+                        const Real& timeStep=1.e-3,
+                        const Real& value=1.); // not working with non-matching grids
+
+
+    //!applies a Robin preconditioner
+    /*!
+      (only used if the operator is a particular preconditioner, that is AdditiveSchwarz or AdditiveSchwarzRN)
+      applies a Robin preconditioner (mixing the two coupling conditions) to the input matrix, to the rhs, and to the internal
+      preconditioner matrix M_blockPrecs.
+      \param matrix: input matrix (the matrix of the linear system)
+      \param rhsFull: right hand side of the linear system
+     */
+    void robinPreconditioner(matrix_ptrtype& matrix, vector_ptrtype& rhsFull){}
+
+    //!sets the vector of raw pointer to the BCHandler
+    /*!
+      each entry of the vector correspond to a block in the same
+      position in the vector of matrix pointers M_blocks. An arbitrary number of raw pointers can be passed.
+      \param blocks: total number of blocks
+     */
+    void setConditions(std::vector<bchandler_ptrtype>& vec );
+
+
+    //!sets the vector of raw pointer to the FESpaces
+    /*!
+      An arbitrary number of raw pointers can be passed.
+      \param blocks: total number of input FESpaces
+     */
+    void setSpaces    ( std::vector<fespace_ptrtype>& vec );
+
+    //!sets the vector of raw pointer to the offsets of the different blocks
+    /*!
+      An arbitrary number of raw pointers can be passed.
+      \param blocks: total number of input offsets
+     */
+    void setOffsets    ( UInt blocks, .../*fespace_ptrtype ptr1, fespace_ptrtype ptr2*/);
+
+
+    //!computes the Robin coupling matrix
+    /*!
+      Computes a matrix (to be summed to the preconditioner) that mixes the  coupling conditions between the blocks.
+      If the coupling
+      conditions are of Dirichlet and Neumann type (e.g. continuity of velocity and stress) then the preconditioned
+      system will have two Robin conditions instead.
+      \param matrix: the input matrix that will hold the robin coupling;
+      \param alphaf: parameter multiplying the b.c. of the first block;
+      \param alphas: parameter multiplying the b.c. of the second block;
+      \param coupling: flag specifying if we want to neglect part of the coupling (used for preconditioning purposes);
+      \param map: global map of the whole problem;
+      \param FESpace1: FESpace of the first block;
+      \param offset1: offset of the first block in the global matrix;
+      \param FESpace2: FESpace of the second block;
+      \param offset2: offset of the second block in the global matrix;
+      \param locDofMap: std::map with the correspondance between the numeration of the interface in the 2 FE spaces.
+      \param numerationInterface:  vector containing the correspondence of the Lagrange multipliers with the interface dofs
+     */
+    void robinCoupling( BlockInterface::matrix_ptrtype matrix,
+                        Real alphaf,
+                        Real alphas,
+                        UInt coupling,
+                        const BlockInterface::fespace_ptrtype FESpace1,
+                        const UInt& offset1,
+                        const BlockInterface::fespace_ptrtype FESpace2,
+                        const UInt& offset2,
+                        const std::map<ID, ID>& locDofMap,
+                        const BlockInterface::vector_ptrtype& numerationInterface );
+
+protected:
+
+
+    //!sums the coupling matrix in the specified position with the corresponding block
+    /*!
+      Everything (but the boundary conditions) must have been set before calling this
+     */
+    virtual void blockAssembling(const UInt k){}
+
+    //!swaps two boost::shared_ptr. The tamplate argument of the shared_ptr is templated
+    /*!
+      \param operFrom shared_ptr to be swapped
+      \param operTo shared_ptr to be swapped
+     */
+    template <typename Operator>
+    void
+    swap(boost::shared_ptr<Operator>& operFrom, boost::shared_ptr<Operator>& OperTo);
+
+    //ComposedPreconditioner<Epetra_Operator>                      M_blocks;
+    std::vector<matrix_ptrtype>                                  M_blocks;
+    std::vector<bchandler_ptrtype>                               M_bch;
+    std::vector<fespace_ptrtype>                                 M_FESpace;
+    std::vector<UInt>                                            M_offset;
+    vector_ptrtype                                               M_numerationInterface;
+
+    //boost::shared_ptr<OperatorPtr>                M_blockPrec;
+private:
+
+    //!private copy constructor: this class should not be copied.
+    /*!
+      If you need a copy you should implement it, so that it copies the shared pointer one by one, without copying
+      the content.
+     */
+    BlockInterface( matrix_ptrtype& T);
+};
+
+
+typedef singleton<factory<BlockInterface,  std::string> >     BlockPrecFactory;
+
+
+    template <typename Operator>
+    void
+    BlockInterface::swap(boost::shared_ptr<Operator>& operFrom, boost::shared_ptr<Operator>& operTo)
+    {
+        boost::shared_ptr<Operator> tmp = operFrom;
+        operFrom = operTo;
+        operTo = tmp;
+    }
+
+} // Namespace LifeV
+
+#endif /* BLOCKINTERFACE_H */
