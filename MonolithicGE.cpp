5c29d90b41d8c75984965ac8d5aa9a7d812be1fa
crosetto <crosetto>
2010-08-03 16:44:28 +0000

    MonolithicGE class: holding the geometric explicit version of the Monolithic FSI solver

---
+++ MonolithicGE.cpp
@@ -0,0 +1,103 @@
+//@HEADER
+/*
+************************************************************************
+
+ This file is part of the LifeV Applications.
+ Copyright (C) 2001-2010 EPFL, Politecnico di Milano, INRIA
+
+ This library is free software; you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as
+ published by the Free Software Foundation; either version 2.1 of the
+ License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ USA
+
+************************************************************************
+*/
+//@HEADER
+
+/*!
+    @file
+    @brief A short description of the file content
+
+    @author Paolo Crosetto <crosetto@iacspc70.epfl.ch>
+    @date 26 Jul 2010
+
+    A more detailed description of the file (if necessary)
+ */
+
+#include <MonolithicGE.hpp>
+
+namespace LifeV {
+
+// ===================================================
+// Constructors & Destructor
+// ===================================================
+
+
+void MonolithicGE::setupFluidSolid()
+{
+    super::setupFluidSolid( );
+    M_meshMotion.reset(new FSIOperator::meshmotion_raw_type(*M_mmFESpace,
+                                                            *M_epetraComm));
+
+    M_fluid.reset(new FSIOperator::fluid_raw_type(dataFluid(),
+                                                  *M_uFESpace,
+                                                  *M_pFESpace,
+                                                  *M_epetraComm,
+                                                  *M_monolithicMap));
+
+    //             if (isLinearFluid())// to be implemented
+    //                 M_fluidLin.reset(new FSIOperator::fluidlin_raw_type(dataFluid(),
+    //                                                                    *M_uFESpace,
+    //                                                                    *M_pFESpace,
+    //                                                                    *M_epetraComm));
+
+    M_rhs.reset(new vector_type(*this->M_monolithicMap));
+    M_rhsFull.reset(new vector_type(*this->M_monolithicMap));
+    M_beta.reset  (new vector_type(M_uFESpace->map()));
+
+    M_solid.reset(new FSIOperator::solid_raw_type(dataSolid(),
+                                                  *M_dFESpace,
+                                                  *M_epetraComm,
+                                                  *M_monolithicMap,
+                                                  M_offset
+                                                  ));
+
+    //             if (isLinearSolid())// to be implemented with the offset
+    //                 M_solidLin.reset(new FSIOperator::solidlin_raw_type(dataSolid(),
+    //                                                                    *M_dFESpace,
+    //
+    //                                                      *M_epetraComm));
+}
+
+
+namespace
+{
+BlockInterface* createComposedDNND(){ return new ComposedDNND(); }
+BlockInterface* createComposedNN(){ return new ComposedNN(); }
+BlockMatrix*    createAdditiveSchwarz(){ return new BlockMatrix(15); }
+BlockMatrix*    createAdditiveSchwarzRN(){ return new BlockMatrixRN(15); }
+BlockInterface* createComposedDN(){ return new ComposedDN(); }
+BlockInterface* createComposedDN2(){ return new ComposedDN2(); }
+FSIOperator* createM(){ return new MonolithicGE(); }
+}
+
+bool MonolithicGE::reg = FSIFactory::instance().registerProduct( "monolithicGE", &createM ) &&
+    BlockPrecFactory::instance().registerProduct("ComposedDNND"  , &createComposedDNND )&&
+    BlockPrecFactory::instance().registerProduct("ComposedNN"  , &createComposedNN ) &&
+    BlockPrecFactory::instance().registerProduct("AdditiveSchwarz"  , &createAdditiveSchwarz ) &&
+    BlockMatrix::Factory::instance().registerProduct("AdditiveSchwarz"  , &createAdditiveSchwarz) &&
+    BlockPrecFactory::instance().registerProduct("AdditiveSchwarzRN"  , &createAdditiveSchwarzRN ) &&
+    BlockMatrix::Factory::instance().registerProduct("AdditiveSchwarzRN"  , &createAdditiveSchwarzRN ) &&
+    BlockPrecFactory::instance().registerProduct("ComposedDN"  , &createComposedDN ) &&
+    BlockPrecFactory::instance().registerProduct("ComposedDN2"  , &createComposedDN2 );
+} // Namespace LifeV
---
+++ MonolithicGE.hpp
@@ -0,0 +1,120 @@
+//@HEADER
+/*
+************************************************************************
+
+ This file is part of the LifeV Applications.
+ Copyright (C) 2001-2010 EPFL, Politecnico di Milano, INRIA
+
+ This library is free software; you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as
+ published by the Free Software Foundation; either version 2.1 of the
+ License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ USA
+
+************************************************************************
+*/
+//@HEADER
+
+/*!
+    @file
+    @brief A short description of the file content
+
+    @author Paolo Crosetto <crosetto@iacspc70.epfl.ch>
+    @date 26 Jul 2010
+
+    A more detailed description of the file (if necessary)
+ */
+
+#ifndef MONOLITHICGE_H
+#define MONOLITHICGE_H 1
+
+#include <lifemc/lifesolver/BlockMatrix.hpp>
+#include <lifemc/lifesolver/BlockMatrixRN.hpp>
+#include <lifemc/lifesolver/ComposedDN.hpp>
+//#include <lifemc/lifesolver/ComposedRN.hpp>
+#include <lifemc/lifesolver/ComposedDN2.hpp>
+#include <lifemc/lifesolver/ComposedNN.hpp>
+#include <lifemc/lifesolver/ComposedDNND.hpp>
+
+#include <life/lifecore/life.hpp>
+
+#include <lifemc/lifesolver/Monolithic.hpp>
+
+namespace LifeV {
+
+//! monolithicGE - Monolithic Geometry-Explicit solver
+/*!
+  \include ../../testsuite/test_monolithic/fluidstructure.dox
+  @author Paolo Crosetto
+  @see \ref CDFQ
+
+
+ Important parameters to set properly in the data file:
+ - useShapeDerivatives: MUST be false, because in the GE approach the geometry is explicit;
+ - domainVelImplicit: MUST be false, because in the GE approach the geometry is explicit;
+ - convectiveTermDer: false if the convective term is linearized (\f$u^{n+1}\nabla(u^n-w^n)\f$),
+ otherwise it can be either true (if we use the Newton method to solve the convective term nonlinearity) or false
+ (fixed-point method). For the GCE must be false;
+ - semiImplicit:  if true only one iteration of the nonlinear solver is performed. Otherwise
+ the nonlinear iterations continue up to the specified tolerance. Set it to true for the GCE;
+ - method: can be either monolithicGE, monolithicGI if the geometry is treated respectively explicitly or implicitly,
+ or exactJacobians, fixedPoint for partitioned strategies;
+ - blockOper: specifies the matrix type to be used for the linear system: if AdditiveSchwarz, the matrix is the standard
+ ine for GE; if AdditiveSchwarzRN the coupling blocks are of Robin type instead of Dirichlet and Neumann. The parameters
+ for the Robin coupling are alphaf and alphas in the data file. NOTE: this method has currently been tested only for
+ alphas=0.
+ - DDBlockPrec: specifies the possible preconditioners to use. Can be: AdditiveSchwarz, ComposedDN, ComposedDN2,
+ ComposedNN, ComposedDNND.
+
+ */
+class MonolithicGE : public Monolithic
+{
+public:
+
+    typedef Monolithic super;
+    //! @name Constructor & Destructor
+    //@{
+
+    //! Empty Constructor
+    MonolithicGE():
+        super()
+    {}
+
+    //! Destructor
+    ~MonolithicGE()
+    {}
+
+    //@}
+
+
+
+    //! @name Methods
+    //@{
+
+    void setupFluidSolid();
+
+    //@}
+
+private:
+
+    void createOperator( std::string& operType )
+    {
+        M_monolithicMatrix.reset(BlockMatrix::Factory::instance().createObject( operType ));
+    }
+
+    static bool reg;
+    //@}
+};
+
+} // Namespace LifeV
+
+#endif /* MONOLITHICGCE_H */
