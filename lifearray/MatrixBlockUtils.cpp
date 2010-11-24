5df8dc2ef0995f59d37182eb0111ad03534d5db9
grandper <gwenol.grandperrin@epfl.ch>
2010-11-24 16:36:08 +0100

    Implementation of the createUpperTriangularBlock function

--- lifearray/MatrixBlockUtils.cpp
+++ lifearray/MatrixBlockUtils.cpp
@@ -96,6 +96,7 @@ void createZeroBlock ( MatrixBlockView& destBlock )
 void createIdentityBlock ( MatrixBlockView& destBlock )
 {
     // SQUARE TEST
+    // BLOCK PTR TEST
 
     int indexBase(0);
 
@@ -187,14 +188,118 @@ void createDiagBlock ( const MatrixBlockView& srcBlock,
 void createInvDiagBlock ( const MatrixBlockView& srcBlock,
                           MatrixBlockView& destBlock )
 {
-    // Usefull command
-    //M->set_mat_inc(i,j,1/value);
+    // SQUARE TEST
+    // BLOCK COMPATIBILITY TEST
+    // BLOCK PTR TEST
+    // ZERO ON DIAGONAL TEST
+
+    int indexBase(0);
+
+    int destRow(0);
+    int destIndex(0);
+    double diagValue(0.0);
+    int diagIndex(0);
+
+    // Processor informations
+    int  numSrcElements    = srcBlock.getMatrixPtr()->RowMap().NumMyElements();
+    int* srcGlobalElements = srcBlock.getMatrixPtr()->RowMap().MyGlobalElements();
+    int  srcRowElement(0);
+
+    // Source informations handlers
+    int numSrcEntries;
+    double* srcValues;
+    int* srcIndices;
+    int srcRow(0);
+
+    for(int i(0);i<numSrcElements;++i)
+    {
+        // Collecting the data from the source
+        srcRowElement = srcGlobalElements[i];
+
+        // Test if the rows are in the source block
+        if((srcRowElement>=srcBlock.firstRowIndex()) && (srcRowElement<=srcBlock.lastRowIndex()))
+        {
+            // Get the data of the row
+            srcRow = srcBlock.getMatrixPtr()->LRID(srcRowElement+indexBase);
+            srcBlock.getMatrixPtr()->ExtractMyRowView(srcRow, numSrcEntries, srcValues, srcIndices);
+
+            // index of the diagonal
+            diagIndex=srcRowElement-srcBlock.firstRowIndex();
+            diagValue = 0.0;
+            for(int j(0);j<numSrcEntries;++j)
+            {
+                // Test if the coefficient is on the diagonal of the source block
+                if(srcIndices[j]-srcBlock.firstColumnIndex()==diagIndex)
+                {
+                    diagValue = 1/srcValues[j];
+                    j=numSrcEntries; //Exit the loop
+                }
+            }
+
+            destRow   = destBlock.firstRowIndex()+diagIndex+indexBase;
+            destIndex = destBlock.firstColumnIndex()+diagIndex+indexBase;
+            destBlock.getMatrixPtr()->InsertGlobalValues(destRow,1,&diagValue,&destIndex);
+        }
+    }
 }
 
 void createUpperTriangularBlock ( const MatrixBlockView& srcBlock,
                                   MatrixBlockView& destBlock )
 {
+    // SQUARE TEST
+    // BLOCK COMPATIBILITY TEST
+    // BLOCK PTR TEST
 
+    int indexBase(0);
+
+    // Processor informations
+    int  numSrcElements    = srcBlock.getMatrixPtr()->RowMap().NumMyElements();
+    int* srcGlobalElements = srcBlock.getMatrixPtr()->RowMap().MyGlobalElements();
+    int  srcRowElement(0);
+
+    //Offset between the first row/column of the source and destination blocks
+    int rowsOffset(destBlock.firstRowIndex()-srcBlock.firstRowIndex());
+    int columnsOffset(destBlock.firstColumnIndex()-srcBlock.firstColumnIndex());
+
+    // Source informations handlers
+    int numSrcEntries;
+    double* srcValues;
+    int* srcIndices;
+    int srcRow(0);
+
+    for(int i(0);i<numSrcElements;++i)
+    {
+        // Collecting the data from the source
+        srcRowElement = srcGlobalElements[i];
+
+        // Test if the rows are in the source block
+        if((srcRowElement>=srcBlock.firstRowIndex()) && (srcRowElement<=srcBlock.lastRowIndex()))
+        {
+            // Get the data of the row
+            srcRow = srcBlock.getMatrixPtr()->LRID(srcRowElement+indexBase);
+            srcBlock.getMatrixPtr()->ExtractMyRowView(srcRow, numSrcEntries, srcValues, srcIndices);
+
+            int destIndices[numSrcEntries];
+            double destValues[numSrcEntries];
+            int numDestEntries(0);
+            int destRow(destBlock.getMatrixPtr()->LRID(i+rowsOffset+indexBase));
+            for(int j(0);j<numSrcEntries;++j)
+            {
+                // Test if the coefficient is:
+                // a) in the block, and
+                // b) on the upper triangular part of the block.
+                if((srcIndices[j]>=srcBlock.firstColumnIndex()) &&
+                   (srcIndices[j]<=srcBlock.lastColumnIndex()) &&
+                   (srcIndices[j]-srcBlock.firstColumnIndex()>=srcRowElement-srcBlock.firstRowIndex()))
+                {
+                    destIndices[numDestEntries] = srcBlock.getMatrixPtr()->GCID(srcIndices[j])+columnsOffset;
+                    destValues[numDestEntries] = srcValues[j];
+                    numDestEntries++;
+                }
+            }
+            destBlock.getMatrixPtr()->InsertGlobalValues(destRow,numDestEntries,destValues,destIndices);
+        }
+    }
 }
 
 void createLowerTriangularBlock ( const MatrixBlockView& srcBlock,
