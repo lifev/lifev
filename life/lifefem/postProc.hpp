1db87da3f9729e267fd4067e98f3318e41677d2a
simone <simone@tsf-wpa-1-0068.epfl.ch>
2010-11-30 17:37:20 +0100

    removed vecUnknown

--- life/lifefem/postProc.hpp
+++ life/lifefem/postProc.hpp
@@ -33,7 +33,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include <sstream>
 #include <life/lifecore/GetPot.hpp>
 #include <life/lifecore/life.hpp>
-#include <life/lifearray/vecUnknown.hpp>
 
 namespace LifeV
 {
