395f069532c2f2d9e0f1437fc3eb2bb6036b30e3
Radu Popescu <radu.popescu@epfl.ch>
2010-12-11 16:37:12 +0100

    Chrono class - implementation cleaned

--- life/lifecore/chrono.hpp
+++ life/lifecore/chrono.hpp
@@ -1,83 +1,137 @@
+//@HEADER
 /*
-This file is part of the LifeV library
-Copyright (C) 2001,2002,2003,2004 EPFL, INRIA and Politecnico di Milano
+*******************************************************************************
 
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
+Copyright (C) 2004, 2005, 2007 EPFL, Politecnico di Milano, INRIA
+Copyright (C) 2010 EPFL, Politecnico di Milano, Emory University
 
-This library is distributed in the hope that it will be useful,
+This file is part of LifeV.
+
+LifeV is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+LifeV is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 Lesser General Public License for more details.
 
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+You should have received a copy of the GNU Lesser General Public License
+along with LifeV.  If not, see <http://www.gnu.org/licenses/>.
+
+*******************************************************************************
 */
-#ifndef _CHRONO_H_INCLUDE
-#define _CHRONO_H_INCLUDE
+//@HEADER
+/*!
+  @file
+  @brief Chronometer and fake chronometer class class
+
+  @date 11-12-2010
+  @author
+
+  @contributor Radu Popescu <radu.popescu@epfl.ch>
+  @maintainer Radu Popescu <radu.popescu@epfl.ch>
+*/
+
+#ifndef CHRONO_H
+#define CHRONO_H
 
 #include <ctime>
 
 namespace LifeV
 {
+
+//! @name Chrono - chronometer class
+/*!
+  This class is used for timing sections of code
+*/
 class Chrono
 {
 public:
-    Chrono()
-            :
-            _t1(0), _t2(0), _dt(0),
-            running(false)
+
+    //! @name Constructor
+    //@{
+    Chrono() :
+            M_t1(0),
+            M_t2(0),
+            M_dt(0),
+            M_running(false)
     {
     }
+    //@}
+
+    //! @name Public methods
+    //@{
+    //! Reset the timer
     void Reset()
     {
-        _dt = 0;
-        running = false;
+        M_dt = 0;
+        M_running = false;
     }
+
+    //! Start the timer
     void start()
     {
-        _t1 = clock();
-        running = true;
-    };
+        M_t1 = clock();
+        M_running = true;
+    }
+
+    //! Stop the timer
     void stop()
     {
-        if (running)
+        if (M_running)
         {
-            _t2 = clock();
-            _dt += _t2 - _t1;
-            running = false;
+            M_t2 = clock();
+            M_dt += M_t2 - M_t1;
+            M_running = false;
         }
-    };
+    }
+
+    //! Compute the difference in time between start and stop
     double diff()
     {
-        if (running)
-            return ( 1. * ( clock() - _t1 ) ) / CLOCKS_PER_SEC;
+        if (M_running)
+            return ( 1. * ( clock() - M_t1 ) ) / CLOCKS_PER_SEC;
 
-        return ( 1. * ( _t2 - _t1 ) ) / CLOCKS_PER_SEC;
-    };
+        return ( 1. * ( M_t2 - M_t1 ) ) / CLOCKS_PER_SEC;
+    }
+
+    //! Return a cumulative time difference
     double diff_cumul()
     {
-        if (running)
-            return ( 1. * ( _dt +  clock() - _t1 ) ) / CLOCKS_PER_SEC;
+        if (M_running)
+            return ( 1. * ( M_dt +  clock() - M_t1 ) ) / CLOCKS_PER_SEC;
 
-        return ( 1. * _dt / CLOCKS_PER_SEC );
-    };
+        return ( 1. * M_dt / CLOCKS_PER_SEC );
+    }
+    //@}
 
 private:
-    clock_t _t1, _t2, _dt;
-    bool    running;
-
+    //! @name Private members
+    //@{
+    clock_t M_t1, M_t2, M_dt;
+    bool    M_running;
+    //@}
 };
 
+//! @name ChronoFake - dummy chronometer class
+/*!
+  When the diff and diff_cumul methods are called, always
+  returns -1.0
+*/
 class ChronoFake
 {
 public:
+    //! @name Constructor
+    //@{
     ChronoFake()
     {
     }
+    //@}
+
+    //! @name Public methods
+    //@{
     void Reset()
     {
     }
@@ -95,9 +149,9 @@ public:
     {
         return -1.;
     };
-
+    //@}
 };
 
 }
-#endif
+#endif // CHRONO_H
 
