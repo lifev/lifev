ac2a80946a4034533100baa7f6cd33f5fdd605bb
quinodoz <samuel.quinodoz@epfl.ch>
2010-12-13 09:17:15 +0100

    Modification by Claudia C.

--- life/lifefem/bdfNS_template.hpp
+++ life/lifefem/bdfNS_template.hpp
@@ -1,74 +1,165 @@
+//@HEADER
 /*
- This file is part of the LifeV library
- Copyright (C) 2001,2002,2003,2004 EPFL, INRIA and Politecnico di Milano
+*******************************************************************************
 
- This library is free software; you can redistribute it and/or
- modify it under the terms of the GNU Lesser General Public
- License as published by the Free Software Foundation; either
- version 2.1 of the License, or (at your option) any later version.
+    Copyright (C) 2004, 2005, 2007 EPFL, Politecnico di Milano, INRIA
+    Copyright (C) 2010 EPFL, Politecnico di Milano, Emory University
 
- This library is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- Lesser General Public License for more details.
+    This file is part of LifeV.
 
- You should have received a copy of the GNU Lesser General Public
- License along with this library; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+    LifeV is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    LifeV is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with LifeV.  If not, see <http://www.gnu.org/licenses/>.
+
+*******************************************************************************
 */
+//@HEADER
+
 /*!
-  \file bdfNS.h
-  \author A. Veneziani
-  \date 04/2003
-  \version 1.0
+    @file
+    @brief File containing a class for an easy handling of different order time 
+            discretizations/extrapolations BDF based specific for the Navier-Stokes problem
 
-  \brief File containing a class for an easy handling of different order time discretizations/extrapolations
-         BDF based SPECIFIC FOR THE NAVIER-STOKES PROBLEM
-         The idea is to couple a BDF of order q with a pressure incremental approach of order q-1 (see Van Kan, Prohl, Guermond, ecc.)
-         If q=1, we still have an incremental pressure treatment (see Guermond)
-         REM: At the moment, the couple BDF of order q + extrapolation of order q seems unstable (why ?)
-*/
+    @date 01-04-2003
+    @author Alessandro Veneziani <ale@mathcs.emory.edu>
+
+    @contributor Laura Cattaneo
+    @mantainer Laura Cattaneo
+    
+ */
+
 #ifndef _BDF_NS_TEMPLATE_H
 #define _BDF_NS_TEMPLATE_H
+
 #include <string>
 #include <iostream>
 #include <algorithm>
+
 #include <life/lifecore/GetPot.hpp>
-#include <life/lifecore/life.hpp>
 #include <life/lifefem/bdf_template.hpp>
+
 
 namespace LifeV
 {
+
+//! \class BdfTNS 
+/*!
+    @author Alessandro Veneziani
+    @see Van Kan, Prohl, Guermond
+
+    This class implements an easy handling of different order time discretizations/extrapolations
+    Bdf based specific for the Navier-Stokes problem.
+
+    The idea is to couple a Bdf of order q with a pressure incremental approach of order q-1.
+
+    If q=1, we still have an incremental pressure treatment.
+
+    At the moment, the couple BDF of order q + extrapolation of order q seems unstable
+
+ */
+
 template<typename VectorType = EpetraVector >
 class BdfTNS
 {
 public:
-    // ! Constructor
-    BdfTNS( const UInt n );
-
-    ~BdfTNS() {}
-
-    inline BdfT<VectorType>& bdf_u() { return _bdf_u; }
-    inline BdfT<VectorType>& bdf_p() { return _bdf_p; }
 
 
+    //! @name Public Types
+    //@{
+
+    typedef VectorType                          vector_Type;
+
+    //@}
+
+
+    //! @name Constructor & Destructor
+    //@{
+
+    //! Constructor
+    /*!
+        @param order Order q of the Bdf
+     */
+    BdfTNS( const UInt order );
+
+    //! Destructor
+    ~BdfTNS( ) { }
+
+    //@}
+
+
+    //! @name Methods
+    //@{
+
+    //! Show the contents of the object
+    void showMe() const;
+
+    //@}
+
+
+    //! @name Get Methods
+    //@{
+
+    //! The method returns the Bdf velocity
+    /*!
+        @return Reference to a new Bdf template which holds the velocity field
+     */
+    BdfT<VectorType>& bdf_u( ) { return M_bdfVelocity; }
+
+    //! The method returns the Bdf pressure
+    /*!
+        @return Reference to a new Bdf template which holds the pressure
+     */
+    BdfT<VectorType>& bdf_p( ) { return M_bdfPressure; }
+
+    //@}
 
 private:
-    BdfT<VectorType> _bdf_u;
-    BdfT<VectorType> _bdf_p;
+
+    //! Bdf velocity
+    BdfT<VectorType> M_bdfVelocity; 
+
+    //! Bdf pressure
+    BdfT<VectorType> M_bdfPressure;
 };
 
 
+// ===================================================
+// Constructors 
+// ===================================================
 template<typename VectorType>
-BdfTNS<VectorType>::BdfTNS( const UInt n )
+BdfTNS<VectorType>::BdfTNS( const UInt order )
         :
-        _bdf_u( n ),
-        _bdf_p( std::max( UInt( 1 ), n - 1 ) )
+        M_bdfVelocity( order ),
+        M_bdfPressure( std::max( UInt( 1 ), order - 1 ) )
 {
-    _bdf_u.setup(n,1);
-    _bdf_p.setup( std::max( UInt( 1 ), n - 1 ), 1 );
+    M_bdfVelocity.setup( order, 1 );
+    M_bdfPressure.setup( std::max( UInt( 1 ), order - 1 ), 1 );
 }
 
 
+// ===================================================
+// Methods
+// ===================================================
+template<typename VectorType>
+void
+BdfTNS<VectorType>::showMe() const
+{
+    std::cout << " *** Bdf velocity: " << std::endl;
+    M_bdfVelocity.showMe();
+    std::cout << " *** Bdf pressure: " << std::endl;
+    M_bdfPressure.showMe();
 }
-#endif
+
+
+}// Namespace LifeV
+
+#endif /* _BDF_NS_TEMPLATE_H */
