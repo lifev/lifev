6e9b630c8377e85fa46f452d66af5c0cef10ec47
Radu Popescu <radu.popescu@epfl.ch>
2010-12-13 10:50:35 +0100

    Cleaning and reordering singleton.hpp

--- life/lifecore/singleton.hpp
+++ life/lifecore/singleton.hpp
@@ -1,40 +1,47 @@
-/* -*- mode: c++ -*-
+//@HEADER
+/*
+*******************************************************************************
 
-This file is part of the LifeV library
+Copyright (C) 2004, 2005, 2007 EPFL, Politecnico di Milano, INRIA
+Copyright (C) 2010 EPFL, Politecnico di Milano, Emory University
 
-Author(s): Christophe Prud'homme <prudhomm@mit.edu>
-     Date: 2004-09-10
+This file is part of LifeV.
 
-Copyright (C) 2004 EPFL
+LifeV is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
-
-This library is distributed in the hope that it will be useful,
+LifeV is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 Lesser General Public License for more details.
 
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
-/**
-   \file Singleton.hpp
-   \author Christophe Prud'homme <christophe.prudhomme@epfl.ch>
-   \date 2004-09-10
- */
-#ifndef __Singleton_H
-#define __Singleton_H 1
+You should have received a copy of the GNU Lesser General Public License
+along with LifeV.  If not, see <http://www.gnu.org/licenses/>.
 
-#include <cstdlib>
-#include <cassert>
-#include <exception>
-#include <stdexcept>
+*******************************************************************************
+*/
+//@HEADER
+/*!
+  @file
+  @brief Singleton pattern class
+
+  @date 10-09-2004
+  @author Christophe Prud'homme <christophe.prudhomme@epfl.ch>
+
+  @maintainer Radu Popescu <radu.popescu@epfl.ch>
+*/
+
+#ifndef SINGLETON_H
+#define SINGLETON_H 1
+
 #include <algorithm>
+#include <cassert>
+#include <cstdlib>
+#include <exception>
 #include <new>
+#include <stdexcept>
 
 #include <life/lifecore/policy.hpp>
 
@@ -47,28 +54,36 @@ namespace LifeV
    A Singleton pattern implementation using the ideas
    from Alexandrescu's book "modern C++ design"
    http://www.moderncppdesign.com/
-
-
-   \author Christophe Prud'homme <christophe.prudhomme@epfl.ch>
 */
 template <typename T>
 class singleton
 {
 public:
-
+    //! @name Public typedefs
+    //@{
     typedef T singleton_type;
-
     typedef policyLifeTimeDefault<singleton_type> lifetime_policy;
     typedef policyCreationUsingNew<singleton_type> creation_policy;
+    //@}
 
-
+    //! @name Public static methods
+    //@{
     /**
        return the instance of the singleton
     */
     static singleton_type& instance();
-
+    //@}
 private:
-    // Helpers
+    //! @name Private typedefs
+    //@{
+    typedef singleton_type* instance_type;
+    //@}
+
+    // Disable the constructor
+    singleton();
+
+    //! @name Private static methods
+    //@{
     static void makeInstance();
 
     /**
@@ -76,29 +91,23 @@ private:
     */
     static void destroySingleton();
 
-    // Protection
-    singleton();
-
-    // Data
-    typedef singleton_type* instance_type;
+    //@}
     static instance_type _S_instance;
     static bool _S_destroyed;
 };
 
-//
-// Instantiate Singleton's static data
-//
+// ===================================
+// Singleton Implementation
+// ===================================
+
 template <class T>
 typename singleton<T>::instance_type singleton<T>::_S_instance;
 
 template <class T>
 bool singleton<T>::_S_destroyed;
 
-
 template <class T>
-inline
-T&
-singleton<T>::instance()
+inline T& singleton<T>::instance()
 {
     if ( !_S_instance )
     {
@@ -107,10 +116,8 @@ singleton<T>::instance()
     return *_S_instance;
 }
 
-
 template <class T>
-void
-singleton<T>::makeInstance()
+void singleton<T>::makeInstance()
 {
     if ( !_S_instance )
     {
@@ -125,13 +132,14 @@ singleton<T>::makeInstance()
 }
 
 template <class T>
-void
-singleton<T>::destroySingleton()
+void singleton<T>::destroySingleton()
 {
     assert( !_S_destroyed );
     creation_policy::destroy( _S_instance );
     _S_instance = 0;
     _S_destroyed = true;
 }
-}
-#endif /* __singleton_HPP */
+
+} // Namespace LifeV
+
+#endif // SINGLETON_H
