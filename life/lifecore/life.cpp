62904d58d42b13d059407b7a476371df42c3e66d
simone <simone@iacspc80.epfl.ch>
2010-12-13 10:03:55 +0100

    cleaning week 1: license and doxygen

--- life/lifecore/life.cpp
+++ life/lifecore/life.cpp
@@ -1,30 +1,36 @@
-/* -*- mode: c++ -*-
+//@HEADER
+/*
+*******************************************************************************
 
-  This file is part of the LifeV library
+    Copyright (C) 2004, 2005, 2007 EPFL, Politecnico di Milano, INRIA
+    Copyright (C) 2010 EPFL, Politecnico di Milano, Emory University
 
-  Author(s): Simone Deparis <simone.deparis@epfl.ch>
-       Date: 2009-12-09
+    This file is part of LifeV.
 
-  Copyright (C) 2009 EPFL
+    LifeV is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
+    LifeV is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
 
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
+    You should have received a copy of the GNU Lesser General Public License
+    along with LifeV.  If not, see <http://www.gnu.org/licenses/>.
 
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*******************************************************************************
 */
-/**
-   \file life.cpp
-   \author Simone Deparis <simone.deparis@epfl.ch>
-   \date 2009-12-09
+//@HEADER
+/*!
+    @file
+    @brief definition of nDimensions
+
+    @author Simone Deparis <simone.deparis@epfl.ch>
+
+    @date 2009-12-09
+
  */
 
 #include <life/lifecore/life.hpp>
