ab0dcca2fd74d9ff96bd5996e6ef79dc703c1d66
Cristiano Malossi <cristiano.malossi@epfl.ch>
2010-12-15 14:46:58 +0100

    Cleaning weeks: container of vector cleaning phase 2.

--- TestFunction.hpp
+++ TestFunction.hpp
@@ -59,113 +59,113 @@ TestFunction( boost::shared_ptr<VectorType> A1, boost::shared_ptr<VectorType> B1
     V1->push_back( A1 );
     V1->push_back( B1 );
     std::cout << "V1" << std::endl;
-    V1->ShowMe();
+    V1->showMe();
 
     V2.reset( new ContainerOfBaseVectors() );
     V2->push_back( A2 );
     V2->push_back( B2 );
     std::cout << "V2" << std::endl;
-    V2->ShowMe();
+    V2->showMe();
 
     V3.reset( new ContainerOfBaseVectors() );
     V3->push_back( A3 );
     V3->push_back( B3 );
     std::cout << "V3" << std::endl;
-    V3->ShowMe();
+    V3->showMe();
 
     V4.reset( new ContainerOfBaseVectors() );
     V4->push_back( A4 );
     V4->push_back( B4 );
     std::cout << "V4" << std::endl;
-    V4->ShowMe();
+    V4->showMe();
 
     // Test operator= (initialize a vector as a copy of another vector)
     ContainerOfBaseVectors_ptr VV1, VV2, VV3;
     VV1.reset( new ContainerOfBaseVectors() );
     *VV1 = *V1;
     std::cout << "VV1 = V1" << std::endl;
-    VV1->ShowMe();
+    VV1->showMe();
 
     // Test operator= (for initialize the vector with a scalar - cannot be done for an empty vector!)
     *VV1 = scalar;
     std::cout << "VV1 = 1.0" << std::endl;
-    VV1->ShowMe();
+    VV1->showMe();
 
     // Test operator+=
     *VV1 += *V1;
     std::cout << "VV1 += V1" << std::endl;
-    VV1->ShowMe();
+    VV1->showMe();
 
     // Test operator-
     *VV1 = *VV1 - *V2;
     std::cout << "VV1 = VV1 - V2" << std::endl;
-    VV1->ShowMe();
+    VV1->showMe();
 
     // Test operator*= (multiplication: scalar * vector)
     scalar = 1.23456789;
     *VV1 = *V1;
     *VV1 = scalar * *V1;
     std::cout << "VV1 = 1.23456789 * V1" << std::endl;
-    VV1->ShowMe();
+    VV1->showMe();
 
     // Test operator* (scalar product multiplication)
-    scalar = VV1->Dot( *V1 );
+    scalar = VV1->dot( *V1 );
     std::cout << "scalarProduct = VV1.Dot(V1) = " << scalar << std::endl << std::endl;
 
     // Concatenate two vector of vectors
     VV1->push_back( *V1 );
     VV1->push_back( *V2 );
     std::cout << "VV1->push_back( V1 ); VV1->push_back( V2 )" << std::endl;
-    VV1->ShowMe();
+    VV1->showMe();
 
     VV2.reset( new ContainerOfBaseVectors() );
     VV2->push_back( *V3 );
     VV2->push_back( *V4 );
     VV2->push_back( *V3 );
     std::cout << "VV2->push_back( V3 ); VV2->push_back( V4 ); VV2->push_back( V3 )" << std::endl;
-    VV2->ShowMe();
+    VV2->showMe();
 
     // Element by element Multiplication
     std::cout << "VV2 *= VV1" << std::endl;
     *VV2 *= *VV1;
-    VV2->ShowMe();
+    VV2->showMe();
     std::cout << "VV1 =" << std::endl;
-    VV1->ShowMe();
+    VV1->showMe();
 
     // Element by element Division
     std::cout << "VV2 /= VV1" << std::endl;
     *VV2 /= *VV1;
-    VV2->ShowMe();
+    VV2->showMe();
     std::cout << "VV1 =" << std::endl;
-    VV1->ShowMe();
+    VV1->showMe();
 
     // Element by element Multiplication (NaN is interpreted as a zero)
     std::cout << "VV2 && VV1" << std::endl;
-    (*VV2 && *VV1).ShowMe();
+    (*VV2 && *VV1).showMe();
 
     // Element by element Comparison ==
     std::cout << "VV1 == 0" << std::endl;
     VV3.reset( new ContainerOfBaseVectors() );
     *VV3 = ( *VV1 == 0.0 );
-    VV3->ShowMe();
+    VV3->showMe();
 
     // Element by element Comparison >=
     std::cout << "VV1 >= 2.2" << std::endl;
     *VV3 = ( *VV1 >= 2.2 );
-    VV3->ShowMe();
+    VV3->showMe();
 
     // Abs
     std::cout << "VV2 *= -1" << std::endl;
     scalar = -1.0;
     *VV2 *= scalar;
-    VV2->ShowMe();
+    VV2->showMe();
 
     std::cout << "VV2->abs( *VV3 )" << std::endl;
-    VV2->Abs( *VV3 );
+    VV2->abs( *VV3 );
     std::cout << "VV2:" << std::endl;
-    VV2->ShowMe();
+    VV2->showMe();
     std::cout << "VV3:" << std::endl;
-    VV3->ShowMe();
+    VV3->showMe();
 
 
 
@@ -177,18 +177,18 @@ TestFunction( boost::shared_ptr<VectorType> A1, boost::shared_ptr<VectorType> B1
     UInt pos = 2;
     (*VV1)( pos ) = B1;
     std::cout << "VV1( 2 ) = B1" << std::endl;
-    VV1->ShowMe();
+    VV1->showMe();
 
     // Compute the weight norm2 of the vector
-    VV1->WeightNorm2();
-    std::cout << "WeightNorm2(VV1) = " << VV1->WeightNorm2() << std::endl << std::endl;
+    VV1->weightNorm2();
+    std::cout << "WeightNorm2(VV1) = " << VV1->weightNorm2() << std::endl << std::endl;
 
     // operator!
     std::cout << "!VV1 " << std::endl;
-    (!(*VV1)).ShowMe();
+    (!(*VV1)).showMe();
 
     std::cout << "!VV3 " << std::endl;
-    (!(*VV3)).ShowMe();
+    (!(*VV3)).showMe();
 
     return EXIT_SUCCESS;
 }
