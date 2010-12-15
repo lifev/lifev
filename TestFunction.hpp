79496a78016e2ca020cba3d535639a04f35b12c8
Cristiano Malossi <cristiano.malossi@epfl.ch>
2010-12-15 18:18:24 +0100

    Cleaning week: cleaning a little bit the mathcard testsuite. Removed a lot of warnings.

--- TestFunction.hpp
+++ TestFunction.hpp
@@ -1,43 +1,57 @@
-/* -*- mode: c++ -*-
+//@HEADER
+/*
+*******************************************************************************
 
-  This file is part of the LifeV Applications.
+    Copyright (C) 2004, 2005, 2007 EPFL, Politecnico di Milano, INRIA
+    Copyright (C) 2010 EPFL, Politecnico di Milano, Emory University
 
-  Author(s): Cristiano Malossi <cristiano.malossi@epfl.ch>
-       Date: 2009-10-06
+    This file is part of LifeV.
 
-  Copyright (C) 2009 EPFL
+    LifeV is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2.1 of the License, or
-  (at your option) any later version.
+    LifeV is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
 
-  This program is distributed in the hope that it will be useful, but
-  WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  General Public License for more details.
+    You should have received a copy of the GNU Lesser General Public License
+    along with LifeV.  If not, see <http://www.gnu.org/licenses/>.
 
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-  USA
+*******************************************************************************
 */
-/**
-   \file TestFunction.hpp
-   \author Cristiano Malossi <cristiano.malossi@epfl.ch>
-   \date 2009-10-06
+//@HEADER
+
+/*!
+ *  @file
+ *  @brief File containing the Vector Container Test
+ *
+ *  @date 29-09-2009
+ *  @author Cristiano Malossi <cristiano.malossi@epfl.ch>
+ *
+ *  @maintainer Cristiano Malossi <cristiano.malossi@epfl.ch>
  */
+
+// Tell the compiler to ignore specific kind of warnings:
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
 
 #include <iomanip>
 #include <string>
 
-#include <life/lifecore/life.hpp>
-
-#include <lifemc/lifearray/VectorContainer.hpp>
-
 #include <boost/shared_ptr.hpp>
 
+// Tell the compiler to restore the warning previously silented
+#pragma GCC diagnostic warning "-Wunused-variable"
+#pragma GCC diagnostic warning "-Wunused-parameter"
 
+// LifeV includes
+#include <life/lifecore/life.hpp>
+
+// Mathcard includes
+#include <lifemc/lifearray/VectorContainer.hpp>
 
 using namespace LifeV;
 
@@ -49,7 +63,7 @@ TestFunction( boost::shared_ptr<VectorType> A1, boost::shared_ptr<VectorType> B1
               boost::shared_ptr<VectorType> A4, boost::shared_ptr<VectorType> B4 )
 {
     //CONTAINER OF BASE VECTORS
-    typedef ContainerOfVectors< VectorType >             ContainerOfBaseVectors;
+    typedef VectorContainer< VectorType >                ContainerOfBaseVectors;
     typedef boost::shared_ptr<ContainerOfBaseVectors>    ContainerOfBaseVectors_ptr;
 
     ContainerOfBaseVectors_ptr V1, V2, V3, V4;
--- main.cpp
+++ main.cpp
@@ -1,55 +1,61 @@
 //@HEADER
 /*
-************************************************************************
+*******************************************************************************
 
- This file is part of the LifeV Applications.
- Copyright (C) 2001-2009 EPFL, Politecnico di Milano, INRIA
+    Copyright (C) 2004, 2005, 2007 EPFL, Politecnico di Milano, INRIA
+    Copyright (C) 2010 EPFL, Politecnico di Milano, Emory University
 
- This library is free software; you can redistribute it and/or modify
- it under the terms of the GNU Lesser General Public License as
- published by the Free Software Foundation; either version 2.1 of the
- License, or (at your option) any later version.
+    This file is part of LifeV.
 
- This library is distributed in the hope that it will be useful, but
- WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- Lesser General Public License for more details.
+    LifeV is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
- You should have received a copy of the GNU Lesser General Public
- License along with this library; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
- USA
+    LifeV is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
 
-************************************************************************
+    You should have received a copy of the GNU Lesser General Public License
+    along with LifeV.  If not, see <http://www.gnu.org/licenses/>.
+
+*******************************************************************************
 */
 //@HEADER
 
 /*!
- * @file
- * @brief ContainerOfVectors Test
+ *  @file
+ *  @brief File containing the Vector Container Test
  *
- * @author Cristiano Malossi <cristiano.malossi@epfl.ch>
- * @date 29-09-2009
+ *  @date 29-09-2009
+ *  @author Cristiano Malossi <cristiano.malossi@epfl.ch>
  *
- *  This is a test to verify that the ContainerOfVectors works correctly.
+ *  @maintainer Cristiano Malossi <cristiano.malossi@epfl.ch>
+ *
+ *  This is a test to verify that the VectorContainer works correctly.
  */
 
+// Tell the compiler to ignore specific kind of warnings:
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+
+#include <boost/numeric/ublas/vector.hpp>
+
 #include <Epetra_ConfigDefs.h>
-#ifdef HAVE_MPI
+#ifdef EPETRA_MPI
 #include <mpi.h>
 #endif
 
-#include <iomanip>
-#include <string>
+// Tell the compiler to restore the warning previously silented
+#pragma GCC diagnostic warning "-Wunused-variable"
+#pragma GCC diagnostic warning "-Wunused-parameter"
 
-#include <life/lifecore/life.hpp>
-
+// LifeV includes
 #include <life/lifealg/EpetraMap.hpp>
 #include <life/lifearray/EpetraVector.hpp>
 
-#include <boost/numeric/ublas/vector.hpp>
-#include <boost/shared_ptr.hpp>
-
+// Mathcard includes
 #include "TestFunction.hpp"
 
 using namespace LifeV;
