a4d07ac9eebc466ce34484e88d514dda3c928a08
grandper <gwenol.grandperrin@epfl.ch>
2010-12-21 09:18:11 +0100

    Cleaning weeks: merge operations

--- FSIMonolithic.hpp
+++ FSIMonolithic.hpp
@@ -113,7 +113,7 @@ public:
 
     typedef MonolithicBlockMatrix                              blockMatrix_Type;
     typedef boost::shared_ptr<blockMatrix_Type>                blockMatrixPtr_Type;
-    typedef SolverTrilinos                                     solver_Type;
+    typedef SolverAztecOO                                      solver_Type;
     //@}
 
     //! OBSOLETE typedefs
