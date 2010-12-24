bb1d1e021c6372a779fecc4c276c733a8ed657d0
Simone Deparis <Simone.Deparis@epfl.ch>
2010-12-24 01:00:16 +0100

    moved file name

--- life/lifesolver/FSIModelFixedPoint.cpp
+++ life/lifesolver/FSIModelFixedPoint.cpp
@@ -17,7 +17,7 @@
    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
-#include <life/lifesolver/fixedPointBase.hpp>
+#include <life/lifesolver/FSIModelFixedPoint.hpp>
 
 namespace LifeV
 {
