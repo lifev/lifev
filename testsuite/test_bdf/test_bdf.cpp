595faa173f231dc25336864cdd897c402ee7185b
fadel <nur.fadel@mail.polimi.it>
2011-03-08 15:22:29 +0100

    bdf fix

--- testsuite/test_bdf/test_bdf.cpp
+++ testsuite/test_bdf/test_bdf.cpp
@@ -217,7 +217,7 @@ void test_bdf::run()
     bdf.setup(ord_bdf);
 
     //Initialization
-    bdf.setInitialCondition(AnalyticalSol::u, u, FeSpace, t0, delta_t);
+    bdf.setInitialCondition<Real(*) (Real, Real, Real, Real, UInt), FESpace<RegionMesh, MapEpetra> >(AnalyticalSol::u, u, FeSpace, t0, delta_t);
     if (verbose) bdf.showMe();
     Members->comm->Barrier();
 
