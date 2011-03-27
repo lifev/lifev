daa5d1b26b7b471a9e3daec64b85489c65605117
Cristiano Malossi <cristiano.malossi@epfl.ch>
2011-03-27 13:37:17 +0200

    Fixing night check: relaxing tolerance from 1e-10 to 1e-8.

--- MultiscaleSolver.cpp
+++ MultiscaleSolver.cpp
@@ -210,7 +210,7 @@ MultiscaleSolver::solveProblem( const Real& referenceSolution )
     if ( M_model->type() == Multiscale )
     {
         Real computedSolution( M_algorithm->couplingVariables()->norm2() );
-        if ( referenceSolution >= 0. && std::abs( referenceSolution - computedSolution ) > 1e-10 )
+        if ( referenceSolution >= 0. && std::abs( referenceSolution - computedSolution ) > 1e-8 )
             multiscaleErrorCheck( Solution, "Algorithm Solution: "  + number2string( computedSolution ) +
                                             " (External Residual: " + number2string( referenceSolution ) + ")\n", M_displayer->isLeader() );
     }
