dc7b2af922c5b991ca033d746ef0b399f508cae3
perego <perego.mauro@gmail.com>
2011-05-03 14:22:39 -0400

    added/modified methods and members so that the mesh entities can be handled using polytopes notation. The advantage of using polytope notation is that usualli polytopes share many properties both in 3D and 2D. As an example, facets have adjiacencies both in 2D and 3D.
    Renamed types in order to comply with the coding guidelines.

--- life/lifemesh/RegionMesh3D.cpp
+++ life/lifemesh/RegionMesh3D.cpp
@@ -39,13 +39,13 @@ namespace LifeV
 {
 void set_switches_for_regionmesh( Switch & sw )
 {
-    sw.create( "HAS_ALL_FACES" );
-    sw.create( "HAS_ALL_EDGES" );
-    sw.create( "HAS_BOUNDARY_FACES" );
-    sw.create( "HAS_BOUNDARY_EDGES" );
-    sw.create( "HAS_VOLUME_TO_FACES" );
-    sw.create( "HAS_VOLUME_TO_EDGES" );
+    sw.create( "HAS_ALL_FACETS" );
+    sw.create( "HAS_ALL_RIDGES" );
+    sw.create( "HAS_BOUNDARY_FACETS" );
+    sw.create( "HAS_BOUNDARY_RIDGES" );
+    sw.create( "HAS_ELEMENT_TO_FACETS" );
+    sw.create( "HAS_ELEMENT_TO_RIDGES" );
     sw.create( "HAS_BEEN_CHECKED" );
-    sw.create( "FACES_HAVE_ADIACENCY" );
+    sw.create( "FACETS_HAVE_ADIACENCY" );
 }
 }
