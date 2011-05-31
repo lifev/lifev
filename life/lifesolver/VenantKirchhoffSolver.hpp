935bf7626a236cc31d92d24527d4d483f37f5d77
paolo.crosetto <paolo.crosetto@epfl.ch>
2011-05-31 09:33:31 +0200

    disabling the allocation by default of the matrix M_massStiff

--- life/lifesolver/VenantKirchhoffSolver.hpp
+++ life/lifesolver/VenantKirchhoffSolver.hpp
@@ -449,11 +449,6 @@ protected:
 
   UInt getDim() const { return M_FESpace->dim(); }
 
-  //Deprecated
-  UInt __attribute__ ((__deprecated__))dim() const
-  { return getDim(); }
-
-
   //!Protected Members
 
   boost::shared_ptr<data_Type>   M_data;
@@ -567,12 +562,12 @@ VenantKirchhoffSolver<Mesh, SolverType>::VenantKirchhoffSolver( ):
   M_BCh                        (),
   M_localMap                   ( ),
   M_mass                       ( ),
-  M_massStiff                  ( /*new matrix_Type(monolithicMap) */),//constructed outside
+  M_massStiff                  ( /*new matrix_Type(monolithicMap)*/ ),//constructed outside
   M_stiff                      ( /*new matrix_Type(M_localMap)*/ ),
   M_linearStiff                ( ),
-  M_jacobian                   (/*M_localMap*/),
-  M_recur                       (),
-  M_source                     (),
+  M_jacobian                   ( /*M_localMap*/ ),
+  M_recur                      ( ),
+  M_source                     ( ),
   M_count                      ( 0 ),//useless
   M_offset                     ( 0 ),
   M_rescaleFactor              ( 1. ),
@@ -599,7 +594,7 @@ VenantKirchhoffSolver<Mesh, SolverType>::setup(boost::shared_ptr<data_Type>     
 {
   setup( data, dFESpace, comm, dFESpace->mapPtr(), (UInt)0 );
   M_stiff.reset                      ( new matrix_Type(*M_localMap) );
-  M_massStiff.reset                  ( new matrix_Type(*M_localMap) );
+  //M_massStiff.reset                  ( new matrix_Type(*M_localMap) );
   M_jacobian.reset                   ( new matrix_Type(*M_localMap) );
   M_rhs.reset                        ( new vector_Type(*M_localMap));
   M_f.reset                          ( new vector_Type(*M_localMap));
@@ -608,7 +603,6 @@ VenantKirchhoffSolver<Mesh, SolverType>::setup(boost::shared_ptr<data_Type>     
   M_syy.reset                        ( new vector_Type(*M_localMap) );
   M_szz.reset                        ( new vector_Type(*M_localMap) );
   M_linearSolver.reset               ( new SolverType( comm ) );
-
 }
 
 template <typename Mesh, typename SolverType>
@@ -844,7 +838,7 @@ VenantKirchhoffSolver<Mesh, SolverType>::iterateLin( bchandler_Type& bch )
 
   //M_Displayer->leaderPrintMax("dz norm     = " , M_disp.NormInf() );
 
-  numIter = abs(numIter);
+  numIter = std::abs(numIter);
 
   *M_residual_d =  *M_massStiff*(*M_disp);
   //    M_residual_d -= M_rhsNoBC;
@@ -1071,7 +1065,7 @@ VenantKirchhoffSolver<Mesh, SolverType>::reduceSolution( Vector& displacement, V
 
   if ( getComunicator()->MyPID() == 0 )
     {
-      for ( UInt iDof = 0; iDof < nDimensions*dim(); ++iDof )
+      for ( UInt iDof = 0; iDof < nDimensions*getDim(); ++iDof )
 	{
 	  disp[ iDof ] = displacement[ iDof ];
 	  vel [ iDof ] = velocity    [ iDof ];
@@ -1093,12 +1087,6 @@ VenantKirchhoffSolver<Mesh, SolverType>::setDataFromGetPot( const GetPot& dataFi
 {
   M_linearSolver->setDataFromGetPot( dataFile, "solid/solver" );
   M_linearSolver->setupPreconditioner(dataFile, "solid/prec");
-
-  UInt marker = M_FESpace->mesh()->volumeList( 0 ).marker();
-  if (!M_data->getYoung(marker))
-    M_data->setYoung(dataFile( "solid/physics/young", 0. ), marker);
-  if (!M_data->getPoisson(marker))
-    M_data->setPoisson(dataFile( "solid/physics/poisson", 0. ), marker);
 }
 
 
