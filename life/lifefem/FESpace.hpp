343599094deeab2e3db8d4b8faea2f60a5cd08ab
Antonio Cervone <ant.cervone@gmail.com>
2011-06-17 10:24:22 +0200

    convert all constructors to use const communicators

--- life/lifefem/FESpace.hpp
+++ life/lifefem/FESpace.hpp
@@ -113,13 +113,13 @@ public:
              const QuadratureRule&			Qr,
              const QuadratureRule&			bdQr,
              const Int				fDim,
-             commPtr_Type&			commptr
+             const commPtr_Type&			commptr
            );
 
     FESpace(	MeshPartitioner<MeshType>&	mesh,
              const std::string&		space,
              const Int				fDim,
-             commPtr_Type&			commptr
+             const commPtr_Type&			commptr
            );
 
     FESpace(	meshPtr_Type			mesh,
@@ -127,13 +127,13 @@ public:
              const QuadratureRule&			Qr,
              const QuadratureRule&			bdQr,
              const Int				fDim,
-             commPtr_Type&			commptr
+             const commPtr_Type&			commptr
            );
 
     FESpace( meshPtr_Type			mesh,
              const std::string&		space,
              const Int				fDim,
-             commPtr_Type&			commptr
+             const commPtr_Type&			commptr
            );
 
     //! Do nothing destructor
@@ -551,7 +551,7 @@ FESpace(	MeshPartitioner<MeshType>& 	mesh,
          const QuadratureRule&      	Qr,
          const QuadratureRule&      	bdQr,
          const Int            	fDim,
-         commPtr_Type&         	commptr
+         const commPtr_Type&         	commptr
        ) :
         M_mesh			( mesh.meshPartition() ),
         M_refFE			( &refFE ),
@@ -578,7 +578,7 @@ FESpace<MeshType, MapType>::
 FESpace(	MeshPartitioner<MeshType>&	mesh,
          const std::string&		space,
          const Int				fDim,
-         commPtr_Type&			commptr
+         const commPtr_Type&			commptr
        ) :
         M_mesh			( mesh.meshPartition() ),
         M_fieldDim		( fDim ),
@@ -620,7 +620,7 @@ FESpace(	meshPtr_Type			mesh,
          const QuadratureRule&			Qr,
          const QuadratureRule&			bdQr,
          const Int				fDim,
-         commPtr_Type&			commptr
+         const commPtr_Type&			commptr
        ) :
         M_mesh			( mesh ),
         M_refFE			( &refFE ),
@@ -648,7 +648,7 @@ FESpace<MeshType, MapType>::
 FESpace(	meshPtr_Type			mesh,
          const std::string&		space,
          const Int				fDim,
-         commPtr_Type&			commptr
+         const commPtr_Type&			commptr
        ) :
         M_mesh			( mesh ),
         M_fieldDim		( fDim ),
