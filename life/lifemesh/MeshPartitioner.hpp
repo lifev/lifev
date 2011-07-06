9c5b4860edf259abd1a7ac8d86c6e8ec8f63ceb4
giori <guido1985@tiscali.it>
2011-07-06 17:11:51 +0200

    use copy constructors in MeshPartitioner in order to preserve markers

--- life/lifemesh/MeshPartitioner.hpp
+++ life/lifemesh/MeshPartitioner.hpp
@@ -1142,19 +1142,12 @@ void MeshPartitioner<MeshType>::constructNodes()
             }
 
             pp = &(*M_meshPartitions)[i]->addPoint(boundary);
-            pp->setMarker(M_originalMesh->point(*it).marker());
+            *pp = M_originalMesh->point( *it );
 
-            pp->x() = M_originalMesh->point(*it).x();
-            pp->y() = M_originalMesh->point(*it).y();
-            pp->z() = M_originalMesh->point(*it).z();
+            pp->setLocalId( inode );
 
-            UInt id = M_originalMesh->point(*it).id();
-
-            pp->setId(id);
-            pp->setLocalId(inode);
-
-            (*M_meshPartitions)[i]->localToGlobalNode().insert(std::make_pair(inode, id));
-            (*M_meshPartitions)[i]->globalToLocalNode().insert(std::make_pair(id, inode));
+            (*M_meshPartitions)[i]->localToGlobalNode().insert(std::make_pair( pp->localId(), pp->id() ));
+            (*M_meshPartitions)[i]->globalToLocalNode().insert(std::make_pair( pp->id(), pp->localId() ));
         }
     }
 }
@@ -1180,10 +1173,10 @@ void MeshPartitioner<MeshType>::constructVolumes()
         for (it = M_localVolumes[i].begin(); it != M_localVolumes[i].end(); ++it, ++count)
         {
             pv = &((*M_meshPartitions)[i]->addVolume());
-            pv->setId (M_originalMesh->volume(*it).id());
-            pv->setLocalId(count);
+            *pv = M_originalMesh->volume( *it );
+            pv->setLocalId( count );
 
-            M_globalToLocalVolume[i].insert(std::make_pair(M_originalMesh->volume(*it).id(), count));
+            M_globalToLocalVolume[i].insert(std::make_pair( pv->id(), pv->localId() ) );
 
             for (ID id = 0; id < M_elementNodes; ++id)
             {
@@ -1194,10 +1187,6 @@ void MeshPartitioner<MeshType>::constructVolumes()
                 im = M_globalToLocalNode[i].find(inode);
                 pv->setPoint(id, (*M_meshPartitions)[i]->pointList( (*im).second ));
             }
-
-            Int ibc = M_originalMesh->volume(*it).marker();
-
-            pv->setMarker(entityFlag_Type( ibc ));
         }
     }
 }
@@ -1230,8 +1219,8 @@ void MeshPartitioner<MeshType>::constructEdges()
             }
 
             pe = &(*M_meshPartitions)[i]->addEdge(boundary);
+            *pe = M_originalMesh->edge( *is );
 
-            pe->setId (M_originalMesh->edge(*is).id());
             pe->setLocalId(count);
 
             for (ID id = 0; id < 2; ++id)
@@ -1243,7 +1232,6 @@ void MeshPartitioner<MeshType>::constructEdges()
                 im = M_globalToLocalNode[i].find(inode);
                 pe->setPoint(id, (*M_meshPartitions)[i]->pointList((*im).second));
             }
-            pe->setMarker(M_originalMesh->edge(*is).marker());
         }
     }
 }
@@ -1276,9 +1264,9 @@ void MeshPartitioner<MeshType>::constructFaces()
             }
 
             pf =  &(*M_meshPartitions)[i]->addFace(boundary);
+            *pf = M_originalMesh->face( *is );
 
-            pf->setId (M_originalMesh->face(*is).id());
-            pf->setLocalId(count);
+            pf->setLocalId( count );
 
             Int elem1 = M_originalMesh->face(*is).firstAdjacentElementIdentity();
             Int elem2 = M_originalMesh->face(*is).secondAdjacentElementIdentity();
@@ -1346,8 +1334,6 @@ void MeshPartitioner<MeshType>::constructFaces()
                 im = M_globalToLocalNode[i].find(inode);
                 pf->setPoint(id, (*M_meshPartitions)[i]->pointList((*im).second));
             }
-
-            pf->setMarker(M_originalMesh->face(*is).marker());
 
             (*M_meshPartitions)[i]->setLinkSwitch("HAS_ALL_FACES");
             (*M_meshPartitions)[i]->setLinkSwitch("FACES_HAVE_ADIACENCY");
