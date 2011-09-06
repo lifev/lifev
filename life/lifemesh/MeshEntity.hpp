473df8e7734e70c42aa9cd98db41f9e1e8cc565a
Antonio Cervone <ant.cervone@gmail.com>
2011-09-06 12:06:28 +0200

    bugfix in flag definition

--- life/lifemesh/MeshEntity.hpp
+++ life/lifemesh/MeshEntity.hpp
@@ -53,7 +53,7 @@ const flag_Type INTERNAL_INTERFACE  ( 0x02 );
 const flag_Type SUBDOMAIN_INTERFACE ( 0x04 );
 const flag_Type OVERLAP             ( 0x08 );
 const flag_Type CUTTED              ( 0x10 );
-const flag_Type VERTEX              ( 0x11 );
+const flag_Type VERTEX              ( 0x20 );
 }
 
 //! This is the base class to store the identifiers.
