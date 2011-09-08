a0c8caecff809235faab22c1ec907a235baec3fa
Simone Deparis <Simone.Deparis@epfl.ch>
2011-09-08 16:42:42 +0200

    Corrected bug in symmetrizedAdvection. Now you have to call both advection and symmetrizedAdvection with 0.5*beta to get 1/2 u\grad \beta v + 1/2 \beta \grad u v

--- life/lifefem/AssemblyElemental.hpp
+++ life/lifefem/AssemblyElemental.hpp
@@ -177,7 +177,7 @@ void interpolateGradient(localVector& localGradient,
 }
 
 
-//! Elementary advection
+//! Elementary advection \beta \grad u v
 template<typename localVector>
 void advection(MatrixElemental& localAdv,
                const CurrentFE& advCFE,
@@ -221,7 +221,7 @@ void advection(MatrixElemental& localAdv,
     }
 }
 
-//! Elementary advection
+//! Elementary advection, term u\grad \beta v
 template<typename localVector, typename localTensor>
 void symmetrizedAdvection(MatrixElemental& localAdv,
                           const CurrentFE& advCFE,
@@ -236,46 +236,35 @@ void symmetrizedAdvection(MatrixElemental& localAdv,
     ASSERT (fieldDim == nDimensions, "Symmetrized operator works only with vectors of the same dimension as the space" );
 
 
-    for (UInt iterFDim(0); iterFDim<fieldDim; ++iterFDim)
+    for (UInt iCoor(0); iCoor<fieldDim; ++iCoor)
     {
-        // Extract the view of the matrix
-        MatrixElemental::matrix_view localView = localAdv.block(iterFDim,iterFDim);
-
-        // Loop over the basis functions
-        for (UInt iDof(0); iDof < nbFEDof ; ++iDof)
+        for (UInt jCoor(0); jCoor<fieldDim; ++jCoor)
         {
-            // Build the local matrix
-            for (UInt jDof(0); jDof < nbFEDof; ++jDof)
+            // Extract the view of the matrix
+            MatrixElemental::matrix_view localView = localAdv.block(iCoor,jCoor);
+
+            // Loop over the basis functions
+            for (UInt iDof(0); iDof < nbFEDof ; ++iDof)
             {
-                localValue = 0.0;
-
-                //Loop on the quadrature nodes
-                for (UInt iQuadPt(0); iQuadPt < nbQuadPt; ++iQuadPt)
+                // Build the local matrix
+                for (UInt jDof(0); jDof < nbFEDof; ++jDof)
                 {
-                    for (UInt iDim(0); iDim<nDimensions; ++iDim)
-                    {
-                        localValue += localValues[iQuadPt][iDim]
-                                      * advCFE.dphi(jDof,iDim,iQuadPt)
-                                      * advCFE.phi(iDof,iQuadPt)
-                                      * advCFE.wDetJacobian(iQuadPt)
-                                      * 0.5;
+                    localValue = 0.0;
 
-                        for (UInt jDim(0); iDim<nDimensions; ++iDim)
-                        {
+                    //Loop on the quadrature nodes
+                    for (UInt iQuadPt(0); iQuadPt < nbQuadPt; ++iQuadPt)
+                    {
                         localValue +=
-                            localGradient[iQuadPt][jDim][iDim]
-                                      * advCFE.phi(jDof,iQuadPt)
-                                      * advCFE.phi(iDof,iQuadPt)
-                                      * advCFE.wDetJacobian(iQuadPt)
-                                      * 0.5;
-                        }
+                            localGradient[iQuadPt][iCoor][jCoor]
+                            * advCFE.phi(jDof,iQuadPt)
+                            * advCFE.phi(iDof,iQuadPt)
+                            * advCFE.wDetJacobian(iQuadPt);
 
                     }
 
+                    // Add on the local matrix
+                    localView(iDof,jDof)=localValue;
                 }
-
-                // Add on the local matrix
-                localView(iDof,jDof)=localValue;
             }
         }
     }
