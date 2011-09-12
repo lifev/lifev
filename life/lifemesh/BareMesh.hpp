86e8939fbf3eb01263d6eaba4e595bfff8349c54
Antonio Cervone <ant.cervone@gmail.com>
2011-09-12 18:04:37 +0200

    mesh reading through RegionMeshBare struct.
    there is a reader for INRIA mesh format and a converter from RegionBareMesh to RegionMesh3D

--- life/lifemesh/BareMesh.hpp
+++ life/lifemesh/BareMesh.hpp
@@ -12,25 +12,47 @@
 
 #ifndef BAREMESH_HPP_
 #define BAREMESH_HPP_
+
+namespace LifeV
+{
+
 //! A struct for a bare mesh
 /**
  * A very simple struct which stores an mesh as read from a file, ready to be imported in
  * a regionmesh
  * All SimpleArray have the first dimension the "shortest" one
  */
+template <typename GeoShape>
 struct RegionMeshBare{
     UInt nDimensions;
-    ReferenceGeometry geoShape;
-    ReferenceGeometry bGeoShape;
-    ArraySimple<UInt> points;
+    ReferenceShapes refShape;
+    ReferenceShapes bRefShape;
+    ArraySimple<Real> points;
     std::vector<ID> pointsMarkers;
     ArraySimple<UInt> edges;
     std::vector<ID> edgesMarkers;
+    UInt numBoundaryFaces;
     ArraySimple<UInt> faces;
     std::vector<ID> facesMarkers;
     ArraySimple<UInt> elements;
     std::vector<ID> elementsMarkers;
 
+    void clear();
 };
+
+template <typename GeoShape>
+void RegionMeshBare<GeoShape>::clear()
+{
+    clearVector ( points );
+    clearVector ( pointsMarkers );
+    clearVector ( edges );
+    clearVector ( edgesMarkers );
+    clearVector ( faces );
+    clearVector ( facesMarkers );
+    clearVector ( elements );
+    clearVector ( elementsMarkers );
+}
+
+}
 
 #endif /* BAREMESH_HPP_ */
--- testsuite/lifemesh/test_mesh.cpp
+++ testsuite/lifemesh/test_mesh.cpp
@@ -59,6 +59,7 @@
 #include <life/lifemesh/MarkerDefinitions.hpp>
 #include <life/lifefilters/ImporterMesh3D.hpp>
 #include <life/lifemesh/RegionMesh3D.hpp>
+#include <life/lifemesh/BareMesh.hpp>
 #include <life/lifemesh/MeshElementBare.hpp>
 
 // A dummy class to imitate a VectorEpetra
@@ -223,6 +224,23 @@ int main(int argc, char** argv)
     cout<<"Number of internal interface edges (should be 2) "<<
                       aMesh.edgeList.countElementsWithFlag(EntityFlags::INTERNAL_INTERFACE,Flag::testOneSet)<<std::endl;
 
+
+    RegionMeshBare<LinearTetra> bareMesh;
+    readINRIAMeshFile( bareMesh, dirname + "../inria/cartesian_cube8.mesh", m );
+
+    RegionMesh3D<LinearTetra> bMesh;
+    convertBareMesh ( bareMesh, bMesh, m );
+    bMesh.check(0,true,true);
+
+    bMesh.edgeList.clear();
+    bMesh.faceList.clear();
+
+    checkMesh3D(bMesh, sw,
+                true, true,
+                cerr, cout, ofile);
+
+
+    bMesh.showMe();
 
 #ifdef HAVE_MPI
     MPI_Finalize();
