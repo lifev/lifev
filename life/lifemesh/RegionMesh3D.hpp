e37ab5797047b94ea45c04d6e33c4d53e4f96810
Antonio Cervone <ant.cervone@gmail.com>
2011-09-15 11:55:55 +0200

    little modification

--- life/lifemesh/RegionMesh3D.hpp
+++ life/lifemesh/RegionMesh3D.hpp
@@ -2149,7 +2149,8 @@ RegionMesh3D<GEOSHAPE, MC>::addPoint( bool const boundary, bool const vertex )
 {
     point_Type aPoint;
     aPoint.setBoundary(boundary);
-    if(vertex) aPoint.replaceFlag(aPoint.flag() | EntityFlags::VERTEX);
+    aPoint.setId(pointList.size());
+    if(vertex) aPoint.setFlag( EntityFlags::VERTEX );
     return addPoint(aPoint);
 }
 
