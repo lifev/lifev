d1eee69f43c7c91752e1fc44b362824714cd40dc
Antonio Cervone <ant.cervone@gmail.com>
2011-12-23 01:37:21 +0100

    setup.sh scripts update

--- tools/emacs/setup.sh
+++ tools/emacs/setup.sh
@@ -1,18 +1,18 @@
 #! /bin/sh
 
-DIRNAME=`dirname $0`
+DIRNAME=$(dirname $0)
 
-DESTDIR=$DIRNAME/../..
+DESTDIR=${DIRNAME}/../..
 
-cd $DESTDIR
+cd ${DESTDIR}
 if test -e .emacs-dirvars ; then
-  echo .emacs-dirvars already exists.
+  echo ".emacs-dirvars already exists. The file will not be linked"
 else 
   ln -s tools/emacs/emacs-dirvars .emacs-dirvars
 fi
 
 if test -e  Templates; then
-  echo directory Templates already exists.
+  echo "directory Templates already exists. The directiry will not be linked."
 else 
  ln -s tools/emacs/Templates .
 fi
--- tools/kdevelop/setup.sh
+++ tools/kdevelop/setup.sh
@@ -1,4 +1,11 @@
 #! /bin/sh
 
-cp life.kdevelop ../../life.kdevelop
+DIRNAME=$(dirname $0)
 
+DESTDIR=${DIRNAME}/../..
+
+if test -e ${DESTDIR}/life.kdevelop ; then
+  echo "life.kdevelop already exists. The file will not be copied"
+else
+  cp ${DIRNAME}/life.kdevelop ${DESTDIR}/life.kdevelop
+fi
