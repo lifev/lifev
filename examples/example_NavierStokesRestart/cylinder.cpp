e9bd8562506b735efbb622b74b190c1913353f09
Toni Mikael Lassila <lassila@mathicsepc10.epfl.ch>
2012-02-28 10:09:57 +0100

    Bugfix to example_NavierStokesRestart

--- examples/example_NavierStokesRestart/cylinder.cpp
+++ examples/example_NavierStokesRestart/cylinder.cpp
@@ -441,7 +441,7 @@ Cylinder::run()
 
     if (verbose) std::cout << "Calling the fluid constructor ... ";
 
-    bcH.setOffset("Inlet", totalVelDof + totalPressDof);
+    //bcH.setOffset("Inlet", totalVelDof + totalPressDof);
 
     OseenSolver< RegionMesh<LinearTetra> > fluid (oseenData,
                                                     *uFESpacePtr,
