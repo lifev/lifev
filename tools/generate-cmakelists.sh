2896f9165f2140d1dde45980fa9fc92ee8e43dcc
Antonio Cervone <ant.cervone@gmail.com>
2012-03-07 09:43:25 +0100

    update to CMakeLists.txt to correctly set include paths

--- tools/generate-cmakelists.sh
+++ tools/generate-cmakelists.sh
@@ -5,12 +5,14 @@
 for dir in $(find . -type d); do
   echo "$dir"
   cd $dir
-#  echo "SET(HEADERS \${HEADERS}" >> CMakeLists.txt
-#  ls -1 *.hpp >> CMakeLists.txt
-#  echo ")" >> CMakeLists.txt
-#  echo >> CMakeLists.txt
-#  echo "SET(SOURCES \${SOURCES}"  >> CMakeLists.txt
-#  ls -1 *.cpp >> CMakeLists.txt
-#  echo ")" >> CMakeLists.txt
+  echo "INCLUDE_DIRECTORIES(\${CMAKE_CURRENT_SOURCE_DIR})" >> CMakeLists.txt
+  echo >> CMakeLists.txt
+  echo "SET(HEADERS \${HEADERS}" >> CMakeLists.txt
+  ls -1 *.hpp >> CMakeLists.txt
+  echo ")" >> CMakeLists.txt
+  echo >> CMakeLists.txt
+  echo "SET(SOURCES \${SOURCES}"  >> CMakeLists.txt
+  ls -1 *.cpp >> CMakeLists.txt
+  echo ")" >> CMakeLists.txt
   cd ..
 done
