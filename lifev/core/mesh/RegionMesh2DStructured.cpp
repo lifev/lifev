0a1066d9c0eb518614d4a09509288f824ca6ce5f
fumagalli <alessio.fumagalli@mail.polimi.it>
2012-03-30 12:34:44 +0200

    Refactoring of RegionMesh2DStructured.cpp

--- lifev/core/mesh/RegionMesh2DStructured.cpp
+++ lifev/core/mesh/RegionMesh2DStructured.cpp
@@ -78,8 +78,33 @@ markerID_Type regularMeshPointPosition2D( const UInt& i_x,
     TOPLEFTCORNER      = 10;
     TOPRIGHTCORNER     = 6;
     */
-    
-}
+    switch ( pointPosition )
+    {
+        // We are inside
+        case 0:
+            return 0;
+        // We are on 1 edge
+        case 1:
+            return 1;
+        case 2:
+            return 2;
+        case 4:
+            return 3;
+        case 8:
+            return 4;
+        // We are on a corner
+        case 5:
+            return 5;
+        case 6:
+            return 6;
+        case 9:
+            return 7;
+        case 10:
+            return 8;
+        default:
+            return 0;
+    }
 
+}
 
 } // Namespace LifeV
