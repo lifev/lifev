09d8b266b67d9a24ed4baf5ea2256b5ccbd41074
fumagalli <alessio.fumagalli@mail.polimi.it>
2012-03-30 12:34:03 +0200

    Make regularMesh2D working.

--- lifev/core/mesh/RegionMesh2DStructured.cpp
+++ lifev/core/mesh/RegionMesh2DStructured.cpp
@@ -33,18 +33,17 @@
 
     @date 23-05-2011
 
- */
+*/
 
-#include <lifev/core/LifeV.hpp>
 #include <lifev/core/mesh/RegionMesh2DStructured.hpp>
 
 namespace LifeV
 {
 
 markerID_Type regularMeshPointPosition2D( const UInt& i_x,
-                                     const UInt& i_y,
-                                     const UInt& n_x,
-                                     const UInt& n_y )
+                                          const UInt& i_y,
+                                          const UInt& n_x,
+                                          const UInt& n_y )
 {
     // We will use a binary representation to
     // find the position of the point
@@ -53,41 +52,14 @@ markerID_Type regularMeshPointPosition2D( const UInt& i_x,
     // 0010 = on the y=l_y axis       ( 2)
     // 0100 = on the x=l_x axis       ( 4)
     // 1000 = on the x=0 axis         ( 8)
-    
+
     UInt pointPosition(0);
+
     if ( i_y == 0 )       { pointPosition = pointPosition |  1; }
     if ( i_y == n_y - 1)  { pointPosition = pointPosition |  2; }
     if ( i_x == n_x - 1)  { pointPosition = pointPosition |  4; }
     if ( i_x == 0 )       { pointPosition = pointPosition |  8; }
 
-    switch ( pointPosition )
-    {
-        // We are inside
-    case 0:
-        return 0;
-        // We are on 1 edge
-    case 1:
-        return 1;
-    case 2:
-        return 2;
-    case 4:
-        return 3;
-    case 8:
-        return 4;
-        // We are on a corner
-    case 5:
-        return 5;
-    case 6:
-        return 6;
-    case 9:
-        return 7;
-    case 10:
-        return 8;
-    
-    default:
-        return 0;
-    }
-    
     /*
     // The boundary of the structured mesh
     // labels:
--- lifev/core/mesh/RegionMesh2DStructured.hpp
+++ lifev/core/mesh/RegionMesh2DStructured.hpp
@@ -30,48 +30,55 @@
 
     @author Iori Guido <guido.iori@mail.polimi.it>
     @contributor -
-    
 
     @date 23-05-2011
 
-
- */
+*/
 
 #ifndef STRUCTUREDMESH2D_HPP
 #define STRUCTUREDMESH2D_HPP 1
 
 #include <lifev/core/LifeV.hpp>
-#include <lifev/core/util/LifeAssert.hpp>
 #include <lifev/core/mesh/RegionMesh.hpp>
 #include <lifev/core/mesh/MeshChecks.hpp>
-#include <lifev/core/mesh/MeshElement.hpp>
-#include <lifev/core/mesh/MeshUtility.hpp>
-#include <lifev/core/mesh/Marker.hpp>
-//#include <fstream>
-
 
 namespace LifeV
 {
 
-//! @name Methods
-//@{
-
-//! This method gives the flags for a rectangle 
 /*!
-  @param i_x 
-  @param i_y 
+  @brief This method gives the flags for a rectangle
+
+  @param i_x
+  @param i_y
   @param n_x Number of elements along the length
   @param n_y Number of elements along the width
 */
-
 markerID_Type regularMeshPointPosition2D( const UInt& i_x,
-                                     	 	const UInt& i_y,
-                                     	 	const UInt& n_x,
-                                     	 	const UInt& n_y );
+                                          const UInt& i_y,
+                                          const UInt& n_x,
+                                          const UInt& n_y );
 
 
-//! This method generate a rectangular structured mesh
 /*!
+  @brief This method generate a rectangular structured mesh
+
+  For the square \f$ [0,1]^2 \f$ the internal flag is 0.
+  <br>
+  For the corners the labels are:
+  <ul>
+  <li> BOTTOM and LEFT = 7, i.e. \f$ x = 0 \f$ and \f$ y = 0 \f$ </li>
+  <li> BOTTOM and RIGHT = 5, i.e. \f$ x = 1 \f$ and \f$ y = 0 \f$ </li>
+  <li> TOP and LEFT = 8, i.e. \f$ x = 0 \f$ and \f$ y = 1 \f$ </li>
+  <li> TOP and RIGHT = 6, i.e. \f$ x = 1 \f$ and \f$ y = 1 \f$ </li>
+  </ul>
+  For the edges the labels are:
+  <ul>
+  <li> BOTTOM = 1, i.e. \f$ x = 0 \f$ </li>
+  <li> LEFT = 2, i.e. \f$ y = 0 \f$ </li>
+  <li> RIGHT = 3, i.e. \f$ y = 1 \f$ </li>
+  <li> TOP = 4, i.e. \f$ x = 1 \f$ </li>
+  </ul>
+
   @param mesh The mesh that we want to generate
   @param regionFlag Flag of the region
   @param m_x Number of elements along the length
@@ -82,97 +89,80 @@ markerID_Type regularMeshPointPosition2D( const UInt& i_x,
   @param t_x translation of the mesh along the x-axis
   @param t_y translation of the mesh along the y-axis
 */
-template <typename GeoShape, typename MC>
-void regularMesh2D( RegionMesh<GeoShape,MC>& mesh,
+template <typename MC>
+void regularMesh2D( RegionMesh < LinearTriangle, MC >& mesh,
                     markerID_Type regionFlag,
                     const UInt& m_x,
                     const UInt& m_y,
-                    bool verbose=false,
-                    const Real& l_x=1.0,
-                    const Real& l_y=1.0,
-                    const Real& t_x=0.0,
-                    const Real& t_y=0.0 )
+                    bool verbose = false,
+                    const Real& l_x = 1.0,
+                    const Real& l_y = 1.0,
+                    const Real& t_x = 0.0,
+                    const Real& t_y = 0.0 )
 {
-    typedef GeoShape geoShape_Type;
-    typedef MC       marker_Type;
 
-	std::cout<<geoShape_Type::S_numRidges<<std::endl;
+    typedef LinearTriangle geoShape_Type;
+    typedef RegionMesh < geoShape_Type, MC > mesh_Type;
 
-    ASSERT(geoShape_Type::S_numRidges == 3,"Not creating a P1 structured mesh");
-	
-    // output stream
-	std::ostream& oStr = std::cout;
-	
-	/*
-    std::cout<<"l_x: "<<l_x<<std::endl;
-    std::cout<<"l_y: "<<l_y<<std::endl;
-    std::cout<<"m_x: "<<m_x<<std::endl;
-    std::cout<<"m_y: "<<m_y<<std::endl;
-    */
-	
+
+    ASSERT( geoShape_Type::S_numRidges == 3,"Not creating a P1 structured mesh" );
+
     // discretization
-    Real dx( l_x / m_x ); std::cout<<"dx: "<<dx<<std::endl;
-    Real dy( l_y / m_y ); std::cout<<"dy: "<<dy<<std::endl;
+    const Real dx( l_x / m_x );
+    const Real dy( l_y / m_y );
 
     // Number of nodes along the side of the unit cube
-    UInt n_x( m_x + 1 );
-    UInt n_y( m_y + 1 );
+    const UInt n_x( m_x + 1 );
+    const UInt n_y( m_y + 1 );
 
     // Incremental values in order to get the indices of the nodes
     // Due to the structure, if we add N_i to the number of the
     // current node, we end up with the number of the next point
     // in the i axis.
-    UInt N_x( 1 );
-    UInt N_y( n_x );
+    const UInt N_x( 1 );
+    const UInt N_y( n_x );
 
     // Data about the mesh
-    UInt verticesNumber( n_x * n_y );
-    UInt boundaryVerticesNumber( verticesNumber - ( n_x - 2 ) * ( n_y - 2 ) ); //Total-inside points
-    UInt boundaryEdgesNumber(2 * ( m_x + m_y ) );
-    UInt edgesNumber(
-        // Edges that draws the rectangles
-        m_x * n_y +                                
-        n_x * m_y +                                 
-        // Edges that go accross the rectangles
-        m_x * m_y                                   
-    );
-    
-    // Faces 
-    UInt elementsNumber( 2*( m_x * m_y ) );
 
-    UInt pointsNumber( 0 );
-    UInt boundaryPointsNumber( 0 );
-    
-    oStr << "Linear Tetra Mesh" << std::endl;
-    pointsNumber = verticesNumber;
-    boundaryPointsNumber = boundaryVerticesNumber;
+    //Total-inside points
+    const UInt verticesNumber( n_x * n_y );
+    const UInt boundaryVerticesNumber( verticesNumber - ( n_x - 2 ) * ( n_y - 2 ) );
+    const UInt boundaryEdgesNumber(2 * ( m_x + m_y ) );
+    const UInt edgesNumber(
+                            // Edges that draws the rectangles
+                            m_x * n_y +
+                            n_x * m_y +
+                            // Edges that go accross the rectangles
+                            m_x * m_y
+                          );
 
+    // Faces
+    const UInt elementsNumber( 2*( m_x * m_y ) );
 
     // Set the data
-    oStr << "initialization of mesh...";
 
     // Note: The vertices are the nodes of the mesh while the points
     //       are the nodes and some new points added for example for
     //       the quadratic triangle
 
     // About points:
-    mesh.setNumBoundaryRidges( boundaryPointsNumber );
-    mesh.setMaxNumRidges( pointsNumber, true );
-    mesh.setMaxNumGlobalRidges( pointsNumber );
+    mesh.setNumBoundaryRidges ( boundaryVerticesNumber );
+    mesh.setMaxNumRidges      ( verticesNumber, true );
+    mesh.setMaxNumGlobalRidges( verticesNumber );
 
     // About vertices:
-    mesh.setNumVertices( verticesNumber );
+    mesh.setNumVertices      ( verticesNumber );
     mesh.setNumGlobalVertices( verticesNumber );
-    mesh.setNumBVertices( boundaryVerticesNumber );
+    mesh.setNumBVertices     ( boundaryVerticesNumber );
 
     // About edges:
-    mesh.setNumFacets( edgesNumber );
-    mesh.setnumBoundaryFacets( boundaryEdgesNumber );
-    mesh.setMaxNumFacets( edgesNumber );
+    mesh.setNumFacets         ( edgesNumber );
+    mesh.setnumBoundaryFacets ( boundaryEdgesNumber );
+    mesh.setMaxNumFacets      ( edgesNumber );
     mesh.setMaxNumGlobalFacets( edgesNumber );
 
     // About faces:
-    mesh.setMaxNumElements( elementsNumber );
+    mesh.setMaxNumElements      ( elementsNumber );
     mesh.setMaxNumGlobalElements( elementsNumber );
 
     mesh.setMaxNumFaces      ( elementsNumber );
@@ -180,183 +170,149 @@ void regularMesh2D( RegionMesh<GeoShape,MC>& mesh,
     mesh.setNumFaces         ( elementsNumber );
 
 
-    mesh.setMarkerID( regionFlag );
-    oStr << "done" << std::endl;
-        
+    mesh.setMarker( regionFlag );
+
     // Declaration of pointers on the different mesh entities
-    typename RegionMesh<GeoShape,MC>::ridge_Type*   pointPtr  = 0;
-    typename RegionMesh<GeoShape,MC>::facet_Type*   edgePtr   = 0;
-    typename RegionMesh<GeoShape,MC>::element_Type* elementPtr   = 0;
-           
+    typename mesh_Type::ridge_Type*   pointPtr   = 0;
+    typename mesh_Type::facet_Type*   edgePtr    = 0;
+    typename mesh_Type::element_Type* elementPtr = 0;
+
     // Build the points of the mesh
-    oStr << "building the points of the mesh...";
     Real xPosition( 0.0 ), yPosition( 0.0 ), zPosition( 0.0 );
     markerID_Type nodeFlag( 0 );
     UInt nodeID( 0 );
     UInt P0( 0 ), P1( 0 ), P2( 0 ), P3( 0 );
-    
+
     /*
          P3___P2
           | / |
           |/__|
-        P0     P1	  
+        P0     P1
     */
-    
+
     for ( UInt j(0); j<n_y; ++j )
-	{
-		yPosition = dy * j;
+    {
+        yPosition = dy * j;
 
-		for ( UInt i(0); i<n_x; ++i )
-		{
-			xPosition = dx * i;
-			nodeFlag = regularMeshPointPosition2D(i, j, n_x, n_y );
+        for ( UInt i(0); i<n_x; ++i )
+        {
+            xPosition = dx * i;
+            nodeFlag = regularMeshPointPosition2D(i, j, n_x, n_y );
 
-			// We create the point
-			if ( nodeFlag>0 )
-			{
-				pointPtr = &mesh.addRidge( true ); //it is a boundary point
-			}
-			else
-			{
-				pointPtr = &mesh.addRidge( false );
-			}
-			
+            // We create the point
+            if ( nodeFlag>0 )
+            {
+                pointPtr = &mesh.addRidge( true ); //it is a boundary point
+            }
+            else
+            {
+                pointPtr = &mesh.addRidge( false );
+            }
 
-			// We set the point properties
-			nodeID = j * N_y + i;
-			pointPtr->setId( nodeID );
-			pointPtr->setLocalId( nodeID );
-			//std::cout<<"Info point: "<<pointPtr->MeshVertex::showMe(true)<<std::endl;
-			
-			//mesh.localToGlobalNode().insert( std::make_pair( nodeID, nodeID) );
-			//mesh.globalToLocalNode().insert( std::make_pair( nodeID, nodeID) );
 
-			pointPtr->setMarkerID( nodeFlag );
-			pointPtr->x() = xPosition + t_x;
-			pointPtr->y() = yPosition + t_y;
-			pointPtr->z() = zPosition;
-			std::cout<<"Info point: "<<pointPtr->MeshVertex::showMe(true)<<std::endl;
-			
-		}
-	}
-    
-    oStr << "done" << std::endl;
+            // We set the point properties
+            nodeID = j * N_y + i;
+            pointPtr->setId( nodeID );
+            pointPtr->setLocalId( nodeID );
+
+            pointPtr->setMarker( nodeFlag );
+            pointPtr->x() = xPosition + t_x;
+            pointPtr->y() = yPosition + t_y;
+            pointPtr->z() = zPosition;
+        }
+    }
 
     // Build the faces
-    oStr << "building the elements...";
-    UInt faceID(0);  
-    
-	for ( UInt j(0); j<m_y; ++j )
-	{
-		for ( UInt i(0); i<m_x; ++i )
-		{
-			faceID = ( j * m_x + i ) * 2;
-				
-			nodeID = j * N_y + i;
-			P0 = nodeID;
-			P1 = nodeID + N_x;
-			P2 = nodeID + N_x + N_y;
-			P3 = nodeID + N_y;
-		
-		
-		// Triangle 1
-		elementPtr = &mesh.addElement();
-		elementPtr->setId( faceID );
-		elementPtr->setLocalId( faceID );
-		elementPtr->setPoint( 0, mesh.point(P1) );
-		elementPtr->setPoint( 1, mesh.point(P2) );
-		elementPtr->setPoint( 2, mesh.point(P0) );
-		elementPtr->setMarkerID( regionFlag );
-		//std::cout<<elementPtr->showMe(true);
-		
-		// Triangle 2
-		elementPtr = &mesh.addElement();
-		elementPtr->setId( faceID + 1 );
-		elementPtr->setLocalId( faceID + 1 );
-		elementPtr->setPoint( 0, mesh.point(P3) );
-		elementPtr->setPoint( 1, mesh.point(P0) );
-		elementPtr->setPoint( 2, mesh.point(P2) );
-		elementPtr->setMarkerID( regionFlag );
-		//std::cout<<elementPtr->showMe(true);
-		
-		}
-	}
-	oStr << "done" << std::endl;
-	
-	// add the boundary edges to the mesh
-	oStr << "building the boundary edges...";
+    UInt faceID(0);
+
+    for ( UInt j(0); j<m_y; ++j )
+    {
+        for ( UInt i(0); i<m_x; ++i )
+        {
+            faceID = ( j * m_x + i ) * 2;
+
+            nodeID = j * N_y + i;
+            P0 = nodeID;
+            P1 = nodeID + N_x;
+            P2 = nodeID + N_x + N_y;
+            P3 = nodeID + N_y;
+
+            // Triangle 1
+            elementPtr = &mesh.addElement();
+            elementPtr->setId( faceID );
+            elementPtr->setLocalId( faceID );
+            elementPtr->setPoint( 0, mesh.point(P1) );
+            elementPtr->setPoint( 1, mesh.point(P2) );
+            elementPtr->setPoint( 2, mesh.point(P0) );
+            elementPtr->setMarker( regionFlag );
+
+            // Triangle 2
+            elementPtr = &mesh.addElement();
+            elementPtr->setId( faceID + 1 );
+            elementPtr->setLocalId( faceID + 1 );
+            elementPtr->setPoint( 0, mesh.point(P3) );
+            elementPtr->setPoint( 1, mesh.point(P0) );
+            elementPtr->setPoint( 2, mesh.point(P2) );
+            elementPtr->setMarker( regionFlag );
+        }
+    }
+
+    // add the boundary edges to the mesh
     for ( UInt i = 0; i < boundaryEdgesNumber ; ++i )
     {
-		UInt edgeLabel = 0;
-		UInt adjID = 0;
-		UInt pos = 0;
+        UInt edgeLabel = 0;
+        UInt adjID = 0;
+        UInt pos = 0;
 
-		//edge labels are:
-		// BOTTOM = 1
-		// LEFT = 2
-		// RIGHT = 3
-		// TOP = 4
+        if(i < m_x)
+        {
+            nodeID = i;
+            P0 = nodeID;
+            P1 = nodeID + 1;
+            edgeLabel = 1; //BOTTOMEDGE
+            adjID = 2*i;
+            pos = 2;
 
-		if(i < m_x)
-		{
-			nodeID = i;
-			P0 = nodeID;
-			P1 = nodeID + 1;
-			edgeLabel = 1; //BOTTOMEDGE
-			adjID = 2*i;
-			pos = 2;
-
-		}else if(i < m_x + m_y)
-		{
-			nodeID = (i + 1 - m_x)*n_x -1;
-			P0 = nodeID;
-			P1 = nodeID + n_x;
-			edgeLabel = 3; //RIGHTEDGE
-			adjID =  ((i - m_x)*m_x + m_x - 1)*2;
-			pos = 0;
-		}
-		else if(i < 2*m_x + m_y)
-		{
-			nodeID = n_x*n_y - 1 - (i - m_x - m_y);
-			P0 = nodeID;
-			P1 = nodeID - 1;
-			edgeLabel = 4; //TOPEDGE
-			adjID = 2*m_x*m_y-(i - m_x - m_y)*2 -1;
-			pos = 2;
-		}
-		else{
-			nodeID =  n_x*n_y - 1 - m_x - (i - 2*m_x - m_y)*n_x ;
-			P0 = nodeID;
-			P1 = nodeID - n_x ;
-			edgeLabel = 2; //LEFTEDGE
-			adjID = -(i - 2*m_x - m_y)*(2*m_x) + (m_x*(m_y-1))*2 + 1;
-			pos = 0;
-		}
+        }else if(i < m_x + m_y)
+        {
+            nodeID = (i + 1 - m_x)*n_x -1;
+            P0 = nodeID;
+            P1 = nodeID + n_x;
+            edgeLabel = 3; //RIGHTEDGE
+            adjID =  ((i - m_x)*m_x + m_x - 1)*2;
+            pos = 0;
+        }
+        else if(i < 2*m_x + m_y)
+        {
+            nodeID = n_x*n_y - 1 - (i - m_x - m_y);
+            P0 = nodeID;
+            P1 = nodeID - 1;
+            edgeLabel = 4; //TOPEDGE
+            adjID = 2*m_x*m_y-(i - m_x - m_y)*2 -1;
+            pos = 2;
+        }
+        else{
+            nodeID =  n_x*n_y - 1 - m_x - (i - 2*m_x - m_y)*n_x ;
+            P0 = nodeID;
+            P1 = nodeID - n_x ;
+            edgeLabel = 2; //LEFTEDGE
+            adjID = -(i - 2*m_x - m_y)*(2*m_x) + (m_x*(m_y-1))*2 + 1;
+            pos = 0;
+        }
 
 
-		std::cout<<"Edge# "<<i<<"  PO="<<P0<<"  P1="<<P1<<"  Adj="<<adjID<<std::endl;
         edgePtr = &mesh.addFacet( true ) ;
-        edgePtr->setMarkerID( edgeLabel );
+        edgePtr->setMarker( edgeLabel );
         edgePtr->setPoint( 0, mesh.point( P0 ));
         edgePtr->setPoint( 1, mesh.point( P1 ));
         edgePtr->firstAdjacentElementIdentity() = adjID;
         edgePtr->firstAdjacentElementPosition() = pos;
      }
 
-    oStr << "done" << std::endl;
-    
-    std::cout << "Numero elementi" << mesh.elementList().size();
-    std::cout << "Numero bedge" << boundaryEdgesNumber;
-    std::cout << "Numero edge" << edgesNumber;
-	// edges update
-    mesh.updateElementFacets( true, true, edgesNumber );
-    
-    // mesh control
-    // std::cout<<mesh.check( 0, true, true )<<std::endl;
-    // mesh.showMe();
-}
+    // edges update
+    mesh.updateElementFacets( true, false, edgesNumber );
 
-//@}
+}
 
 
 } // Namespace LifeV
