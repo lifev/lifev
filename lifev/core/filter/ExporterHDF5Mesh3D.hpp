92ee5fe2ecd2b11edd116d2eaffa1f84feeb751f
Antonio Cervone <locutus80@gmail.com>
2012-04-25 18:08:01 +0200

    fix return of temporary value

--- lifev/core/filter/ExporterHDF5Mesh3D.hpp
+++ lifev/core/filter/ExporterHDF5Mesh3D.hpp
@@ -179,7 +179,7 @@ public:
     Int queryStoredInterfaceNumber();
 
     //! Get the types of the stored DOF interfaces
-    std::vector<std::string>& queryStoredInterfaceTypes();
+    std::vector<std::string> queryStoredInterfaceTypes();
 
     //! Return a pointer to the graph
     graphPtr_Type getGraph();
@@ -361,7 +361,7 @@ int ExporterHDF5Mesh3D<MeshType>::queryStoredInterfaceNumber()
 }
 
 template <typename MeshType>
-std::vector<std::string>& ExporterHDF5Mesh3D<MeshType>::queryStoredInterfaceTypes()
+std::vector<std::string> ExporterHDF5Mesh3D<MeshType>::queryStoredInterfaceTypes()
 {
     if (this->M_HDF5.get() == 0)
     {
