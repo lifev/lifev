01c9b2a362e824d3aec059cab94c11013adf746b
Cristiano Malossi <cristiano.malossi@epfl.ch>
2012-08-06 14:18:45 +0200

    Fixing includes.

--- lifev/multiscale/solver/MultiscaleCouplingMeanTotalNormalStress.cpp
+++ lifev/multiscale/solver/MultiscaleCouplingMeanTotalNormalStress.cpp
@@ -34,7 +34,7 @@
  *  @maintainer Cristiano Malossi <cristiano.malossi@epfl.ch>
  */
 
-#include <lifemc/lifesolver/MultiscaleCouplingMeanTotalNormalStress.hpp>
+#include <lifev/multiscale/solver/MultiscaleCouplingMeanTotalNormalStress.hpp>
 
 namespace LifeV
 {
