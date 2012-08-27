4566659fe589e79e7ad6f066114e231bd958e2ff
Antonio Cervone <ant.cervone@gmail.com>
2012-08-27 16:25:20 +0200

    remove useless return in EntityFlags::name()

--- lifev/core/mesh/MeshEntity.cpp
+++ lifev/core/mesh/MeshEntity.cpp
@@ -4,27 +4,28 @@
  *  Created on: 11 Sep 2011
  *      Author: formaggia
  */
+#include <lifev/core/LifeV.hpp>
 #include <lifev/core/mesh/MeshEntity.hpp>
-#include <iostream>
+
 namespace LifeV{
+
 namespace EntityFlags{
 
-//
 std::string name( const flag_Type & flag )
 {
     switch( flag )
     {
-        case PHYSICAL_BOUNDARY: return "PHYSICAL_BOUNDARY";
-        case INTERNAL_INTERFACE: return "INTERNAL_INTERFACE";
+        case PHYSICAL_BOUNDARY:   return "PHYSICAL_BOUNDARY";
+        case INTERNAL_INTERFACE:  return "INTERNAL_INTERFACE";
         case SUBDOMAIN_INTERFACE: return "SUBDOMAIN_INTERFACE";
-        case OVERLAP: return "OVERLAP";
-        case CUTTED: return "CUTTED";
-        case VERTEX: return "VERTEX";
-        case GHOST_ENTITY: return "GHOST_ENTITY";
-        default: return "FLAG_NOT_FOUND";
-        return "NONE";
+        case OVERLAP:             return "OVERLAP";
+        case CUTTED:              return "CUTTED";
+        case VERTEX:              return "VERTEX";
+        case GHOST_ENTITY:        return "GHOST_ENTITY";
+        default:                  return "FLAG_NOT_FOUND";
     }
 }
+
 }// namespace EntityFlags
 
 void MeshEntity::showMe( std::ostream& output) const
