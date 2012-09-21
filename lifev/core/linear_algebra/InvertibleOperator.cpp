2c6a42b04c217d69c953629ac647080d7b04f477
Luca Bertagna <bartgol@KISH.(none)>
2012-09-21 16:46:52 -0400

    Important bug fix in InvertibleOperator. When mixing Teuchos::RCP with booster::shared_ptr it's important not to give to both of them the ownership, otherwise one might delete the object while the other one still needs it. Therefore we store both a Teuchos and a boost pointer, so that InvertibleOperator is sure that the underlying object is still alive.

--- lifev/core/linear_algebra/InvertibleOperator.cpp
+++ lifev/core/linear_algebra/InvertibleOperator.cpp
@@ -8,62 +8,70 @@ namespace Operators
 {
 
 InvertibleOperator::InvertibleOperator():
-	M_name("InvertibleOperator"),
-	M_useTranspose(false)
+    M_name("InvertibleOperator"),
+    M_useTranspose(false)
 { }
 
 int InvertibleOperator::SetUseTranspose(bool useTranspose)
 {
-	M_useTranspose = useTranspose;
+    M_useTranspose = useTranspose;
 
-	int ierr(0);
-	if(M_useTranspose)
-		ierr = -1;
+    int ierr(0);
+    if(M_useTranspose)
+        ierr = -1;
 
-	return ierr;
+    return ierr;
 }
 
 void InvertibleOperator::setOperator(const operatorPtr_Type& _oper)
 {
-	ASSERT_PRE(_oper.get() != this, "Can't self assign");
-	ASSERT_PRE(_oper.get() != 0, "Can't assign a null pointer");
-	M_oper = Teuchos::rcp(_oper);
-	doSetOperator();
+    ASSERT_PRE(_oper.get() != this, "Can't self assign");
+    ASSERT_PRE(_oper.get() != 0, "Can't assign a null pointer");
+
+    // Since Teuchos and boost have different reference counter, we make sure that a copy
+    // of the object is available to this class, using a boost to keep the bost count >0
+    M_operBoost = _oper;
+    M_oper.reset(_oper.get(),false);
+    doSetOperator();
 }
 
 void InvertibleOperator::setPreconditioner(const operatorPtr_Type& _prec)
 {
-	ASSERT_PRE(_prec.get() != this, "Self Assignment is forbidden");
-	ASSERT_PRE(_prec.get() != 0, "Can't assign a null pointer");
-	M_prec = Teuchos::rcp(_prec);
-	doSetPreconditioner();
+    ASSERT_PRE(_prec.get() != this, "Self Assignment is forbidden");
+    ASSERT_PRE(_prec.get() != 0, "Can't assign a null pointer");
+
+    // Since Teuchos and boost have different reference counter, we make sure that a copy
+    // of the object is available to this class, using a boost to keep the bost count >0
+    M_precBoost = _prec;
+    M_prec.reset(_prec.get(),false);
+    doSetPreconditioner();
 }
 
 void InvertibleOperator::setParameterList(const Teuchos::ParameterList& _pList)
 {
-	M_pList = Teuchos::rcp(new Teuchos::ParameterList(_pList), true);
-	doSetParameterList();
+    M_pList = Teuchos::rcp(new Teuchos::ParameterList(_pList), true);
+    doSetParameterList();
 }
 
 int InvertibleOperator::Apply(const vector_Type& X, vector_Type& Y) const
 {
-	ASSERT_PRE(M_oper.assert_valid_ptr().get() != 0, "M_oper must be assigned");
-	ASSERT_PRE(X.Map().SameAs(M_oper->OperatorDomainMap()), "X and domain map do no coincide \n");
-	ASSERT_PRE(Y.Map().SameAs(M_oper->OperatorRangeMap()) , "Y and range map do no coincide \n");
+    ASSERT_PRE(M_oper.assert_valid_ptr().get() != 0, "M_oper must be assigned");
+    ASSERT_PRE(X.Map().SameAs(M_oper->OperatorDomainMap()), "X and domain map do no coincide \n");
+    ASSERT_PRE(Y.Map().SameAs(M_oper->OperatorRangeMap()) , "Y and range map do no coincide \n");
 
-	return M_oper->Apply(X,Y);
+    return M_oper->Apply(X,Y);
 }
 
 int InvertibleOperator::ApplyInverse(const vector_Type& X, vector_Type& Y) const
 {
-	ASSERT_PRE(M_oper.assert_valid_ptr().get() != 0, "M_oper must be assigned \n");
-	ASSERT_PRE(Y.Map().SameAs(M_oper->OperatorDomainMap()), "Y and domain map do no coincide \n");
-	ASSERT_PRE(X.Map().SameAs(M_oper->OperatorRangeMap()) , "X and range map do no coincide \n");
+    ASSERT_PRE(M_oper.assert_valid_ptr().get() != 0, "M_oper must be assigned \n");
+    ASSERT_PRE(Y.Map().SameAs(M_oper->OperatorDomainMap()), "Y and domain map do no coincide \n");
+    ASSERT_PRE(X.Map().SameAs(M_oper->OperatorRangeMap()) , "X and range map do no coincide \n");
 
-	if (M_useTranspose)
-		return -1;
+    if (M_useTranspose)
+        return -1;
 
-	return doApplyInverse(X,Y);
+    return doApplyInverse(X,Y);
 }
 
 } // Namespace Operators
--- lifev/core/linear_algebra/InvertibleOperator.hpp
+++ lifev/core/linear_algebra/InvertibleOperator.hpp
@@ -41,75 +41,79 @@ class InvertibleOperator : public LinearOperator
 {
 public:
 
-	InvertibleOperator();
+    InvertibleOperator();
 
-	//! @name Attribute set methods
-	//@{
+    //! @name Attribute set methods
+    //@{
 
-	//! If set true, transpose of this operator will be applied.
-	virtual int SetUseTranspose(bool useTranspose);
+    //! If set true, transpose of this operator will be applied.
+    virtual int SetUseTranspose(bool useTranspose);
 
-	void setOperator(const operatorPtr_Type & _oper);
+    void setOperator(const operatorPtr_Type & _oper);
 
-	void setPreconditioner(const operatorPtr_Type & _prec);
+    void setPreconditioner(const operatorPtr_Type & _prec);
 
-	void setParameterList(const Teuchos::ParameterList & _pList);
+    void setParameterList(const Teuchos::ParameterList & _pList);
 
-	//@}
+    //@}
 
-	//! @name Mathematical functions
-	//@{
+    //! @name Mathematical functions
+    //@{
 
-	//! Returns the result of a Epetra_Operator applied to a vector_Type X in Y.
-	virtual int Apply(const vector_Type& X, vector_Type& Y) const;
+    //! Returns the result of a Epetra_Operator applied to a vector_Type X in Y.
+    virtual int Apply(const vector_Type& X, vector_Type& Y) const;
 
-	//! Returns the result of a Epetra_Operator inverse applied to an vector_Type X in Y.
-	virtual int ApplyInverse(const vector_Type& X, vector_Type& Y) const;
+    //! Returns the result of a Epetra_Operator inverse applied to an vector_Type X in Y.
+    virtual int ApplyInverse(const vector_Type& X, vector_Type& Y) const;
 
-	//! Returns the infinity norm of the global matrix.
-	double NormInf() const {return M_oper->NormInf();}
+    //! Returns the infinity norm of the global matrix.
+    double NormInf() const {return M_oper->NormInf();}
 
-	//@}
+    //@}
 
-	//! @name Attribute access functions
-	//@{
+    //! @name Attribute access functions
+    //@{
 
-	//! Returns a character string describing the operator
-	virtual const char * Label() const {return M_name.c_str();}
+    //! Returns a character string describing the operator
+    virtual const char * Label() const {return M_name.c_str();}
 
-	//! Returns the current UseTranspose setting.
-	virtual bool UseTranspose() const {return M_useTranspose;}
+    //! Returns the current UseTranspose setting.
+    virtual bool UseTranspose() const {return M_useTranspose;}
 
-	//! Returns true if the \e this object can provide an approximate Inf-norm, false otherwise.
-	virtual bool HasNormInf() const {return M_oper->HasNormInf();}
+    //! Returns true if the \e this object can provide an approximate Inf-norm, false otherwise.
+    virtual bool HasNormInf() const {return M_oper->HasNormInf();}
 
-	//! Returns a pointer to the Epetra_Comm communicator associated with this operator.
-	virtual const comm_Type & Comm() const {return M_oper->Comm();}
+    //! Returns a pointer to the Epetra_Comm communicator associated with this operator.
+    virtual const comm_Type & Comm() const {return M_oper->Comm();}
 
-	//! Returns the Epetra_Map object associated with the domain of this operator.
-	virtual const map_Type & OperatorDomainMap() const {return M_oper->OperatorDomainMap();}
+    //! Returns the Epetra_Map object associated with the domain of this operator.
+    virtual const map_Type & OperatorDomainMap() const {return M_oper->OperatorDomainMap();}
 
-	//! Returns the Epetra_Map object associated with the range of this operator.
-	virtual const map_Type & OperatorRangeMap() const {return M_oper->OperatorRangeMap();}
+    //! Returns the Epetra_Map object associated with the range of this operator.
+    virtual const map_Type & OperatorRangeMap() const {return M_oper->OperatorRangeMap();}
 
-	//@}
+    //@}
 protected:
 
-	virtual int doApplyInverse(const vector_Type& X, vector_Type& Y) const = 0;
-	virtual void doSetOperator() = 0;
-	virtual void doSetPreconditioner() = 0;
-	virtual void doSetParameterList() = 0;
+    virtual int doApplyInverse(const vector_Type& X, vector_Type& Y) const = 0;
+    virtual void doSetOperator() = 0;
+    virtual void doSetPreconditioner() = 0;
+    virtual void doSetParameterList() = 0;
 
-	//! The name of the Operator
-	std::string M_name;
-	//! The list of Parameter to feed the linear solver
-	Teuchos::RCP<Teuchos::ParameterList> M_pList;
-	//! The preconditioner operator
-	Teuchos::RCP<Epetra_Operator> M_prec;
-	//! The operator to be solved
-	Teuchos::RCP<Epetra_Operator> M_oper;
-	//! Whenever to use the transpose
-	bool M_useTranspose;
+    //! The name of the Operator
+    std::string M_name;
+    //! The list of Parameter to feed the linear solver
+    Teuchos::RCP<Teuchos::ParameterList> M_pList;
+
+    //! The preconditioner operator (the boost copy makes sure that the preconditioner is still alive; see .cpp file for details)
+    Teuchos::RCP<Epetra_Operator> M_prec;
+    boost::shared_ptr<Epetra_Operator> M_precBoost;
+
+    //! The operator to be solved (the boost copy makes sure that the operator is still alive; see .cpp file for details)
+    Teuchos::RCP<Epetra_Operator> M_oper;
+    boost::shared_ptr<Epetra_Operator> M_operBoost;
+    //! Whenever to use the transpose
+    bool M_useTranspose;
 };
 
 typedef FactorySingleton<Factory<InvertibleOperator, std::string> > InvertibleOperatorFactory;
