0593c07b4f0f0105260653d7934f2810fb1b7295
fumagalli <alessio.fumagalli@mail.polimi.it>
2012-11-12 09:17:46 +0100

    Fix some type error in the doxy of the Darcy Solvers

--- lifev/darcy/solver/DarcySolverNonLinear.hpp
+++ lifev/darcy/solver/DarcySolverNonLinear.hpp
@@ -237,7 +237,7 @@ namespace LifeV
     \f]
     @note In the code we do not use the matrix \f$ H \f$ and the vector \f$ G \f$, because all the boundary
     conditions are imposed via BCHandler class.
-    @note Example of useing can be found in darcy_nonlinear and darcy_linear.
+    @note Example of usage can be found in darcy_nonlinear and darcy_linear.
     Coupled with an hyperbolic solver in impes.
     @todo Insert any scientific publications that use this solver.
     @todo Attention! We have the hypothesis that we use P0 elements for the primal unknown. Change this in a future!
