92f9b8c12f6735f4560f64734db6aeeb4235716f
Helene Ruffieux <helene.ruffieux@epfl.ch>
2012-12-21 10:05:31 +0100

    Bug fix for intel compiler

--- lifev/eta/expression/IntegrateVectorElement.hpp
+++ lifev/eta/expression/IntegrateVectorElement.hpp
@@ -75,7 +75,7 @@ public:
 
     //! Type of the Evaluation
 	typedef typename ExpressionToEvaluation< ExpressionType,
-                                             TestSpaceType::S_fieldDim,
+                                             TestSpaceType::field_dim,
                                              0,
                                              3>::evaluation_Type evaluation_Type;
 
@@ -162,7 +162,7 @@ private:
 	evaluation_Type M_evaluation;
 
 	ETCurrentFE<3,1>* M_globalCFE;
-	ETCurrentFE<3,TestSpaceType::S_fieldDim>* M_testCFE;
+	ETCurrentFE<3,TestSpaceType::field_dim>* M_testCFE;
 
     //ETVectorElemental<1> M_elementalVector;
     ETVectorElemental M_elementalVector;
@@ -189,10 +189,10 @@ IntegrateVectorElement(const boost::shared_ptr<MeshType>& mesh,
 		M_evaluation(expression),
 
 		M_globalCFE(new ETCurrentFE<3,1>(feTetraP0,geometricMapFromMesh<MeshType>(),quadrature)),
-		M_testCFE(new ETCurrentFE<3,TestSpaceType::S_fieldDim>(testSpace->refFE(),testSpace->geoMap(),quadrature)),
+		M_testCFE(new ETCurrentFE<3,TestSpaceType::field_dim>(testSpace->refFE(),testSpace->geoMap(),quadrature)),
 
 		//M_elementalVector(testSpace->refFE().nbDof())
-        M_elementalVector(TestSpaceType::S_fieldDim*testSpace->refFE().nbDof())
+        M_elementalVector(TestSpaceType::field_dim*testSpace->refFE().nbDof())
 {
     M_evaluation.setQuadrature(quadrature);
     M_evaluation.setGlobalCFE(M_globalCFE);
@@ -209,7 +209,7 @@ IntegrateVectorElement( const IntegrateVectorElement < MeshType, TestSpaceType, 
 		M_evaluation(integrator.M_evaluation),
 
 	  	M_globalCFE(new ETCurrentFE<3,1>(feTetraP0,geometricMapFromMesh<MeshType>(),M_quadrature)),
-		M_testCFE(new ETCurrentFE<3,TestSpaceType::S_fieldDim>(M_testSpace->refFE(), M_testSpace->geoMap(),M_quadrature)),
+		M_testCFE(new ETCurrentFE<3,TestSpaceType::field_dim>(M_testSpace->refFE(), M_testSpace->geoMap(),M_quadrature)),
 
 		M_elementalVector(integrator.M_elementalVector)
 {
@@ -268,7 +268,7 @@ addTo(VectorType& vec)
 
         // Loop on the blocks
 
-        for (UInt iblock(0); iblock < TestSpaceType::S_fieldDim; ++iblock)
+        for (UInt iblock(0); iblock < TestSpaceType::field_dim; ++iblock)
         {
             // Set the row global indices in the local vector
             for (UInt i(0); i<nbTestDof; ++i)
