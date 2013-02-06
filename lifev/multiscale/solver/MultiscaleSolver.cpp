386704a00ff239c031059186d29ab39be83d6ae9
Cristiano Malossi <cristiano.malossi@epfl.ch>
2013-02-06 11:36:51 +0100

    Cosmetic change.

--- lifev/multiscale/solver/MultiscaleSolver.cpp
+++ lifev/multiscale/solver/MultiscaleSolver.cpp
@@ -206,10 +206,11 @@ MultiscaleSolver::solveProblem( const Real& referenceSolution, const Real& toler
 
     // Numerical check of the last iteration solution (used for the night testsuite check)
     Real computedSolution( M_model->checkSolution() );
-    if ( referenceSolution >= 0. && std::abs( ( referenceSolution - computedSolution ) / referenceSolution ) >  tolerance )
+    Real relativeError( std::abs( ( referenceSolution - computedSolution ) / referenceSolution ) );
+    if ( referenceSolution >= 0. && relativeError > tolerance )
         multiscaleErrorCheck( Solution, "Problem solution: "    + number2string( computedSolution ) +
-                                        " (Reference solution: " + number2string( referenceSolution ) 
-			      + " Relative error: " + number2string( std::abs( ( referenceSolution - computedSolution ) / referenceSolution ) ) + ")\n", M_comm->MyPID() == 0 );
+                                        " (Reference solution: " + number2string( referenceSolution ) +
+                                        "; Relative error: " + number2string( relativeError ) + ")\n", M_comm->MyPID() == 0 );
 
     return multiscaleExitFlag;
 }
