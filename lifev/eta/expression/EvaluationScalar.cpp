7efc14a01a939c8e7ed692a37b8422d18c2d51be
Luca Pasquale <lgpasquale@gmail.com>
2013-04-28 23:05:39 +0200

    Moved initializations of const static members of completely specialized Evaluation* classes to *.cpp files
    const static members of completely specialized classes (EvaluationPhiI
    and EvaluationPhiJ) and non templetized classes (EvaluationScalar)
    needed to be moved to .cpp files to avoid linking errors when
    Integrate.hpp is included in multiple files to be linked together

---
+++ lifev/eta/expression/EvaluationScalar.cpp
@@ -0,0 +1,54 @@
+//@HEADER
+/*
+*******************************************************************************
+
+   Copyright (C) 2004, 2005, 2007 EPFL, Politecnico di Milano, INRIA
+   Copyright (C) 2010 EPFL, Politecnico di Milano, Emory UNiversity
+
+   This file is part of the LifeV library
+
+   LifeV is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   LifeV is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, see <http://www.gnu.org/licenses/>
+
+
+*******************************************************************************
+*/
+//@HEADER
+
+/*!
+ *   @file
+     @brief This file contains the definition of the EvaluationScalar class.
+
+     @date 06/2011
+     @author Samuel Quinodoz <samuel.quinodoz@epfl.ch>
+ */
+
+#include <lifev/eta/expression/EvaluationScalar.hpp>
+
+
+namespace LifeV
+{
+
+namespace ExpressionAssembly
+{
+
+const flag_Type EvaluationScalar::S_globalUpdateFlag = ET_UPDATE_NONE;
+
+const flag_Type EvaluationScalar::S_testUpdateFlag = ET_UPDATE_NONE;
+
+const flag_Type EvaluationScalar::S_solutionUpdateFlag = ET_UPDATE_NONE;
+
+
+} // Namespace ExpressionAssembly
+
+} // Namespace LifeV
