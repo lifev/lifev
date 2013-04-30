869a45539fdd77cddf2c3688a695c99464176e55
Radu Popescu <i.radu.popescu@gmail.com>
2013-04-30 18:53:10 +0200

    Astyle on the HDF5IO branch

--- lifev/core/testsuite/partition_io/main_write.cpp
+++ lifev/core/testsuite/partition_io/main_write.cpp
@@ -126,7 +126,7 @@ int main (int argc, char** argv)
     else
     {
         boost::shared_ptr<Epetra_MpiComm> mpiComm =
-            boost::dynamic_pointer_cast<Epetra_MpiComm>(comm);
+            boost::dynamic_pointer_cast<Epetra_MpiComm> (comm);
         PartitionIO<mesh_Type> partitionIO (partsFileName, mpiComm);
         partitionIO.write (meshPart.meshPartitions() );
     }
