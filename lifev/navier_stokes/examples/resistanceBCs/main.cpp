effac55020080587f66af597ff7160e4b5485fb4
Paolo Tricerri <paolo.tricerri@epfl.ch>
2013-06-18 08:54:30 +0200

    adding an example with resistance BCs applied explictly

---
+++ lifev/navier_stokes/examples/resistanceBCs/main.cpp
@@ -0,0 +1,99 @@
+/* -*- mode: c++ -*-
+
+  This file is part of the LifeV Applications.
+
+  Author(s): Christophe Prud'homme <christophe.prudhomme@epfl.ch>
+       Date: 2005-04-16
+
+  Copyright (C) 2005 EPFL
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2.1 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+  USA
+*/
+/**
+   \file main.cpp
+   \author Christophe Prud'homme <christophe.prudhomme@epfl.ch>
+   \date 2005-04-16
+ */
+
+#ifdef TWODIM
+#error test_cylinder cannot be compiled in 2D
+#endif
+
+// Tell the compiler to ignore specific kind of warnings:
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+
+#include <Epetra_ConfigDefs.h>
+#ifdef EPETRA_MPI
+#include <mpi.h>
+#include <Epetra_MpiComm.h>
+#else
+#include <Epetra_SerialComm.h>
+#endif
+
+//Tell the compiler to restore the warning previously silented
+#pragma GCC diagnostic warning "-Wunused-variable"
+#pragma GCC diagnostic warning "-Wunused-parameter"
+
+#include <lifev/core/LifeV.hpp>
+#include <lifev/core/algorithm/PreconditionerIfpack.hpp>
+#include <lifev/core/algorithm/PreconditionerML.hpp>
+
+#include "cylinder.hpp"
+
+
+using namespace LifeV;
+
+std::set<UInt> parseList ( const std::string& list )
+{
+    std::string stringList = list;
+    std::set<UInt> setList;
+    if ( list == "" )
+    {
+        return setList;
+    }
+    size_t commaPos = 0;
+    while ( commaPos != std::string::npos )
+    {
+        commaPos = stringList.find ( "," );
+        setList.insert ( atoi ( stringList.substr ( 0, commaPos ).c_str() ) );
+        stringList = stringList.substr ( commaPos + 1 );
+    }
+    setList.insert ( atoi ( stringList.c_str() ) );
+    return setList;
+}
+
+int
+main ( int argc, char** argv )
+{
+#ifdef HAVE_MPI
+    MPI_Init (&argc, &argv);
+#endif
+
+    //**************** cylinder
+    //    MPI_Init(&argc,&argv);
+
+    Cylinder cyl ( argc, argv );
+    cyl.run();
+
+#ifdef HAVE_MPI
+    MPI_Finalize();
+#endif
+    return ( EXIT_SUCCESS );
+}
+
+
+
