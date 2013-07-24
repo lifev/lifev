125cac2c66cdcdee8d300827a041a59991fec14d
Claudia Maria Colciago <colciago@mathicsepc4.epfl.ch>
2013-07-24 16:11:53 +0200

    Fixing the order of the packages

--- PackagesList.cmake
+++ PackagesList.cmake
@@ -74,12 +74,12 @@ INCLUDE(TribitsListHelpers)
 
 SET( LifeV_PACKAGES_AND_DIRS_AND_CLASSIFICATIONS
   Core                  lifev/core                        PS
+  ETA                   lifev/eta                         PS
   BCInterface           lifev/bc_interface                PS
   OneDFSI               lifev/one_d_fsi                   PS
   ZeroDimensional       lifev/zero_dimensional            PS  
   LevelSet              lifev/level_set                   PS
   Darcy                 lifev/darcy                       PS
-  ETA                   lifev/eta                         PS
   NavierStokes          lifev/navier_stokes               PS
   Structure             lifev/structure                   PS
   Heart                 lifev/heart                       PS
