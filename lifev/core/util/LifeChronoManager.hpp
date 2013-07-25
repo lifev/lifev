33262741d781d41c09fe827b378956c7cdab6ee1
Antonio Cervone <ant.cervone@gmail.com>
2013-07-25 16:02:04 +0200

    reorder and document LifeChronoManager

--- lifev/core/util/LifeChronoManager.hpp
+++ lifev/core/util/LifeChronoManager.hpp
@@ -48,6 +48,10 @@
 namespace LifeV
 {
 
+//! @name LifeChronoManager - chronometer manager class
+/*!
+  This class is used for managing multiple chronometers
+*/
 template <typename TimerType = LifeChrono>
 class LifeChronoManager
 {
@@ -56,65 +60,86 @@ public:
     typedef std::map<std::string const, timer_Type*> timerList_Type;
     typedef boost::shared_ptr<Epetra_Comm const> commPtr_Type;
 
+    /*!
+     * @brief Constructor
+     * @param comm Communicator
+     */
     LifeChronoManager ( commPtr_Type comm ) :
         M_stringMaxSize ( 16 ),
         M_comm ( comm )
     {}
 
-    void add ( std::string const& name, timer_Type* timer )
-    {
-        UInt const nameSize = name.size();
-        if ( nameSize > M_stringMaxSize )
-        {
-            M_stringMaxSize = nameSize;
-        }
-        M_timerList.insert ( std::make_pair ( name, timer) );
-    }
+    /*!
+     * @brief Register a timer
+     * @param name String to be displayed when printing data relative to the timer
+     * @param timer A pointer to the timer to be registered
+     */
+    void add ( std::string const& name, timer_Type* timer );
 
-    void print ( std::ostream& out = std::cout )
-    {
-        UInt const printSize = 80;
-        bool isLeader = M_comm->MyPID() == 0;
-
-        std::vector<Real> times ( M_timerList.size() );
-        Real globalTime = 0;
-        UInt count = 0;
-        for ( typename timerList_Type::const_iterator it = M_timerList.begin();
-                it != M_timerList.end(); ++it, count++ )
-        {
-            times[ count ] = it->second->globalDiff ( *M_comm );
-            globalTime += times[ count ];
-        }
-
-        if ( isLeader )
-        {
-            out << std::string (printSize, '=') << std::endl;
-            out << std::setw ( M_stringMaxSize ) << "Name";
-            out << std::setw ( 16 ) << "Time (s)";
-            out << std::setw ( 16 ) << "Perc (%)" << std::endl;
-            out << std::string (printSize, '=') << std::endl;
-
-            count = 0;
-            for ( typename timerList_Type::const_iterator it = M_timerList.begin();
-                    it != M_timerList.end(); ++it, count++ )
-            {
-                out << std::setw ( M_stringMaxSize ) << it->first;
-                out << std::setw ( 16 ) << std::fixed << std::setprecision (2) << times[ count ];
-                out << std::setw ( 16 ) << std::fixed << std::setprecision (2) << 100.* times[ count ] / globalTime << std::endl;
-            }
-            out << std::string (printSize, '=') << std::endl;
-            out << std::setw ( M_stringMaxSize ) << "Total Time";
-            out << std::setw ( 16 ) << std::fixed << std::setprecision (2) << globalTime;
-            out << std::setw ( 16 ) << std::fixed << std::setprecision (2) << 100. << std::endl;
-            out << std::string (printSize, '=') << std::endl;
-        }
-    }
+    /*!
+     * @brief Print out strings and time diffs for the registered timers
+     * \param out Output stream
+     */
+    void print ( std::ostream& out = std::cout );
 
 protected:
     timerList_Type M_timerList;
     UInt M_stringMaxSize;
     commPtr_Type M_comm;
-};
+
+    static UInt const S_printSize = 80;
+    static UInt const S_columnSize = 16;
+}; // class LifeChronoManager
+
+template <typename TimerType>
+inline void LifeChronoManager<TimerType>::add ( std::string const& name, timer_Type* timer )
+{
+    UInt const nameSize = name.size();
+    if ( nameSize > M_stringMaxSize )
+    {
+        M_stringMaxSize = nameSize;
+    }
+    M_timerList.insert ( std::make_pair ( name, timer) );
+} // LifeChronoManager::add
+
+template <typename TimerType>
+inline void LifeChronoManager<TimerType>::print ( std::ostream& out )
+{
+    bool isLeader = M_comm->MyPID() == 0;
+
+    std::vector<Real> times ( M_timerList.size() );
+    Real globalTime = 0;
+    UInt count = 0;
+    for ( typename timerList_Type::const_iterator it = M_timerList.begin();
+            it != M_timerList.end(); ++it, count++ )
+    {
+        times[ count ] = it->second->globalDiff ( *M_comm );
+        globalTime += times[ count ];
+    }
+
+    if ( isLeader )
+    {
+        out << std::string (S_printSize, '=') << std::endl;
+        out << std::setw ( M_stringMaxSize ) << "Name";
+        out << std::setw ( S_columnSize ) << "Time (s)";
+        out << std::setw ( S_columnSize ) << "Perc (%)" << std::endl;
+        out << std::string (S_printSize, '=') << std::endl;
+
+        count = 0;
+        for ( typename timerList_Type::const_iterator it = M_timerList.begin();
+                it != M_timerList.end(); ++it, count++ )
+        {
+            out << std::setw ( M_stringMaxSize ) << it->first;
+            out << std::setw ( S_columnSize ) << std::fixed << std::setprecision (2) << times[ count ];
+            out << std::setw ( S_columnSize ) << std::fixed << std::setprecision (2) << 100.* times[ count ] / globalTime << std::endl;
+        }
+        out << std::string (S_printSize, '=') << std::endl;
+        out << std::setw ( M_stringMaxSize ) << "Total Time";
+        out << std::setw ( S_columnSize ) << std::fixed << std::setprecision (2) << globalTime;
+        out << std::setw ( S_columnSize ) << std::fixed << std::setprecision (2) << 100. << std::endl;
+        out << std::string (S_printSize, '=') << std::endl;
+    }
+} // LifeChronoManager::print
 
 } // end namespace LifeV
 
