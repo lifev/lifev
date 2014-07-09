906cf70b9a176893e824b131fe1465a318d30b35
Simone Deparis <simone.deparis@epfl.ch>
2014-07-09 20:35:30 +0200

    Electrophysioogy is still experimental since c++11 is required

--- PackagesList.cmake
+++ PackagesList.cmake
@@ -81,7 +81,7 @@ SET( LifeV_PACKAGES_AND_DIRS_AND_CLASSIFICATIONS
   Darcy                 lifev/darcy                       PS
   NavierStokes          lifev/navier_stokes               PS
   Structure             lifev/structure                   PS
-  Electrophysiology     lifev/electrophysiology           PS
+  Electrophysiology     lifev/electrophysiology           EX
   Heart                 lifev/heart                       EX
   FSI                   lifev/fsi                         PS
   ZeroDimensional       lifev/zero_dimensional            PS
