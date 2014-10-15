644b7095fe5870e25add7076705acbc472e88ced
Davide Forti <davide.forti@epfl.ch>
2014-10-15 18:32:18 +0200

    Adding time advancing and exporter

--- lifev/navier_stokes/examples/test_block_solver/main.cpp
+++ lifev/navier_stokes/examples/test_block_solver/main.cpp
@@ -38,6 +38,10 @@
 #include <lifev/core/mesh/MeshData.hpp>
 #include <lifev/core/mesh/MeshPartitioner.hpp>
 #include "lifev/navier_stokes/solver/NavierStokesSolver.hpp"
+#include <lifev/core/fem/TimeAndExtrapolationHandler.hpp>
+#include <lifev/core/filter/ExporterEnsight.hpp>
+#include <lifev/core/filter/ExporterHDF5.hpp>
+#include <lifev/core/filter/ExporterVTK.hpp>
 
 using namespace LifeV;
 
@@ -58,6 +62,8 @@ main ( int argc, char** argv )
 #endif
 
     typedef RegionMesh<LinearTetra> mesh_Type;
+    typedef VectorEpetra vector_Type;
+    typedef boost::shared_ptr<vector_Type> vectorPtr_Type;
 
     // Reading the dataFile
     const std::string defaultDataName = "data";
@@ -81,6 +87,57 @@ main ( int argc, char** argv )
     NavierStokesSolver ns( dataFile, Comm);
     ns.setup(localMeshPtr);
     ns.buildSystem();
+
+    // Time handler objects to deal with time advancing and extrapolation
+    TimeAndExtrapolationHandler timeVelocity;
+    Real dt       = dataFile("fluid/time_discretization/timestep",0.0);
+    Real t0       = dataFile("fluid/time_discretization/initialtime",0.0);
+    Real tFinal   = dataFile("fluid/time_discretization/endtime",0.0);
+    UInt orderBDF = dataFile("fluid/time_discretization/BDF_order",2);
+
+    // Order of BDF and extrapolation for the velocity
+    timeVelocity.setBDForder(orderBDF);
+    timeVelocity.setMaximumExtrapolationOrder(orderBDF);
+    timeVelocity.setTimeStep(dt);
+
+    // Initialize time advance
+    vector_Type velocityInitial ( ns.uFESpace()->map() );
+    std::vector<vector_Type> initialStateVelocity;
+    velocityInitial *= 0 ;
+    for ( UInt i = 0; i < orderBDF; ++i )
+    	initialStateVelocity.push_back(velocityInitial);
+
+    timeVelocity.initialize(initialStateVelocity);
+
+    // Exporter
+    std::string outputName = dataFile ( "exporter/filename", "result");
+    boost::shared_ptr< Exporter<mesh_Type > > exporter;
+    std::string const exporterType =  dataFile ( "exporter/type", "ensight");
+
+#ifdef HAVE_HDF5
+    if (exporterType.compare ("hdf5") == 0)
+    {
+    	exporter.reset ( new ExporterHDF5<mesh_Type > ( dataFile, outputName ) );
+    	exporter->setPostDir ( "./" ); // This is a test to see if M_post_dir is working
+    	exporter->setMeshProcId ( localMeshPtr, Comm->MyPID() );
+    }
+#endif
+    else if(exporterType.compare ("vtk") == 0)
+    {
+    	exporter.reset ( new ExporterVTK<mesh_Type > ( dataFile, outputName ) );
+    	exporter->setPostDir ( "./" ); // This is a test to see if M_post_dir is working
+    	exporter->setMeshProcId ( localMeshPtr, Comm->MyPID() );
+    }
+
+    vectorPtr_Type velocity( new vector_Type(ns.uFESpace()->map(), exporter->mapType() ) );
+    vectorPtr_Type pressure( new vector_Type(ns.pFESpace()->map(), exporter->mapType() ) );
+    exporter->addVariable ( ExporterData<mesh_Type>::VectorField, "velocity", ns.uFESpace(), velocity, UInt (0) );
+    exporter->addVariable ( ExporterData<mesh_Type>::ScalarField, "pressure", ns.pFESpace(), pressure, UInt (0) );
+    exporter->postProcess ( t0 );
+
+
+
+    exporter->closeFile();
 
 #ifdef HAVE_MPI
     if (verbose)
