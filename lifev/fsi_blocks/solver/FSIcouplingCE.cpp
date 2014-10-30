e534da91b452efa0de7fbb5372c3e837b9b28c3b
Davide Forti <davide.forti@epfl.ch>
2014-10-30 09:29:37 +0100

    Creating class for handling the coupling blocks for the CE

---
+++ lifev/fsi_blocks/solver/FSIcouplingCE.cpp
@@ -0,0 +1,127 @@
+#include <lifev/fsi_blocks/solver/FSIcouplingCE.hpp>
+
+namespace LifeV
+{
+
+FSIcouplingCE::FSIcouplingCE ( const commPtr_Type& communicator ) :
+M_comm ( communicator )
+{
+}
+
+FSIcouplingCE::~FSIcouplingCE ( )
+{
+}
+
+void
+FSIcouplingCE::setUp ( const Real& timeStep, const Real& interfaceDofs, const Real& coefficientFirstDerivative,
+					   const mapPtr_Type& interfaceMap, const FESpacePtr_Type& fluidVelocityFESpace,
+					   const FESpacePtr_Type& structureDisplacementFESpace, const vectorPtr_Type& numerationInterface)
+{
+	M_timeStep = timeStep;
+	M_interface = interfaceDofs; // Number of interface dofs per component
+	M_fluidVelocityFESpace = fluidVelocityFESpace;
+	M_structureDisplacementFESpace = structureDisplacementFESpace;
+	M_interfaceMap = interfaceMap;
+	M_numerationInterface = numerationInterface;
+	M_coefficientFirstDerivative = coefficientFirstDerivative;
+}
+
+void
+FSIcouplingCE::buildBlocks ( std::map<ID, ID> const& localDofMap )
+{
+	Real value = -1.0;
+	std::map<ID, ID>::const_iterator ITrow;
+
+	M_lambdaToFluidMomentum.reset ( new MatrixEpetra<Real> ( M_fluidVelocityFESpace->map() ) );
+	for (UInt dim = 0; dim < 3; ++dim)
+	{
+		for (ITrow = localDofMap.begin(); ITrow != localDofMap.end(); ++ITrow)
+		{
+			if ( M_numerationInterface->map().map (Unique)->LID ( static_cast<EpetraInt_Type> (ITrow->second) ) >= 0 )
+			{
+				M_lambdaToFluidMomentum->addToCoefficient( ITrow->first + dim * M_fluidVelocityFESpace->dof().numTotalDof(),
+														   (int) (*M_numerationInterface)[ITrow->second] + dim * M_interface,
+														   value );
+			}
+		}
+	}
+	M_lambdaToFluidMomentum->globalAssemble(M_interfaceMap, M_fluidVelocityFESpace->mapPtr());
+
+	// ----------------------------------------------------------------------------------------------------------------------------
+
+	value = 1.0;
+	M_lambdaToStructureMomentum.reset ( new MatrixEpetra<Real> ( M_structureDisplacementFESpace->map() ) );
+	for (UInt dim = 0; dim < 3; ++dim)
+	{
+		for (ITrow = localDofMap.begin(); ITrow != localDofMap.end(); ++ITrow)
+		{
+			if ( M_numerationInterface->map().map (Unique)->LID ( static_cast<EpetraInt_Type> (ITrow->second) ) >= 0 )
+			{
+				M_lambdaToStructureMomentum->addToCoefficient( ITrow->second + dim * M_structureDisplacementFESpace->dof().numTotalDof(),
+													 	 	   (int) (*M_numerationInterface)[ITrow->second] + dim * M_interface,
+													 	 	   value );
+			}
+		}
+	}
+	M_lambdaToStructureMomentum->globalAssemble(M_interfaceMap, M_structureDisplacementFESpace->mapPtr());
+
+	// ----------------------------------------------------------------------------------------------------------------------------
+
+	value = 1.0;
+	M_fluidVelocityToLambda.reset ( new MatrixEpetra<Real> ( *M_interfaceMap ) );
+	for (UInt dim = 0; dim < 3; ++dim)
+	{
+		for (ITrow = localDofMap.begin(); ITrow != localDofMap.end(); ++ITrow)
+		{
+			if ( M_numerationInterface->map().map (Unique)->LID ( static_cast<EpetraInt_Type> (ITrow->second) ) >= 0 )
+			{
+				M_fluidVelocityToLambda->addToCoefficient( (int) (*M_numerationInterface)[ITrow->second] + dim * M_interface,
+						                                   ITrow->second + dim * M_structureDisplacementFESpace->dof().numTotalDof(),
+						                                   value );
+			}
+		}
+	}
+	M_fluidVelocityToLambda->globalAssemble(M_structureDisplacementFESpace->mapPtr(), M_interfaceMap);
+
+	// ----------------------------------------------------------------------------------------------------------------------------
+
+	value = -1.0;
+	value /= M_timeStep;
+	value *= M_coefficientFirstDerivative;
+	M_structureDisplacementToLambda.reset ( new MatrixEpetra<Real> ( *M_interfaceMap ) );
+	for (UInt dim = 0; dim < 3; ++dim)
+	{
+		for (ITrow = localDofMap.begin(); ITrow != localDofMap.end(); ++ITrow)
+		{
+			if ( M_numerationInterface->map().map (Unique)->LID ( static_cast<EpetraInt_Type> (ITrow->second) ) >= 0 )
+			{
+				M_structureDisplacementToLambda->addToCoefficient( (int) (*M_numerationInterface)[ITrow->second] + dim * M_interface,
+																   ITrow->first + dim * M_fluidVelocityFESpace->dof().numTotalDof(),
+																   value );
+			}
+		}
+	}
+	M_structureDisplacementToLambda->globalAssemble(M_fluidVelocityFESpace->mapPtr(), M_interfaceMap);
+
+	// ----------------------------------------------------------------------------------------------------------------------------
+
+	value = -1.0;
+	M_structureDisplacementToFluidDisplacement.reset( new MatrixEpetra<Real> ( M_fluidVelocityFESpace->map() ) );
+	for (UInt dim = 0; dim < 3; ++dim)
+	{
+		for (ITrow = localDofMap.begin(); ITrow != localDofMap.end(); ++ITrow)
+		{
+			if ( M_numerationInterface->map().map (Unique)->LID ( static_cast<EpetraInt_Type> (ITrow->second) ) >= 0 )
+			{
+				M_structureDisplacementToFluidDisplacement->addToCoefficient( ITrow->first + dim * M_fluidVelocityFESpace->dof().numTotalDof(),
+																			  (int) (*M_numerationInterface)[ITrow->second] + dim * M_structureDisplacementFESpace->dof().numTotalDof(),
+																			  value );
+			}
+		}
+	}
+	M_structureDisplacementToFluidDisplacement->globalAssemble(M_structureDisplacementFESpace->mapPtr(), M_fluidVelocityFESpace->mapPtr());
+}
+
+
+
+}
---
+++ lifev/fsi_blocks/solver/FSIcouplingCE.hpp
@@ -0,0 +1,148 @@
+//@HEADER
+/*
+*******************************************************************************
+
+Copyright (C) 2004, 2005, 2007 EPFL, Politecnico di Milano, INRIA
+Copyright (C) 2010 EPFL, Politecnico di Milano, Emory University
+
+This file is part of LifeV.
+
+LifeV is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+LifeV is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with LifeV.  If not, see <http://www.gnu.org/licenses/>.
+
+*******************************************************************************
+*/
+//@HEADER
+
+/*!
+@file
+@brief Settings - File handling the solution of the FSI problem
+
+@author Davide Forti <davide.forti@epfl.ch>
+@date 28-10-2014
+
+@maintainer Simone Deparis <simone.deparis@epfl.ch>
+*/
+
+
+#ifndef FSICOUPLINGCE_H
+#define FSICOUPLINGCE_H
+
+#include <lifev/core/LifeV.hpp>
+
+// datafile
+#include <lifev/core/filter/GetPot.hpp>
+
+// includes for matrices and vector
+#include <lifev/core/array/MatrixEpetra.hpp>
+#include <lifev/core/array/VectorEpetra.hpp>
+
+#include <lifev/core/mesh/RegionMesh.hpp>
+#include <lifev/core/fem/FESpace.hpp>
+
+namespace LifeV
+{
+
+//! FSIcouplingCE - File handling the coupling when using the monolithic CE time-discretization
+/*!
+*  @author Davide Forti
+*/
+
+class FSIcouplingCE
+{
+public:
+
+	// Public typedefs
+
+    typedef Epetra_Comm comm_Type;
+	typedef boost::shared_ptr< comm_Type > commPtr_Type;
+
+    typedef RegionMesh<LinearTetra> mesh_Type;
+    typedef boost::shared_ptr<mesh_Type> meshPtr_Type;
+
+    typedef MapEpetra map_Type;
+	typedef boost::shared_ptr<map_Type> mapPtr_Type;
+
+	typedef MatrixEpetra<Real> matrix_Type;
+	typedef boost::shared_ptr<matrix_Type> matrixPtr_Type;
+
+	typedef boost::shared_ptr<VectorEpetra> vectorPtr_Type;
+
+    typedef FESpace< mesh_Type, map_Type > FESpace_Type;
+    typedef boost::shared_ptr<FESpace_Type> FESpacePtr_Type;
+
+    //! Constructor
+	FSIcouplingCE(const commPtr_Type& communicator);
+
+    //! Destructor
+    ~FSIcouplingCE();
+
+    void buildBlocks ( std::map<ID, ID> const& locDofMap );
+
+    void setUp ( const Real& timeStep, const Real& interfaceDofs, const Real& coefficientFirstDerivative,
+    			 const mapPtr_Type& interfaceMap, const FESpacePtr_Type& fluidVelocityFESpace,
+    			 const FESpacePtr_Type& structureDisplacementFESpace, const vectorPtr_Type& numerationInterface);
+
+    matrixPtr_Type lambdaToFluidMomentum() const
+    {
+    	return M_lambdaToFluidMomentum;
+    }
+
+    matrixPtr_Type lambdaToStructureMomentum() const
+    {
+    return M_lambdaToStructureMomentum;
+    }
+
+    matrixPtr_Type fluidVelocityToLambda() const
+    {
+    	return M_fluidVelocityToLambda;
+    }
+
+    matrixPtr_Type structureDisplacementToLambda() const
+    {
+    	return M_structureDisplacementToLambda;
+    }
+
+    matrixPtr_Type structureDisplacementToFluidDisplacement() const
+    {
+    	return M_structureDisplacementToFluidDisplacement;
+    }
+
+//@}
+
+private:
+
+    //! communicator
+    commPtr_Type M_comm;
+
+    matrixPtr_Type M_lambdaToFluidMomentum;
+    matrixPtr_Type M_lambdaToStructureMomentum;
+    matrixPtr_Type M_fluidVelocityToLambda; // I need to test if it faster to assemble or to transpose M_lambdaToFluidVelocity
+    matrixPtr_Type M_structureDisplacementToLambda; // I need to test if it faster to assemble or to transpose M_lambdaToStructureDisplacement
+    matrixPtr_Type M_structureDisplacementToFluidDisplacement;
+
+    FESpacePtr_Type M_fluidVelocityFESpace;
+    FESpacePtr_Type M_structureDisplacementFESpace;
+
+    mapPtr_Type M_interfaceMap;
+
+    vectorPtr_Type M_numerationInterface;
+
+    Real M_interface;
+    Real M_timeStep;
+    Real M_coefficientFirstDerivative;
+};
+
+} // end namespace LifeV
+
+#endif // end FSICOUPLINGCE_H
