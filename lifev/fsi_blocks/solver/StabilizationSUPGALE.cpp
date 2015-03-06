85a9a62883683c15dbb8d86ece8df988ccdb0c43
Davide Forti <davide.forti@epfl.ch>
2015-03-06 09:24:52 +0100

    Adding stabilization SUPG for the ALE frame of reference

---
+++ lifev/fsi_blocks/solver/StabilizationSUPGALE.cpp
@@ -0,0 +1,273 @@
+#include <lifev/fsi_blocks/solver/StabilizationSUPGALE.hpp>
+
+// MACRO TO DEFINE TAU_M
+#define TAU_M 	       value(1)/( eval(squareroot,TAU_M_DEN) )
+#define TAU_M_DEN      TAU_M_DEN_DT + TAU_M_DEN_VEL + TAU_M_DEN_VISC
+#define TAU_M_DEN_DT   value(M_density*M_density)*value(M_bdfOrder*M_bdfOrder)/value(M_timestep * M_timestep)
+#define TAU_M_DEN_VEL  value(M_density*M_density)*dot(value(M_fespaceUETA, beta_km1), value(M_fespaceUETA, beta_km1))/(h_K*h_K)
+#define TAU_M_DEN_VISC value(M_C_I)*value(M_viscosity*M_viscosity)/(h_K*h_K*h_K*h_K)
+
+// MACRO TO DEFINE TAU_C
+#define TAU_C (h_K*h_K)/(TAU_M)
+
+namespace LifeV
+{
+
+//=============================================================================
+// Constructor
+//=============================================================================
+
+StabilizationSUPGALE::StabilizationSUPGALE(FESpace<mesh_Type, MapEpetra>&  velocityFESpace,
+						             FESpace<mesh_Type, MapEpetra>&  pressureFESpace):
+M_uFESpace (velocityFESpace),
+M_pFESpace (pressureFESpace)
+{
+}
+
+//=============================================================================
+// Methods
+//=============================================================================
+
+void StabilizationSUPGALE::setConstant(const int & value)
+{
+	if ( value == 1 )
+		M_C_I = 30;
+	else if ( value == 2 )
+		M_C_I = 60;
+	else
+		ASSERT(0!=0, "Please implement a suitable value for M_C_I for your velocity FE order");
+}
+
+void StabilizationSUPGALE::buildGraphs()
+{
+	// We assume that the fluid velocity and the mesh velocity are discretized by FE of same order
+	vector_Type u_km1( M_uFESpace.map(), Repeated);
+	vector_Type beta_km1( M_uFESpace.map(), Repeated);
+	vector_Type p_km1( M_pFESpace.map(), Repeated);
+	vector_Type u_bdf( M_uFESpace.map(), Repeated);
+
+	u_km1.zero();
+	beta_km1.zero();
+	p_km1.zero();
+	u_bdf.zero();
+
+	u_km1 += 1;
+	beta_km1 += 1;
+	p_km1 += 1;
+	u_bdf += 1;
+
+	boost::shared_ptr<SquareRoot> squareroot(new SquareRoot());
+
+	MatrixSmall<3, 3> Eye;
+	Eye *= 0.0;
+	Eye[0][0] = 1;
+	Eye[1][1] = 1;
+	Eye[2][2] = 1;
+
+	{
+		using namespace ExpressionAssembly;
+
+		// Graph for block 00
+		M_graph_block00.reset (new Epetra_FECrsGraph (Copy, * (M_uFESpace.map().map (Unique) ), 0) );
+		buildGraph ( elements ( M_uFESpace.mesh() ),
+					 quadRuleTetra4pt,
+					 M_fespaceUETA,
+					 M_fespaceUETA,
+			/*(1)*/	  TAU_M * value(M_density*M_density) * value(M_alpha/M_timestep) * dot( value(M_fespaceUETA, beta_km1)*grad(phi_i), phi_j )
+			/*(2)*/	 +TAU_M * value(M_density*M_density) * value(M_alpha/M_timestep) * dot( phi_j*grad(phi_i), value(M_fespaceUETA, u_km1) )
+			/*(3)*/	 -TAU_M * value(M_density*M_density) * dot( phi_j*grad(phi_i), value(M_fespaceUETA, u_bdf) )
+			/*(4)*/	 +TAU_M * value(M_density*M_density) * dot( value(M_fespaceUETA, beta_km1)*grad(phi_i), phi_j*grad(M_fespaceUETA, u_km1) )
+			/*(5)*/	 +TAU_M * value(M_density*M_density) * dot( value(M_fespaceUETA, beta_km1)*grad(phi_i), value(M_fespaceUETA, beta_km1)*grad(phi_j) )
+			/*(6)*/	 +TAU_M * value(M_density*M_density) * dot( phi_j*grad(phi_i), value(M_fespaceUETA, beta_km1)*grad(M_fespaceUETA, u_km1) )
+			/*(8)*/  +TAU_M * value(M_density) * dot( phi_j*grad(phi_i), grad(M_fespacePETA, p_km1) )
+			/*(9)*/  -TAU_M * value(M_density*M_viscosity) * dot( value(M_fespaceUETA, beta_km1)*grad(phi_i), laplacian(phi_j) )
+		   /*(10)*/  -TAU_M * value(M_density*M_viscosity) * dot( phi_j*grad(phi_i), laplacian(M_fespaceUETA, u_km1) )
+		   /*(16)*/  +TAU_C * div(phi_i)*div(phi_j)
+				   ) >> M_graph_block00;
+		M_graph_block00->GlobalAssemble();
+		M_graph_block00->OptimizeStorage();
+
+		// Graph for block 10
+		M_graph_block10.reset (new Epetra_FECrsGraph (Copy, * (M_pFESpace.map().map (Unique) ), 0) );
+		buildGraph ( elements (M_pFESpace.mesh() ),
+					 quadRuleTetra4pt,
+					 M_fespacePETA,
+					 M_fespaceUETA,
+			/*(11)*/  TAU_M * value(M_alpha*M_density/M_timestep) * dot( grad(phi_i), phi_j )
+			/*(12)*/ +TAU_M * value(M_density) * dot( grad(M_fespaceUETA, u_km1)*grad(phi_i), phi_j )
+			/*(13)*/ +TAU_M * value(M_density) * dot( grad(phi_i), value(M_fespaceUETA, beta_km1)*grad(phi_j) )
+			/*(15)*/ -TAU_M * value(M_viscosity) * dot( grad(phi_i), laplacian(phi_j) )
+				   ) >> M_graph_block10;
+		M_graph_block10->GlobalAssemble ( * (M_uFESpace.map().map (Unique) ), * (M_pFESpace.map().map (Unique) ) );
+		M_graph_block10->OptimizeStorage();
+
+		// Graph for block 01
+		M_graph_block01.reset (new Epetra_FECrsGraph (Copy, * (M_uFESpace.map().map (Unique) ), 0) );
+		buildGraph ( elements (M_pFESpace.mesh() ),
+					 quadRuleTetra4pt,
+					  M_fespaceUETA,
+					  M_fespacePETA,
+			/*(7)*/	   TAU_M * value(M_density) * dot( value(M_fespaceUETA, beta_km1)*grad(phi_i), grad(phi_j) )
+				   ) >> M_graph_block01;
+		M_graph_block01->GlobalAssemble ( * (M_pFESpace.map().map (Unique) ), * (M_uFESpace.map().map (Unique) ) );
+		M_graph_block01->OptimizeStorage();
+
+		// Graph for block 11
+		M_graph_block11.reset (new Epetra_FECrsGraph (Copy, * (M_pFESpace.map().map (Unique) ), 0) );
+		buildGraph ( elements (M_pFESpace.mesh() ),
+					 quadRuleTetra4pt,
+					 M_fespacePETA,
+					 M_fespacePETA,
+			/*(14)*/  TAU_M*dot(  grad(phi_i), grad(phi_j) )
+		           ) >> M_graph_block11;
+		M_graph_block11->GlobalAssemble ( );
+		M_graph_block11->OptimizeStorage();
+
+	}
+
+	M_block_00.reset (new matrix_Type ( M_uFESpace.map(), *M_graph_block00 ) );
+	M_block_00->zero();
+	M_block_00->globalAssemble();
+
+	M_block_01.reset (new matrix_Type ( M_uFESpace.map(), *M_graph_block01 ) );
+	M_block_01->zero();
+	M_block_01->globalAssemble( M_pFESpace.mapPtr(), M_uFESpace.mapPtr() );
+
+	M_block_10.reset (new matrix_Type ( M_pFESpace.map(), *M_graph_block10 ) );
+	M_block_10->zero();
+	M_block_10->globalAssemble( M_uFESpace.mapPtr(), M_pFESpace.mapPtr() );
+
+	M_block_11.reset (new matrix_Type ( M_pFESpace.map(), *M_graph_block11 ) );
+	M_block_11->zero();
+	M_block_11->globalAssemble( );
+}
+
+void StabilizationSUPGALE::jacobian( const vector_Type& convective_velocity_previous_newton_step,
+									 const vector_Type& velocity_previous_newton_step,
+		  	  	  	  	  	  	  	 const vector_Type& pressure_previous_newton_step,
+		  	  	  	  	  	  	     const vector_Type& velocity_rhs)
+{
+	// missing force
+
+	M_block_00->zero();
+	M_block_01->zero();
+	M_block_10->zero();
+	M_block_11->zero();
+
+	vector_Type beta_km1( convective_velocity_previous_newton_step, Repeated);
+	vector_Type u_km1( velocity_previous_newton_step, Repeated);
+	vector_Type p_km1( pressure_previous_newton_step, Repeated);
+	vector_Type u_bdf( velocity_rhs, Repeated);
+
+	boost::shared_ptr<SquareRoot> squareroot(new SquareRoot());
+
+	MatrixSmall<3, 3> Eye;
+	Eye *= 0.0;
+	Eye[0][0] = 1;
+	Eye[1][1] = 1;
+	Eye[2][2] = 1;
+
+	using namespace ExpressionAssembly;
+
+	integrate(
+				elements(M_uFESpace.mesh()),
+				M_uFESpace.qr(),
+				M_fespaceUETA, // test  w -> phi_i
+				M_fespaceUETA, // trial \delta u -> phi_j
+		/*(1)*/	  TAU_M * value(M_density*M_density) * value(M_alpha/M_timestep) * dot( value(M_fespaceUETA, beta_km1)*grad(phi_i), phi_j )
+		/*(2)*/	 +TAU_M * value(M_density*M_density) * value(M_alpha/M_timestep) * dot( phi_j*grad(phi_i), value(M_fespaceUETA, u_km1) )
+		/*(3)*/	 -TAU_M * value(M_density*M_density) * dot( phi_j*grad(phi_i), value(M_fespaceUETA, u_bdf) )
+		/*(4)*/	 +TAU_M * value(M_density*M_density) * dot( value(M_fespaceUETA, beta_km1)*grad(phi_i), phi_j*grad(M_fespaceUETA, u_km1) )
+		/*(5)*/	 +TAU_M * value(M_density*M_density) * dot( value(M_fespaceUETA, beta_km1)*grad(phi_i), value(M_fespaceUETA, beta_km1)*grad(phi_j) )
+		/*(6)*/	 +TAU_M * value(M_density*M_density) * dot( phi_j*grad(phi_i), value(M_fespaceUETA, beta_km1)*grad(M_fespaceUETA, u_km1) )
+		/*(8)*/  +TAU_M * value(M_density) * dot( phi_j*grad(phi_i), grad(M_fespacePETA, p_km1) )
+		/*(9)*/  -TAU_M * value(M_density*M_viscosity) * dot( value(M_fespaceUETA, beta_km1)*grad(phi_i), laplacian(phi_j) )
+	   /*(10)*/  -TAU_M * value(M_density*M_viscosity) * dot( phi_j*grad(phi_i), laplacian(M_fespaceUETA, u_km1) )
+	   /*(16)*/  +TAU_C * div(phi_i)*div(phi_j)
+			) >> M_block_00;
+	M_block_00->globalAssemble();
+
+	integrate(
+				elements(M_uFESpace.mesh()),
+				M_pFESpace.qr(),
+				M_fespacePETA, // test  q -> phi_i
+				M_fespaceUETA, // trial \delta u -> phi_j
+		/*(11)*/  TAU_M * value(M_alpha*M_density/M_timestep) * dot( grad(phi_i), phi_j )
+		/*(12)*/ +TAU_M * value(M_density) * dot( grad(M_fespaceUETA, u_km1)*grad(phi_i), phi_j )
+		/*(13)*/ +TAU_M * value(M_density) * dot( grad(phi_i), value(M_fespaceUETA, beta_km1)*grad(phi_j) )
+		/*(15)*/ -TAU_M * value(M_viscosity) * dot( grad(phi_i), laplacian(phi_j) )
+	         ) >> M_block_10;
+	M_block_10->globalAssemble( M_uFESpace.mapPtr(), M_pFESpace.mapPtr() );
+
+	integrate(
+				elements(M_uFESpace.mesh()),
+				M_uFESpace.qr(),
+				M_fespaceUETA, // test  w -> phi_i
+				M_fespacePETA, // trial \delta p -> phi_j
+		/*(7)*/	  TAU_M * value(M_density) * dot( value(M_fespaceUETA, beta_km1)*grad(phi_i), grad(phi_j) )
+		     ) >> M_block_01;
+	M_block_01->globalAssemble( M_pFESpace.mapPtr(), M_uFESpace.mapPtr() );
+
+	integrate(
+				elements(M_uFESpace.mesh()),
+				M_pFESpace.qr(),
+				M_fespacePETA, // test   q -> phi_i
+				M_fespacePETA, // trial  \delta p -> phi_j
+		/*(14)*/  TAU_M*dot(  grad(phi_i), grad(phi_j) )
+		     ) >> M_block_11;
+	M_block_11->globalAssemble();
+
+}
+
+void StabilizationSUPGALE::residual( vectorPtr_Type& residual_velocity,
+								     vectorPtr_Type& residual_pressure,
+								     const vector_Type& convective_velocity_previous_newton_step,
+								     const vector_Type& velocity_previous_newton_step,
+								     const vector_Type& pressure_previous_newton_step,
+								     const vector_Type& velocity_rhs)
+{
+	// missing force, terms 11 and 12
+
+	vector_Type beta_km1( convective_velocity_previous_newton_step, Repeated);
+	vector_Type u_km1( velocity_previous_newton_step, Repeated);
+	vector_Type p_km1( pressure_previous_newton_step, Repeated);
+	vector_Type u_bdf( velocity_rhs, Repeated);
+
+	boost::shared_ptr<SquareRoot> squareroot(new SquareRoot());
+
+	// Matrix needed to evaluate the divergence of a vector (term with TAU_C)
+	MatrixSmall<3, 3> Eye;
+	Eye *= 0.0;
+	Eye[0][0] = 1;
+	Eye[1][1] = 1;
+	Eye[2][2] = 1;
+
+    using namespace ExpressionAssembly;
+
+	integrate(
+				elements(M_uFESpace.mesh()),
+				M_uFESpace.qr(),
+				M_fespaceUETA,
+	  /*(1)*/	  TAU_M * value(M_density*M_density)*value(M_alpha/M_timestep) * dot( value(M_fespaceUETA, beta_km1)*grad(phi_i), value(M_fespaceUETA, u_km1) )
+	  /*(2)*/    -TAU_M * value(M_density*M_density) * dot( value(M_fespaceUETA, beta_km1)*grad(phi_i), value(M_fespaceUETA, u_bdf) )
+	  /*(3)*/    +TAU_M*value(M_density*M_density)*dot( value(M_fespaceUETA, beta_km1)*grad(phi_i), value(M_fespaceUETA, beta_km1)*grad(M_fespaceUETA, u_km1) )
+	  /*(4)*/	 +TAU_M*value(M_density)*dot( value(M_fespaceUETA, beta_km1)*grad(phi_i), grad(M_fespacePETA, p_km1) )
+	  /*(5)*/	 -TAU_M*value(M_density*M_viscosity)*dot( value(M_fespaceUETA, beta_km1)*grad(phi_i), laplacian(M_fespaceUETA, u_km1) )
+	 /*(11)*/	 +TAU_C*div(phi_i)* trace ( grad(M_fespaceUETA, u_km1) )
+			 )
+			 >> residual_velocity;
+
+	integrate(
+				elements(M_uFESpace.mesh()),
+				M_pFESpace.qr(),
+				M_fespacePETA,
+	  /*(6)*/	  TAU_M*value(M_density*M_alpha/M_timestep)*dot( grad(phi_i), value(M_fespaceUETA, u_km1) )
+	  /*(7)*/	-TAU_M*value(M_density)*dot( grad(phi_i), value(M_fespaceUETA, u_bdf))
+	  /*(8)*/   +TAU_M*value(M_density)*dot( grad(phi_i), value(M_fespaceUETA, beta_km1)*grad(M_fespaceUETA, u_km1) )
+	  /*(9)*/	+TAU_M*dot( grad(phi_i), grad(M_fespacePETA,p_km1) )
+	 /*(10)*/   -TAU_M*value(M_viscosity)*dot(grad(phi_i), laplacian(M_fespaceUETA, u_km1))
+			 )
+		     >> residual_pressure;
+}
+
+} // namespace LifeV
---
+++ lifev/fsi_blocks/solver/StabilizationSUPGALE.hpp
@@ -0,0 +1,255 @@
+//@HEADER
+/*
+ *******************************************************************************
+
+    Copyright (C) 2004, 2005, 2007 EPFL, Politecnico di Milano, INRIA
+    Copyright (C) 2010 EPFL, Politecnico di Milano, Emory University
+
+    This file is part of LifeV.
+
+    LifeV is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    LifeV is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with LifeV.  If not, see <http://www.gnu.org/licenses/>.
+
+ *******************************************************************************
+ */
+//@HEADER
+/*!
+   @file
+   @brief SUPG stabilization.
+   @author Davide Forti <davide.forti@epfl.ch>
+   @maintainer Davide Forti <davide.forti@epfl.ch>
+   @date 03-02-2015
+
+   This file contains an ETA implementation of SUPG, fully implicit for the moment.
+
+   For reference, see Paper Y. Bazilevs, V.M. Calo, J.A. Cottrell, T.J.R. Hughes, A. Reali, and G. Scovazzi. <i>Variational multiscale
+   residual-based turbulence modeling for large eddy simulation of incompressible flows</i>. Comput. Methods Appl. Mech. Engr. 197(1):173–201, 2007.
+
+*/
+
+#ifndef _STABILIZATIONSUPGALE_HPP_
+#define _STABILIZATIONSUPGALE_HPP_
+
+// Tell the compiler to ignore specific kind of warnings:
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+
+#include <Epetra_ConfigDefs.h>
+#ifdef EPETRA_MPI
+    #include <mpi.h>
+    #include <Epetra_MpiComm.h>
+#else
+    #include <Epetra_SerialComm.h>
+#endif
+
+#include <Epetra_FECrsMatrix.h>
+
+//Tell the compiler to restore the warning previously silented
+#pragma GCC diagnostic warning "-Wunused-variable"
+#pragma GCC diagnostic warning "-Wunused-parameter"
+
+#include <lifev/core/fem/FESpace.hpp>
+#include <lifev/core/fem/ReferenceFE.hpp>
+
+#include <lifev/eta/fem/ETFESpace.hpp>
+
+// includes for building the matrix graph
+#include <Epetra_FECrsGraph.h>
+#include <lifev/eta/expression/Integrate.hpp>
+#include <lifev/eta/expression/BuildGraph.hpp>
+
+namespace LifeV
+{
+
+class SquareRoot
+{
+public:
+    typedef Real return_Type;
+
+    inline return_Type operator() (const Real& a)
+    {
+        return std::sqrt(a);
+    }
+
+    SquareRoot() {}
+    SquareRoot (const SquareRoot&) {}
+    ~SquareRoot() {}
+};
+
+class StabilizationSUPGALE
+{
+public:
+
+    //@name Public Types
+    //@{
+    typedef RegionMesh<LinearTetra> mesh_Type;
+    typedef MapEpetra  map_Type;
+
+    typedef VectorEpetra  vector_Type;
+    typedef boost::shared_ptr<vector_Type>  vectorPtr_Type;
+
+    typedef MatrixEpetra<Real>  matrix_Type;
+    typedef boost::shared_ptr<matrix_Type>  matrixPtr_Type;
+
+    typedef FESpace< mesh_Type, map_Type > fespace_Type;
+    typedef boost::shared_ptr< fespace_Type > fespacePtr_Type;
+
+    typedef ETFESpace<mesh_Type, map_Type, 3, 3 > ETFESpace_velocity;
+    typedef ETFESpace<mesh_Type, map_Type, 3, 1 > ETFESpace_pressure;
+
+    typedef boost::shared_ptr<ETFESpace_velocity > ETFESpacePtr_velocity;
+    typedef boost::shared_ptr<ETFESpace_pressure > ETFESpacePtr_pressure;
+
+    typedef Epetra_FECrsGraph graph_Type;
+    typedef boost::shared_ptr<Epetra_FECrsGraph> graphPtr_Type;
+
+    //@}
+
+    //! @name Constructor and Destructor
+    //@{
+    //! Default Constructor
+    StabilizationSUPGALE(FESpace<mesh_Type, MapEpetra>&  velocityFESpace, FESpace<mesh_Type, MapEpetra>&  pressureFESpace);
+
+    //! ~Destructor
+    ~StabilizationSUPGALE() {};
+
+    //@}
+
+    //! Build the graphs of each single block
+    void buildGraphs();
+
+    //! Updates the jacobian matrix
+    /*!
+     * @param convective_velocity_previous_newton_step convective velocity from the previous Newton step
+     * @param velocity_previous_newton_step velocity from the previous Newton step
+     * @param pressure_previous_newton_step pressure from the previous Newton step
+     * @param velocity_rhs velocity term from approximation time derivative
+     */
+    void jacobian(	const vector_Type& convective_velocity_previous_newton_step,
+    		        const vector_Type& velocity_previous_newton_step,
+    		        const vector_Type& pressure_previous_newton_step,
+    		        const vector_Type& velocity_rhs );
+
+    //! Adds to the residual the contribution coming from the SUPG stabilization
+    /*!
+     * @param residual_velocity velocity component of the residual
+     * @param residual_pressure pressure component of the residual
+     * @param convective_velocity_previous_newton_step convective velocity from the previous Newton step
+     * @param velocity_previous_newton_step velocity from the previous Newton step
+     * @param pressure_previous_newton_step pressure from the previous Newton step
+     * @param velocity_rhs velocity term from approximation time derivative
+     */
+    void residual(  vectorPtr_Type& residual_velocity,
+    			    vectorPtr_Type& residual_pressure,
+    			    const vector_Type& convective_velocity_previous_newton_step,
+    			    const vector_Type& velocity_previous_newton_step,
+    			    const vector_Type& pressure_previous_newton_step,
+    			    const vector_Type& velocity_rhs);
+
+    //! Set the constant C_I for the supg
+    void setConstant (const int & value);
+
+    //! Set the fluid density
+    void setDensity (const Real & density) { M_density = density;}
+
+    //! Set the bdf order
+    void setBDForder (const Real & bdfOrder) { M_bdfOrder = bdfOrder;}
+
+    //! Set the bdf order
+    void setAlpha (const Real & alpha) { M_alpha = alpha;}
+
+    //! Set the fluid dynamic viscosity
+    void setViscosity (const Real & viscosity) { M_viscosity = viscosity;}
+
+    //! Set the Epetra communicator
+    void setCommunicator (boost::shared_ptr<Epetra_Comm> comm) { M_comm = comm;}
+
+    //! Set the time step size
+    void setTimeStep  (const Real & timestep)  { M_timestep = timestep;}
+
+    void setETvelocitySpace(const ETFESpacePtr_velocity & velocityEta_fespace){ M_fespaceUETA = velocityEta_fespace;}
+
+    void setETpressureSpace(const ETFESpacePtr_pressure & pressureEta_fespace){ M_fespacePETA = pressureEta_fespace;}
+
+    //! @name Getters
+    //@{
+
+    matrixPtr_Type const& block_00() const
+    {
+    	return M_block_00;
+    }
+
+    matrixPtr_Type const& block_01() const
+    {
+    	return M_block_01;
+    }
+
+    matrixPtr_Type const& block_10() const
+    {
+    	return M_block_10;
+    }
+
+    matrixPtr_Type const& block_11() const
+    {
+    	return M_block_11;
+    }
+
+    //@}
+
+private:
+
+    //! @name Private Attributes
+    //@{
+
+    //! finite element spaces for velocity and pressure
+    fespace_Type& M_uFESpace;
+    fespace_Type& M_pFESpace;
+
+    ETFESpacePtr_velocity M_fespaceUETA;
+    ETFESpacePtr_pressure M_fespacePETA;
+
+    //! Epetra communicator
+    boost::shared_ptr<Epetra_Comm> M_comm;
+
+    //! fluid dynamic viscosity @f$\nu@f$
+    Real         M_viscosity;
+
+    //! fluid density @f$\nu@f$
+    Real         M_density;
+
+    //! stabilization parameters for the momentum and continuity equations
+    Real         M_timestep;
+    bool         M_flag_timestep;
+    Real         M_bdfOrder;
+    Real         M_alpha;
+
+    Real         M_C_I;
+
+    // Graphs
+    graphPtr_Type M_graph_block00;
+    graphPtr_Type M_graph_block01;
+    graphPtr_Type M_graph_block10;
+    graphPtr_Type M_graph_block11;
+
+    // Matrices
+    matrixPtr_Type M_block_00;
+    matrixPtr_Type M_block_01;
+    matrixPtr_Type M_block_10;
+    matrixPtr_Type M_block_11;
+
+    //@}
+}; // class StabilizationSUPG
+
+} // namespace LifeV
+
+#endif /* _STABILIZATIONSUPGALE_HPP_ */
